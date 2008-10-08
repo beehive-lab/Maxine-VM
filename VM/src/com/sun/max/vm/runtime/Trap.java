d006188ae054f88deae66e95ba512e2bc0df8908
Ben Titzer <ben.titzer@sun.com>
2008-10-08 16:54:34 -0700

    Extraneous import

--- VM/src/com/sun/max/vm/runtime/Trap.java
+++ VM/src/com/sun/max/vm/runtime/Trap.java
@@ -36,7 +36,6 @@ import com.sun.max.vm.jit.*;
 import com.sun.max.vm.reference.*;
 import com.sun.max.vm.thread.*;
 import com.sun.max.vm.type.*;
-import com.sun.max.memory.VirtualMemory;
 
 /**
  * This class handles operating systems traps that can arise from implicit null pointer checks,
