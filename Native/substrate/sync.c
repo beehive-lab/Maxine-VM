e43ef5841dfa4095c1e59fe9f43f35b7070caf75
Bernd Mathiske <Bernd.Mathiske@sun.com>
2008-11-13 10:26:23 -0800

    minor edits

--- Native/substrate/sync.c
+++ Native/substrate/sync.c
@@ -63,12 +63,8 @@ Java_com_sun_max_vm_monitor_modal_sync_nat_ConditionVariable_nativeConditionWait
 }
 
 jboolean nativeConditionNotify(Condition condition, jboolean all) {
-#if os_SOLARIS || os_LINUX || os_DARWIN || os_GUESTVMXEN
-	if (all) {
-		return condition_notifyAll(condition);
-	}
+    if (all) {
+        return condition_notifyAll(condition);
+    }
     return condition_notify(condition);
-#else
-#error Unimplemented
-#endif
 }
