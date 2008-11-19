159ea9b41930ae440dff92782268eae4681af1b9
ldayne <ldayne@localhost>
2008-11-19 11:59:08 +0100

    Fix trapStub bugs. Refactored RET instruction on sparc to take a FROM arguments that specify where the RET instruction return from (trampoline static or dynamic, trap stub, or normal method). Previous implementation of return from a trap stub was incorrect: the stub cannot use register %i7 for returning as this one must have the value that was in the %o7 of the trapped instruction. This required changing the implementation of RET for trap stub.

---
+++ VM/src/com/sun/max/vm/heap/beltway/BeltwayConfiguration.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+
+package com.sun.max.vm.heap.beltway;
+
+import com.sun.max.annotate.*;
+import com.sun.max.unsafe.*;
+
+/**
+ * @author Christos Kotselidis
+ */
+
+public final class BeltwayConfiguration extends BeltwayHeapSchemeConfiguration {
+
+    public static final int _numberOfGCThreads = 1;
+
+    private static int _numberOfBelts;
+
+    public static final boolean _useTLABS = true;
+    public static boolean _useGCTlabs = true;
+    public static boolean _parallelScavenging = false;
+
+    private static int[] _percentagesOfUsableMemoryPerBelt;
+
+
+    public void initializeBeltWayConfiguration(Address applicationHeapStartAddress, Size applicationMaxHeapSize, int numberOfBelts, int[] percentagesOfUsableMemoryPerBelt) {
+        _applicationHeapStartAddress = applicationHeapStartAddress;
+        _applicationHeapMaxSize = applicationMaxHeapSize;
+        _numberOfBelts = numberOfBelts;
+        _percentagesOfUsableMemoryPerBelt = percentagesOfUsableMemoryPerBelt;
+    }
+
+    public BeltwayConfiguration() {
+        super(Address.zero(), Size.zero());
+    }
+
+    @INLINE
+    public static void setNumberOfBelts(int numOfBelts) {
+        _numberOfBelts = numOfBelts;
+    }
+
+    @INLINE
+    public static void setPercentageOfUsableMemoryPerFrame(int[] percent) {
+        _percentagesOfUsableMemoryPerBelt = percent;
+    }
+
+    @INLINE
+    public static int getPercentOfUsableMemoryPerBelt(int indexOfBelt) {
+        return _percentagesOfUsableMemoryPerBelt[indexOfBelt];
+    }
+
+    @INLINE
+    public static int getNumberOfBelts() {
+        return _numberOfBelts;
+    }
+
+}
