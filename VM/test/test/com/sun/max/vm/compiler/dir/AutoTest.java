e7488ea065f4ca6e3beb20d02a5bc2ce5fd5b97f
Doug Simon <doug.simon@sun.com>
2008-11-26 17:51:31 -0800

    Added auto-test logs to trace. Disabled slow DIR auto-tests

--- VM/test/test/com/sun/max/vm/compiler/dir/AutoTest.java
+++ VM/test/test/com/sun/max/vm/compiler/dir/AutoTest.java
@@ -34,7 +34,8 @@ public final class AutoTest {
     }
 
     public static Test suite() {
-        final TestSuite suite = new TestCaseClassSet(new Package()).addToEnd(DirTranslatorTest_max.class).toTestSuite();
+        final Class[] slowTests = {DirTranslatorTest_max.class};
+        final TestSuite suite = new TestCaseClassSet(new Package()).removeAll(slowTests).toTestSuite();
         return new DirTranslatorTestSetup(suite);
     }
 }
