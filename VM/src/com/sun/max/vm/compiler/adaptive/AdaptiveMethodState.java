7b3a88cd322051113f5bb17df7c22c4f57a51894
Mick Jordan <Mick.Jordan@sun.com>
2008-12-05 14:53:14 -0800

    Fix bug in ProxyAcquirableJavaMonitor, improve JavaMonitorManager unbound monitor list expansion logic, make choice of VirtualMemory/Memory in SemiSpaceHeapScheme property based

---
+++ VM/src/com/sun/max/vm/compiler/adaptive/AdaptiveMethodState.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (c) 2008 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.vm.compiler.adaptive;
+
+import com.sun.max.annotate.*;
+import com.sun.max.lang.*;
+import com.sun.max.vm.actor.member.*;
+import com.sun.max.vm.code.*;
+import com.sun.max.vm.compiler.*;
+import com.sun.max.vm.compiler.instrument.*;
+import com.sun.max.vm.compiler.target.*;
+
+/**
+ * The {@code MethodState} class implements a container for a number of pieces of information about a class method
+ * actor that are used during compilation and recompilation, including any instrumentation attached to various
+ * versions as well as the "current" compiled code.
+ *
+ * Multiple histories are kept for each {@link CompilationDirective}.
+ *
+ * @author Ben L. Titzer
+ * @author Michael Bebenita
+ */
+public class AdaptiveMethodState extends MethodState {
+
+    /**
+     * The enclosing instance of the compilation scheme.
+     */
+    private final AdaptiveCompilationScheme _adaptiveCompilationScheme;
+
+    protected TargetMethod[][] _targetMethods;
+
+    protected MethodInstrumentation _methodInstrumentation;
+    protected Compilation [] _currentCompilations;
+
+    protected AdaptiveMethodState(AdaptiveCompilationScheme adaptiveCompilationScheme, ClassMethodActor classMethodActor) {
+        super(classMethodActor, AdaptiveCompilationScheme.DEFAULT_HISTORY_LENGTH);
+        this._adaptiveCompilationScheme = adaptiveCompilationScheme;
+        _targetMethods = new TargetMethod[CompilationDirective.count()][];
+        _currentCompilations = new Compilation[CompilationDirective.count()];
+    }
+
+    /**
+     * Constructs the method instrumentation for this method state, if it does not exist already.
+     *
+     * @return the method instrumentation for this method state
+     */
+    public synchronized MethodInstrumentation makeMethodInstrumentation() {
+        if (_methodInstrumentation == null) {
+            _methodInstrumentation = new MethodInstrumentation(_classMethodActor);
+        }
+        return _methodInstrumentation;
+    }
+
+    /**
+     * Gets the method instrumentation for this method state.
+     *
+     * @return the method instrumentation if it exists; {@code null} if it does not exist
+     */
+    public MethodInstrumentation getMethodInstrumentation() {
+        return _methodInstrumentation;
+    }
+
+    /**
+     * Gets the currently pending compilation for this method state, if any.
+     *
+     * @return any currently pending compilation
+     */
+    @INLINE Compilation currentCompilation(CompilationDirective compilationDirective) {
+        return _currentCompilations[compilationDirective.ordinal()];
+    }
+
+    void setCurrentCompilation(Compilation compilation, CompilationDirective compilationDirective) {
+        _currentCompilations[compilationDirective.ordinal()] = compilation;
+    }
+
+    /**
+     * Update this method state to a new compiled version. If the current target method is {@code null} (e.g. this
+     * method has never been compiled), then the update takes effect immediately. If there is already a target
+     * method, then the compilation scheme will decide if the new code is a valid update, and if so, perform the
+     * necessary code patching to forward calls to the old target method to the new one.
+     *
+     * @param newTargetMethod the new target method
+     */
+    protected final void setTargetMethod(TargetMethod newTargetMethod, CompilationDirective compilationDirective) {
+        final TargetMethod currentTargetMethod = currentTargetMethod(compilationDirective);
+        if (currentTargetMethod == null) {
+            // no previous code for this method and this compilation directive
+            addTargetMethod(newTargetMethod, compilationDirective);
+        } else if (currentTargetMethod == newTargetMethod) {
+            // nothing to be done.
+        } else if (this._adaptiveCompilationScheme.allowUpdate(this, newTargetMethod)) {
+            // the method update was allowed, perform code update on previous compilations
+            // we only update target methods that were compiled with promotable compilation directives
+            for (CompilationDirective promotableDirective : compilationDirective.promotableFrom()) {
+                final TargetMethod[] methodHistory = methodHistory(promotableDirective);
+                if (methodHistory != null) {
+                    for (int i = 0; i < methodHistory.length; i++) {
+                        Code.updateTargetMethod(methodHistory[i], newTargetMethod);
+                    }
+                }
+            }
+            addTargetMethod(newTargetMethod, compilationDirective);
+        } else {
+            Code.discardTargetMethod(newTargetMethod);
+        }
+    }
+
+    private TargetMethod[] methodHistory(CompilationDirective compilationDirective) {
+        final TargetMethod[] methodHistory = _targetMethods[compilationDirective.ordinal()];
+        return methodHistory;
+    }
+
+    @Override
+    public TargetMethod currentTargetMethod(CompilationDirective compilationDirective) {
+        final TargetMethod[] methodHistory = methodHistory(compilationDirective);
+        if (methodHistory != null) {
+            return methodHistory[methodHistory.length - 1];
+        }
+        return null;
+    }
+
+    private void addTargetMethod(TargetMethod targetMethod, CompilationDirective compilationDirective) {
+        TargetMethod[] methodHistory = methodHistory(compilationDirective);
+        if (methodHistory != null) {
+            methodHistory = Arrays.extend(methodHistory, methodHistory.length + 1);
+        } else {
+            methodHistory = new TargetMethod[1];
+        }
+        methodHistory[methodHistory.length - 1] = targetMethod;
+        _targetMethods[compilationDirective.ordinal()] = methodHistory;
+        // TODO: No need to keep track of method history in two places, remove if from the super class.
+        // For now we leave it in to make the inspector happy.
+        addTargetMethod(targetMethod);
+    }
+
+    public final TargetMethod jittedTargetMethod() {
+        return currentTargetMethod(CompilationDirective.JIT);
+    }
+
+    public final TargetMethod tracedTargetMethod() {
+        return currentTargetMethod(CompilationDirective.TRACE_JIT);
+    }
+
+    public final boolean isJitted() {
+        return jittedTargetMethod() != null;
+    }
+
+    public final boolean isTraced() {
+        return tracedTargetMethod() != null;
+    }
+
+    @Override
+    public TargetMethod currentTargetMethod() {
+        final TargetMethod targetMethod = currentTargetMethod(CompilationDirective.DEFAULT);
+        if (targetMethod != null) {
+            return targetMethod;
+        }
+        return currentTargetMethod(CompilationDirective.JIT);
+    }
+}
