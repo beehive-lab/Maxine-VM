0586726fc6738bffee2f0263f0a6bffb57fcb187
Doug Simon <doug.simon@sun.com>
2008-12-17 18:14:17 -0800

    Merged BytecodeToHCirTranslation in BytecodeTranslation.

---
+++ Tele/src/com/sun/max/tele/object/TeleRuntimeMemoryRegion.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.tele.object;
+
+import com.sun.max.memory.MemoryRegion;
+import com.sun.max.memory.RuntimeMemoryRegion;
+import com.sun.max.tele.TeleVM;
+import com.sun.max.unsafe.Address;
+import com.sun.max.unsafe.Size;
+import com.sun.max.vm.reference.Reference;
+
+/**
+ * Canonical surrogate for objects in the {@link TeleVM} that represent a region of memory.
+ * 
+ * @author Michael Van De Vanter
+ */
+public class TeleRuntimeMemoryRegion extends TeleTupleObject implements MemoryRegion {
+		
+	TeleRuntimeMemoryRegion(TeleVM teleVM, Reference runtimeMemoryRegionReference) {
+		super(teleVM, runtimeMemoryRegionReference);
+	}
+	
+	/**
+	 * Reads from the {@link TeleVM} the start field of the {@link RuntimeMemoryRegion}.
+	 */
+	private Address readStart() {
+	       return teleVM().fields().RuntimeMemoryRegion_start.readWord(reference()).asAddress();       
+	}
+	
+	public Address start() {
+		// No caching for now
+		Address start = readStart();
+		if (start.isZero() && this == teleVM().teleHeapManager().teleBootHeapRegion()) {
+			// Ugly special case:  the start field of the static that defines the boot heap region
+			// is set at zero in the boot image, only set to the real value when the VM starts running.
+			// Lie about it.
+			start = teleVM().bootImageStart();
+		}
+		return start;
+	}
+	
+	/**
+	 * @return whether memory has been allocated yet in the {@link TeleVM} for this region.
+	 */
+	public boolean isAllocated() {
+		return ! start().isZero();
+	}
+
+	/**
+	 * Reads from the {@link TeleVM} the mark field of the {@link RuntimeMemoryRegion}.
+	 */
+	public Address mark() {
+		return teleVM().fields().RuntimeMemoryRegion_mark.readWord(reference()).asAddress();       
+	}
+	
+	/**
+	 * @return how much memory in region has been allocated to objects, {@link Size#zero()) if memory for region not allocated.
+	 */
+	public Size allocatedSize() {
+		if (isAllocated()) {
+			final Address mark = mark();
+			if (!mark.isZero()) {
+				return mark.minus(start()).asSize();
+			}
+		}
+		return Size.zero();
+	}
+	
+	/**
+	 * Reads from the {@link TeleVM} the size field of the {@link RuntimeMemoryRegion}.
+	 */
+	public Size size() {
+		return teleVM().fields().RuntimeMemoryRegion_size.readWord(reference()).asSize();       
+	}
+
+    public Address end() {
+        return start().plus(size());
+    }
+    
+    public String description() {
+    	final Reference descriptionStringReference = teleVM().fields().RuntimeMemoryRegion_description.readReference(reference());
+    	final TeleString teleString = (TeleString) TeleObject.make(teleVM(), descriptionStringReference);
+    	return teleString.getString();
+    }
+   
+
+    public boolean contains(Address address) {
+        return address.greaterEqual(start()) && address.lessThan(end());
+    }
+
+    public boolean overlaps(MemoryRegion memoryRegion) {
+        final Address start = start();
+		final Address end = end();
+		return (start.greaterEqual(memoryRegion.start()) && start.lessThan(memoryRegion.end())) ||
+               (end.greaterEqual(memoryRegion.start()) && end.lessThan(memoryRegion.end()));
+    }
+
+    public boolean sameAs(MemoryRegion otherMemoryRegion) {
+    	  return otherMemoryRegion != null && start().equals(otherMemoryRegion.start()) && size().equals(otherMemoryRegion.size());
+    }
+}
---
+++ VM/src/com/sun/max/vm/heap/TLAB.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.vm.heap;
+
+import com.sun.max.annotate.*;
+import com.sun.max.memory.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.*;
+import com.sun.max.vm.actor.holder.*;
+import com.sun.max.vm.heap.beltway.*;
+
+/**
+ * This class implements a thread local allocation buffer.
+ *
+ * @author Christos Kotselidis
+ */
+public class TLAB extends RuntimeMemoryRegion implements Allocator {
+
+    private Address _endAllocationMark;
+    private Address _previousAllocationMark;
+    private Address _nextTLAB = Address.zero();
+    private Address _startScavengeAddress = Address.zero();
+    private Address _stopScavengeAddress = Address.zero();
+
+    // This variable will be written only during GC allocate TLABS
+    // When 0 that TLAB (space) has not been scavenged yet.
+    // It is also used for synchronization when accessing the linked list
+    private Address _scavenged;
+    private Pointer _scavengedPointer;
+
+    public TLAB() {
+        super();
+        _mark = start();
+    }
+
+    public TLAB(Address start, Size size) {
+        super(start, size);
+        _mark = start();
+
+    }
+
+    public TLAB(Size size) {
+        super(size);
+        _mark = start();
+        setDescription("TLAB");
+    }
+
+    public void initializeTLAB(Address newAddress) {
+        setStart(newAddress);
+        setSize(BeltwayHeapSchemeConfiguration.TLAB_SIZE);
+        _mark = newAddress;
+        if (VMConfiguration.hostOrTarget().debugging()) {
+            _endAllocationMark = end().asPointer().minusWords(4);
+        } else {
+            _endAllocationMark = end().asPointer().minusWords(3);
+
+        }
+        _scavenged.asPointer().bitSet(0);
+        _scavengedPointer = ClassActor.fromJava(TLAB.class).findLocalInstanceFieldActor("_scavenged").pointer(this);
+
+    }
+
+    public void initializeTLAB(Address newAddress, Address allocationMark, Size size) {
+        setStart(newAddress);
+        // Add one word in case of debugging, because we pass the parameter of the start
+        // address without the taking account the debug word
+        setSize(size);
+        _mark = allocationMark;
+        _previousAllocationMark = allocationMark;
+        if (VMConfiguration.hostOrTarget().debugging()) {
+            _endAllocationMark = end().asPointer().minusWords(4);
+        } else {
+            _endAllocationMark = end().asPointer().minusWords(3);
+
+        }
+        _scavenged.asPointer().bitSet(0);
+        _scavengedPointer = ClassActor.fromJava(TLAB.class).findLocalInstanceFieldActor("_scavenged").pointer(this);
+
+    }
+
+    public void undoLastAllocation() {
+        _mark = _previousAllocationMark;
+    }
+
+    @INLINE
+    public final void resetTLAB() {
+        _mark = Address.zero();
+    }
+
+    @INLINE
+    public final void setAllocationMark(Address allocationMark) {
+        _mark = allocationMark;
+    }
+
+    @INLINE
+    public final void setScavengeStart(Address startScavenge) {
+        _startScavengeAddress = startScavenge;
+    }
+
+    @INLINE
+    public final void setScavengeStop(Address stopScavenge) {
+        _stopScavengeAddress = stopScavenge;
+    }
+
+    @INLINE
+    public final  boolean isSet() {
+        return !_mark.isZero();
+    }
+
+    @INLINE
+    public final void unSet() {
+        _mark = Address.zero();
+    }
+
+    @INLINE
+    public final void setNextTLAB(Address tlab) {
+        _nextTLAB = tlab;
+    }
+
+    @INLINE
+    public final Address getNextTLAB() {
+        return _nextTLAB;
+    }
+
+    @INLINE
+    public final Address getScavengeStartAddress() {
+        return _startScavengeAddress;
+    }
+
+    @INLINE
+    public final Address getScavengeStopAddress() {
+        return _stopScavengeAddress;
+    }
+
+    @INLINE
+    public final boolean isScavenged() {
+        return _scavengedPointer.isBitSet(0);
+    }
+
+    @INLINE
+    public final void setScavenged() {
+        _scavengedPointer.setBit(0);
+    }
+
+    @INLINE
+    public  final Pointer getScavengedPointer() {
+        return _scavengedPointer;
+    }
+
+    @INLINE
+    public final Address getScavengedAddress() {
+        return _scavenged;
+    }
+
+    @INLINE
+    public final void setScavengedAddress(Address scavenged) {
+        _scavenged = scavenged;
+    }
+
+    @INLINE
+    @Override
+    public final Pointer allocate(Size size) {
+        Pointer cell;
+        final Pointer oldAllocationMark = _mark.asPointer();
+        if (VMConfiguration.hostOrTarget().debugging()) {
+            cell = oldAllocationMark.plusWords(1);
+        } else {
+            cell = oldAllocationMark;
+        }
+        final Pointer end = cell.plus(size);
+
+        if (end.greaterThan(_endAllocationMark)) {
+            fillTLAB();
+            return Pointer.zero();
+        }
+        _mark = end;
+        _previousAllocationMark = _mark;
+        return cell;
+    }
+
+    @Override
+    @INLINE
+    public final Pointer allocate(RuntimeMemoryRegion space, Size size) {
+        return null;
+    }
+
+    @INLINE
+    public final boolean isFull() {
+        return (getAllocationMark() != end()) ? false : true;
+    }
+
+    @INLINE
+    public final void fillTLAB() {
+        final Size fillingSize = _endAllocationMark.minus(_mark).asPointer().asSize();
+        _endAllocationMark = end();
+        Cell.plantArray(_mark.asPointer().plusWords(1), PrimitiveClassActor.BYTE_ARRAY_CLASS_ACTOR.dynamicHub(), fillingSize.toInt());
+        _mark = end();
+    }
+
+    public Pointer compareAndSwapScavengePointer(Pointer suspectedValue, Pointer newValue) {
+        return _scavengedPointer.compareAndSwapWord(suspectedValue, newValue).asPointer();
+    }
+}
