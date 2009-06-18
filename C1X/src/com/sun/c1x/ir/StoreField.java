495344e3c2e364cc3c53547e566fd3f89a56e1c8
Ben L. Titzer <ben.titzer@sun.com>
2009-06-18 11:21:17 -0700

    CHECKSTYLE!!!!!!!!!!!!!!!!!!!

--- C1X/src/com/sun/c1x/ir/StoreField.java
+++ C1X/src/com/sun/c1x/ir/StoreField.java
@@ -20,11 +20,9 @@
  */
 package com.sun.c1x.ir;
 
-import com.sun.c1x.ci.CiField;
-import com.sun.c1x.value.ValueStack;
-import com.sun.c1x.util.InstructionClosure;
-import com.sun.c1x.util.InstructionVisitor;
-import com.sun.c1x.util.Util;
+import com.sun.c1x.ci.*;
+import com.sun.c1x.util.*;
+import com.sun.c1x.value.*;
 
 /**
  * The <code>StoreField</code> instruction represents a write to a static or instance field.
--- C1X/src/com/sun/c1x/ir/StoreIndexed.java
+++ C1X/src/com/sun/c1x/ir/StoreIndexed.java
@@ -20,12 +20,8 @@
  */
 package com.sun.c1x.ir;
 
-import com.sun.c1x.util.InstructionClosure;
-import com.sun.c1x.util.InstructionVisitor;
-import com.sun.c1x.util.Util;
-import com.sun.c1x.value.BasicType;
-import com.sun.c1x.value.ValueStack;
-import com.sun.c1x.value.ValueType;
+import com.sun.c1x.util.*;
+import com.sun.c1x.value.*;
 
 /**
  * The <code>StoreIndexed</code> instruction represents a write to an array element.
