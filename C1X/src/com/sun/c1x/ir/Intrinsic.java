312d7e13c521be104f0db3edfece65d9caebd726
Michael Van De Vanter <michael.vandevanter@sun.com>
2009-07-01 12:32:10 -0700

    Inspector:  some much-needed refactoring in the two code viewers.

---
+++ C1X/src/com/sun/c1x/ir/Intrinsic.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2009 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.c1x.ir;
+
+import com.sun.c1x.util.InstructionVisitor;
+import com.sun.c1x.util.InstructionClosure;
+import com.sun.c1x.value.ValueStack;
+import com.sun.c1x.value.ValueType;
+import com.sun.c1x.C1XIntrinsic;
+
+/**
+ * The <code>Intrinsic</code> instruction represents a call to a JDK method
+ * that has been made {@linkplain C1XIntrinsic intrinsic}.
+ *
+ * @author Ben L. Titzer
+ * @see C1XIntrinsic
+ */
+public class Intrinsic extends StateSplit {
+
+    final C1XIntrinsic intrinsic;
+    final boolean isStatic;
+    Instruction[] arguments;
+    ValueStack lockStack;
+
+    /**
+     * Creates a new Intrinsic instruction.
+     * @param type the result type of the instruction
+     * @param intrinsic the actual intrinsic
+     * @param arguments the arguments to the call (including the receiver object)
+     * @param isStatic <code>true</code> if this method is static
+     * @param lockStack the lock stack
+     * @param preservesState <code>true</code> if the implementation of this intrinsic preserves register state
+     * @param canTrap <code>true</code> if this intrinsic can cause a trap
+     */
+    public Intrinsic(ValueType type, C1XIntrinsic intrinsic, Instruction[] arguments, boolean isStatic,
+                     ValueStack lockStack, boolean preservesState, boolean canTrap) {
+        super(type);
+        this.intrinsic = intrinsic;
+        this.arguments = arguments;
+        this.lockStack = lockStack;
+        this.isStatic = isStatic;
+        // Preserves state means that the intrinsic preserves register state across all cases,
+        // including slow cases--even if it causes a trap. If so, it can still be a candidate
+        // for load elimination and common subexpression elimination
+        initFlag(Flag.PreservesState, preservesState);
+        initFlag(Flag.CanTrap, canTrap);
+        initFlag(Flag.PinStateSplitConstructor, canTrap);
+    }
+
+    /**
+     * Gets the intrinsic represented by this instruction.
+     * @return the intrinsic
+     */
+    public C1XIntrinsic intrinsic() {
+        return intrinsic;
+    }
+
+    /**
+     * Gets the list of instructions that produce input for this instruction.
+     * @return the list of instructions that produce input
+     */
+    public Instruction[] arguments() {
+        return arguments;
+    }
+
+    /**
+     * Gets the lock stack for this instruction.
+     * @return the lock stack
+     */
+    public ValueStack lockStack() {
+        return lockStack;
+    }
+
+    /**
+     * Checks whether this intrinsic has a receiver object.
+     * @return <code>true</code> if this intrinsic has a receiver object
+     */
+    public boolean hasReceiver() {
+        return !isStatic;
+    }
+
+    /**
+     * Gets the instruction which produces the receiver object for this intrinsic.
+     * @return the instruction producing the receiver object
+     */
+    public Instruction receiver() {
+        assert !isStatic;
+        return arguments[0];
+    }
+
+    /**
+     * Checks whether this intrinsic preserves the state of registers across all cases.
+     * @return <code>true</code> if this intrinsic always preserves register state
+     */
+    public boolean preservesState() {
+        return checkFlag(Flag.PreservesState);
+    }
+
+    /**
+     * Checks whether this intrinsic can cause a trap.
+     * @return <code>true</code> if this intrinsic can cause a trap
+     */
+    @Override
+    public boolean canTrap() {
+        return checkFlag(Flag.CanTrap);
+    }
+
+    /**
+     * Iterates over the state values of this instruction.
+     * @param closure the closure to apply
+     */
+    @Override
+    public void stateValuesDo(InstructionClosure closure) {
+        if (lockStack != null) {
+            lockStack.valuesDo(closure);
+        }
+    }
+
+    /**
+     * Iterates over the input values to this instruction.
+     * @param closure the closure to apply
+     */
+    @Override
+    public void inputValuesDo(InstructionClosure closure) {
+        for (int i = 0; i < arguments.length; i++) {
+            arguments[i] = closure.apply(arguments[i]);
+        }
+    }
+
+    /**
+     * Implements this instruction's half of the visitor pattern.
+     * @param v the visitor to accept
+     */
+    @Override
+    public void accept(InstructionVisitor v) {
+        v.visitIntrinsic(this);
+    }
+}
---
+++ Tele/src/com/sun/max/tele/object/TeleObject.java
@@ -0,0 +1,405 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.tele.object;
+
+import java.lang.reflect.*;
+import java.util.*;
+
+import com.sun.max.jdwp.vm.proxy.*;
+import com.sun.max.lang.*;
+import com.sun.max.program.*;
+import com.sun.max.tele.*;
+import com.sun.max.tele.reference.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.actor.holder.*;
+import com.sun.max.vm.actor.member.*;
+import com.sun.max.vm.reference.*;
+import com.sun.max.vm.type.*;
+import com.sun.max.vm.value.*;
+
+/**
+ * Canonical surrogate for a heap object in the {@link TeleVM}.
+ *
+ * This class and its subclasses play the role of typed wrappers for References to heap objects in the {@link TeleVM},
+ * encapsulating implementation details for working with those objects remotely.
+ *
+ * @author Michael Van De Vanter
+ */
+public abstract class TeleObject extends AbstractTeleVMHolder implements ObjectProvider {
+
+    /**
+     * Identification for the three low-level Maxine heap objects implementations upon which all objects are implemented.
+     * @see com.sun.max.vm.object.ObjectAccess
+     */
+    public enum ObjectKind {
+
+        /**
+         * A Maxine implementation object that represents a Java array.
+         * @see com.sun.max.vm.object.ArrayAccess
+         */
+        ARRAY,
+
+        /**
+         * A Maxine implementation object that represents a Java object instance:  a collection of name/value pairs.
+         * @see com.sun.max.vm.object.TupleAccess
+         */
+        TUPLE,
+
+        /**
+         * A special Maxine implementation object used to implement {@link Hub}s.
+         * These represent special objects that cannot be described in ordinary Java;
+         * they have both fields (as in an object instance) and a collection of
+         * specialized arrays.
+         * @see com.sun.max.vm.object.Hybrid
+         */
+        HYBRID;
+    }
+    /**
+     * Controls tracing for object copying.
+     */
+    protected static final int COPY_TRACE_VALUE = 2;
+
+    /**
+     * The factory method {@link TeleObjectFactory#make(Reference)} ensures synchronized TeleObjects creation.
+     */
+    protected TeleObject(TeleVM teleVM, Reference reference) {
+        super(teleVM);
+        this.reference = (TeleReference) reference;
+        oid = this.reference.makeOID();
+    }
+
+    protected void refresh(long processEpoch) {
+    }
+
+    private final TeleReference reference;
+
+    /**
+     * @return canonical reference to this object in the {@link TeleVM}
+     */
+    public TeleReference reference() {
+        return reference;
+    }
+
+    /**
+     * @return current absolute location of the beginning of the object's memory allocation in the {@link TeleVM},
+     * subject to relocation by GC.
+     */
+    public Pointer getCurrentCell() {
+        return teleVM().referenceToCell(reference);
+    }
+
+    /**
+     * @return the current size of memory allocated for the object in the {@link TeleVM}.
+     */
+    public Size getCurrentSize() {
+        return classActorForType().dynamicTupleSize();
+    }
+
+    /**
+     * @return current absolute location of the object's origin (not necessarily beginning of memory)
+     *  in the {@link TeleVM}, subject to relocation by GC
+     */
+    public Pointer getCurrentOrigin() {
+        return reference.toOrigin();
+    }
+
+    /**
+     * @return to which of the Maxine heap object representations does this surrogate refer?
+     */
+    public abstract ObjectKind getObjectKind();
+
+    private final long oid;
+
+    /**
+     * @return a number that uniquely identifies this object in the {@link TeleVM} for the duration of the inspection
+     */
+    public long getOID() {
+        return oid;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().toString() + "<" + oid + ">";
+    }
+
+    private TeleHub teleHub = null;
+
+    /**
+     * @return a short string describing the role played by this object if it is of special interest in the Maxine
+     *         implementation, null if any other kind of object.
+     */
+    public String maxineRole() {
+        return null;
+    }
+
+    /**
+     * @return an extremely short, abbreviated version of the string {@link #maxineRole()}, describing the role played
+     *         by this object in just a few characters.
+     */
+    public String maxineTerseRole() {
+        return maxineRole();
+    }
+
+    /**
+     * @return the local surrogate for the Hub of this object
+     */
+    public TeleHub getTeleHub() {
+        if (teleHub == null) {
+            final Reference hubReference = teleVM().wordToReference(teleVM().layoutScheme().generalLayout.readHubReferenceAsWord(reference));
+            teleHub = (TeleHub) teleVM().makeTeleObject(hubReference);
+        }
+        return teleHub;
+    }
+
+    /**
+     * @return the "misc" word from the header of this object in the teleVM
+     */
+    public Word getMiscWord() {
+        return teleVM().layoutScheme().generalLayout.readMisc(reference);
+    }
+
+    /**
+     * @return local {@link ClassActor}, equivalent to the one in the teleVM that describes the type
+     * of this object in the {@link TeleVM}.
+     * Note that in the singular instance of {@link StaticTuple} this does not correspond to the actual type of the
+     * object, which is an exceptional Maxine object that has no ordinary Java type; it returns in this case
+     * the type of the class that the tuple helps implement.
+     */
+    public ClassActor classActorForType() {
+        return getTeleHub().getTeleClassActor().classActor();
+    }
+
+    /**
+     * return local surrogate for the{@link ClassMethodActor} associated with this object in the {@link TeleVM}, either
+     * because it is a {@link ClassMethodActor} or because it is a class closely associated with a method that refers to
+     * a {@link ClassMethodActor}. Null otherwise.
+     */
+    public TeleClassMethodActor getTeleClassMethodActorForObject() {
+        return null;
+    }
+
+    /**
+     *  Gets the fields for either a tuple or hybrid object, returns empty set for arrays.
+     *  Returns static fields in the special case of a {@link StaticTuple} object.
+     */
+    public Set<FieldActor> getFieldActors() {
+        final Set<FieldActor> instanceFieldActors = new HashSet<FieldActor>();
+        collectInstanceFieldActors(classActorForType(), instanceFieldActors);
+        return instanceFieldActors;
+    }
+
+    private void collectInstanceFieldActors(ClassActor classActor, Set<FieldActor> instanceFieldActors) {
+        if (classActor != null) {
+            for (FieldActor fieldActor : classActor.localInstanceFieldActors()) {
+                instanceFieldActors.add(fieldActor);
+            }
+            collectInstanceFieldActors(classActor.superClassActor, instanceFieldActors);
+        }
+    }
+
+    /**
+     * @param fieldActor local {@link FieldActor}, part of the {@link ClassActor} for the type of this object, that
+     *            describes a field in this object in the {@link TeleVM}
+     * @return contents of the designated field in this object in the {@link TeleVM}
+     */
+    public abstract Value readFieldValue(FieldActor fieldActor);
+
+    /**
+     * @return a shallow copy of the object in the teleVM, with any references in it nulled out
+     */
+    public abstract Object shallowCopy();
+
+    /**
+     * Filter for pruning the object graph copied during a {@linkplain TeleObject#deepCopy}.
+     */
+    protected static interface FieldIncludeChecker {
+
+        /**
+         * Determines if a given field is to be traversed and copied during a deep copy.
+         *
+         * @param level  the depth of the sub-graph currently being copied
+         * @param fieldActor  the field to be queried
+         */
+        boolean include(int level, FieldActor fieldActor);
+    }
+
+    protected static final class DeepCopyContext {
+
+        private int level = 0;
+        private final FieldIncludeChecker fieldIncludeChecker;
+        private final Map<TeleObject, Object> teleObjectToObject = new HashMap<TeleObject, Object>();
+
+        private static final FieldIncludeChecker defaultIFieldIncludeChecker = new FieldIncludeChecker() {
+            public boolean include(int level, FieldActor fieldActor) {
+                return true;
+            }
+        };
+
+        /**
+         * Creates a context for a deep copy.
+         */
+        protected DeepCopyContext() {
+            this.fieldIncludeChecker = defaultIFieldIncludeChecker;
+        }
+
+        /**
+         * Creates a context for a deep copy in which a filter suppresses copying of specified fields.
+         */
+        protected DeepCopyContext(FieldIncludeChecker fieldIncludeChecker) {
+            this.fieldIncludeChecker = fieldIncludeChecker;
+        }
+
+        /**
+         * @return the depth of the object graph currently being copied
+         */
+        protected int level() {
+            return level;
+        }
+
+        /**
+         * Registers a newly copied object in the context to avoid duplication.
+         */
+        protected void register(TeleObject teleObject, Object newObject) {
+            teleObjectToObject.put(teleObject, newObject);
+        }
+
+        /**
+         * @return whether the specified object field at this level of the object graph should be copied.
+         */
+        protected boolean include(int level, FieldActor fieldActor) {
+            return fieldIncludeChecker.include(level, fieldActor);
+        }
+
+    }
+
+    /**
+     * @return produces a deep copy of an object as part of
+     * a larger deep copy in which this particular object may have
+     * already been copied.
+     */
+    protected final Object makeDeepCopy(DeepCopyContext context) {
+        Object newObject = context.teleObjectToObject.get(this);
+        if (newObject == null) {
+            context.level++;
+            newObject = createDeepCopy(context);
+            context.register(this, newObject);
+            context.level--;
+        }
+        return newObject;
+    }
+
+    /**
+     * @return creates a local deep copy of the object, using Maxine-specific shortcuts when
+     * possible to produce a local equivalent without copying.
+     * Implementations that copy recursively must call {@link TeleObject#makeDeepCopy(DeepCopyContext)},
+     * and must register newly allocated objects before doing so.  This will result in redundant registrations
+     * in those cases.
+     */
+    protected abstract Object createDeepCopy(DeepCopyContext context);
+
+    /**
+     * @return a best effort deep copy - with certain substitutions
+     */
+    public final Object deepCopy() {
+        Trace.begin(COPY_TRACE_VALUE, "Deep copying from VM: " + this);
+        final Object objectCopy = makeDeepCopy(new DeepCopyContext());
+        Trace.end(COPY_TRACE_VALUE, "Deep copying from VM: " + this);
+        return objectCopy;
+    }
+
+    /**
+     * @return a best effort deep copy - with certain substitutions, and with
+     * certain specified field omissions.
+     */
+    public final Object deepCopy(FieldIncludeChecker fieldIncludeChecker) {
+        Trace.begin(COPY_TRACE_VALUE, "Deep copying from VM: " + this);
+        final Object objectCopy = makeDeepCopy(new DeepCopyContext(fieldIncludeChecker));
+        Trace.end(COPY_TRACE_VALUE, "Deep copying from VM: " + this);
+        return objectCopy;
+    }
+
+    /**
+     * Updates the field of an object or class from the {@link TeleVM}.
+     *
+     * @param teleObject surrogate for a tuple in the {@link TeleVM}. This will be a static tuple if the field is static.
+     * @param tuple the local object to be updated in the host VM. This value is ignored if the field is static.
+     * @param fieldActor the field to be copied/updated
+     */
+    protected static final void copyField(DeepCopyContext context, final TeleObject teleObject, final Object newTuple, final FieldActor fieldActor) {
+        if (context.include(context.level(), fieldActor)) {
+            if (!fieldActor.isInjected()) {
+                final Field field = fieldActor.toJava();
+                field.setAccessible(true);
+                try {
+                    final Value value = teleObject.readFieldValue(fieldActor);
+                    final Object newJavaValue;
+                    if (fieldActor.kind == Kind.REFERENCE) {
+                        final TeleObject teleFieldReferenceObject = teleObject.teleVM().makeTeleObject(value.asReference());
+                        if (teleFieldReferenceObject == null) {
+                            newJavaValue = null;
+                        } else {
+                            newJavaValue = teleFieldReferenceObject.makeDeepCopy(context);
+                        }
+                    } else if (fieldActor.kind == Kind.WORD) {
+                        final Class<Class< ? extends Word>> type = null;
+                        final Class< ? extends Word> wordType = StaticLoophole.cast(type, fieldActor.toJava().getType());
+                        newJavaValue = value.asWord().as(wordType);
+                    } else {
+                        newJavaValue = value.asBoxedJavaValue();
+                    }
+                    field.set(newTuple, newJavaValue);
+                } catch (IllegalAccessException illegalAccessException) {
+                    ProgramError.unexpected("could not access field: " + field, illegalAccessException);
+                }
+            }
+        }
+    }
+
+    /**
+     * Updates the static fields of a specified local class from the {@link TeleVM}.
+     */
+    public static void copyStaticFields(TeleVM teleVM, Class javaClass) {
+        final ClassActor classActor = ClassActor.fromJava(javaClass);
+        final TeleClassActor teleClassActor = teleVM.findTeleClassActor(javaClass);
+        final TeleStaticTuple teleStaticTuple = teleClassActor.getTeleStaticTuple();
+
+        final String classMessage = "Copying static fields of " + javaClass + " from VM";
+        Trace.begin(COPY_TRACE_VALUE, classMessage);
+        try {
+            for (FieldActor fieldActor : classActor.localStaticFieldActors()) {
+                final String fieldMessage = "Copying static field " + fieldActor.format("%t %n") + " from VM";
+                Trace.begin(COPY_TRACE_VALUE, fieldMessage);
+                copyField(new DeepCopyContext(), teleStaticTuple, null, fieldActor);
+                Trace.end(COPY_TRACE_VALUE, fieldMessage);
+            }
+        } finally {
+            Trace.end(COPY_TRACE_VALUE, classMessage);
+        }
+    }
+
+    public Reference getReference() {
+        return this.reference();
+    }
+
+    public ReferenceTypeProvider getReferenceType() {
+        return teleVM().findTeleClassActor(classActorForType().typeDescriptor);
+    }
+}
