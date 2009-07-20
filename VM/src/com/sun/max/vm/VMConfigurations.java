3bad9f38a860de131c7c09d6714be2d1b6e45ac2
Doug Simon <doug.simon@sun.com>
2009-07-20 13:32:20 -0700

    Removed valiant attempt at direct threaded interpreter implementation.

--- VM/src/com/sun/max/vm/VMConfigurations.java
+++ VM/src/com/sun/max/vm/VMConfigurations.java
@@ -59,15 +59,6 @@ public final class VMConfigurations {
         }
     }
 
-    private static VMPackage defaultInterpreterPackage(Platform platform) {
-        switch (platform.processorKind.instructionSet) {
-            case AMD64:
-                return new com.sun.max.vm.interpret.dt.amd64.Package();
-            default:
-                throw FatalError.unimplemented();
-        }
-    }
-
     private static VMPackage defaultTargetABIsPackage(Platform platform) {
         switch (platform.processorKind.instructionSet) {
             case AMD64:
@@ -89,14 +80,14 @@ public final class VMConfigurations {
 
     public static VMConfiguration createStandardJit(BuildLevel buildLevel, Platform platform) {
         return new VMConfiguration(buildLevel, platform, new com.sun.max.vm.grip.direct.Package(), defaultReferenceScheme(), new com.sun.max.vm.layout.ohm.Package(),
-                        defaultHeapPackage(), new com.sun.max.vm.monitor.modal.schemes.thin_inflated.Package(), defaultCompilerPackage(platform), defaultJitCompilerPackage(platform), new com.sun.max.vm.interpret.empty.Package(),
-                        new com.sun.max.vm.trampoline.template.Package(), defaultTargetABIsPackage(platform), new com.sun.max.vm.run.java.Package());
+                        defaultHeapPackage(), new com.sun.max.vm.monitor.modal.schemes.thin_inflated.Package(), defaultCompilerPackage(platform), defaultJitCompilerPackage(platform), new com.sun.max.vm.trampoline.template.Package(),
+                        defaultTargetABIsPackage(platform), new com.sun.max.vm.run.java.Package());
     }
 
     public static VMConfiguration createStandardInterpreter(BuildLevel buildLevel, Platform platform) {
         return new VMConfiguration(buildLevel, platform, new com.sun.max.vm.grip.direct.Package(), defaultReferenceScheme(), new com.sun.max.vm.layout.ohm.Package(),
-                        defaultHeapPackage(), new com.sun.max.vm.monitor.modal.schemes.thin_inflated.Package(), defaultCompilerPackage(platform), defaultJitCompilerPackage(platform), defaultInterpreterPackage(platform),
-                        new com.sun.max.vm.trampoline.template.Package(), defaultTargetABIsPackage(platform), new com.sun.max.vm.run.java.Package());
+                        defaultHeapPackage(), new com.sun.max.vm.monitor.modal.schemes.thin_inflated.Package(), defaultCompilerPackage(platform), defaultJitCompilerPackage(platform), new com.sun.max.vm.trampoline.template.Package(),
+                        defaultTargetABIsPackage(platform), new com.sun.max.vm.run.java.Package());
     }
 
     public static VMConfiguration createStandard(BuildLevel buildLevel, Platform platform) {
@@ -105,13 +96,13 @@ public final class VMConfigurations {
 
     public static VMConfiguration createStandard(BuildLevel buildLevel, Platform platform, VMPackage compilerPackage) {
         return new VMConfiguration(buildLevel, platform, new com.sun.max.vm.grip.direct.Package(), defaultReferenceScheme(), new com.sun.max.vm.layout.ohm.Package(),
-                        defaultHeapPackage(), new com.sun.max.vm.monitor.modal.schemes.thin_inflated.Package(), compilerPackage, null, new com.sun.max.vm.interpret.empty.Package(), new com.sun.max.vm.trampoline.template.Package(),
-                        defaultTargetABIsPackage(platform), new com.sun.max.vm.run.java.Package());
+                        defaultHeapPackage(), new com.sun.max.vm.monitor.modal.schemes.thin_inflated.Package(), compilerPackage, null, new com.sun.max.vm.trampoline.template.Package(), defaultTargetABIsPackage(platform),
+                        new com.sun.max.vm.run.java.Package());
     }
 
     public static VMConfiguration createPrototype(BuildLevel buildLevel, Platform platform) {
         return new VMConfiguration(buildLevel, platform, new com.sun.max.vm.grip.prototype.Package(), new com.sun.max.vm.reference.prototype.Package(), new com.sun.max.vm.layout.ohm.Package(),
-                        defaultHeapPackage(), new com.sun.max.vm.monitor.prototype.Package(), new com.sun.max.vm.compiler.prototype.Package(), null, new com.sun.max.vm.interpret.empty.Package(), new com.sun.max.vm.trampoline.template.Package(),
-                        defaultTargetABIsPackage(platform), new com.sun.max.vm.run.java.Package());
+                        defaultHeapPackage(), new com.sun.max.vm.monitor.prototype.Package(), new com.sun.max.vm.compiler.prototype.Package(), null, new com.sun.max.vm.trampoline.template.Package(), defaultTargetABIsPackage(platform),
+                        new com.sun.max.vm.run.java.Package());
     }
 }
