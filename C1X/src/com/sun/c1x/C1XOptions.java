e6f87afe38dd4150a8989c4e0fcbc0f1636c26b4
Ben L. Titzer <ben.titzer@sun.com>
2009-07-21 14:44:03 -0700

    Fixed global value numbering, simplified ValueMap for local value numbering; cleaned up C1XOptions; fixed optimization bugs

--- C1X/src/com/sun/c1x/C1XOptions.java
+++ C1X/src/com/sun/c1x/C1XOptions.java
@@ -56,11 +56,11 @@ public class C1XOptions {
     public static boolean TestPatching                       = ____;
     public static boolean TestSlowPath                       = ____;
     public static boolean PrintInitialBlockList              = ____;
-    public static boolean PrintIR                            = TRUE;
+    public static boolean PrintIR                            = ____;
     public static boolean PrintCFGToFile                     = ____;
     public static boolean PrintMetrics                       = ____;
     public static boolean PrintCFG                           = ____;
-    public static boolean PrintCompilation                   = TRUE;
+    public static boolean PrintCompilation                   = ____;
     public static boolean PrintExceptionHandlers             = ____;
 
     // canonicalizer settings
@@ -128,10 +128,11 @@ public class C1XOptions {
 
     // Linear scan settings
     public static boolean StressLinearScan                   = ____;
-    public static int     TraceLinearScanLevel               = 4;
+    public static int     TraceLinearScanLevel               = 0;
 
     // LIR settings
-    public static boolean PrintIRWithLIR                     = TRUE;
+    public static boolean GenerateLIR                        = ____;
+    public static boolean PrintIRWithLIR                     = ____;
     public static boolean LIRFillDelaySlots                  = ____;
     public static boolean LIRTraceExecution                  = ____;
     public static boolean TwoOperandLIRForm                  = ____;
@@ -143,7 +144,7 @@ public class C1XOptions {
     public static boolean GenerateCompilerNullChecks         = ____;
     public static boolean UseTableRanges                     = ____;
     public static boolean DetailedAsserts                    = TRUE;
-    public static boolean PrintLIR                           = TRUE;
+    public static boolean PrintLIR                           = ____;
     public static boolean Verbose                            = ____;
     public static boolean LIRTracePeephole                   = ____;
 
@@ -166,6 +167,7 @@ public class C1XOptions {
     public static int     StackShadowPages                   = 3;
 
     // Assembler settings
+    public static boolean GenerateAssembly                   = ____;
     public static boolean CommentedAssembly                  = ____;
     public static boolean PrintLIRWithAssembly               = ____;
     public static boolean VerifyOopMaps                      = ____;
