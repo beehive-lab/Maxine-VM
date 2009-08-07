51272100c30d2ba98f00cf1ad61392d256fa85ec
Michael Van De Vanter <michael.vandevanter@sun.com>
2009-08-07 12:44:16 -0700

    Inspector:  a bit more work on the new MemoryWordsInspector (not yet live).

--- Inspector/src/com/sun/max/ins/memory/MemoryWordsViewPreferences.java
+++ Inspector/src/com/sun/max/ins/memory/MemoryWordsViewPreferences.java
@@ -20,8 +20,16 @@
  */
 package com.sun.max.ins.memory;
 
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
 import com.sun.max.ins.*;
+import com.sun.max.ins.InspectionSettings.*;
 import com.sun.max.ins.gui.*;
+import com.sun.max.ins.memory.MemoryWordsInspector.*;
+import com.sun.max.program.option.*;
 
 
 /**
@@ -29,7 +37,7 @@ import com.sun.max.ins.gui.*;
  *
  * @author Michael Van De Vanter
   */
-public class MemoryWordsViewPreferences extends com.sun.max.ins.gui.TableColumnVisibilityPreferences<MemoryWordsColumnKind> {
+public class MemoryWordsViewPreferences extends TableColumnVisibilityPreferences<MemoryWordsColumnKind> {
 
     private static MemoryWordsViewPreferences globalPreferences;
 
@@ -43,17 +51,26 @@ public class MemoryWordsViewPreferences extends com.sun.max.ins.gui.TableColumnV
         return globalPreferences;
     }
 
+    private static final String MEMORY_WORDS_MODE_PREFERENCE = "memoryWordsNavigationMode";
+
+    private NavMode navigationMode;
+
+
+    private MemoryWordsViewPreferences(Inspection inspection) {
+        super(inspection, "memoryWordsViewPrefs", MemoryWordsColumnKind.class, MemoryWordsColumnKind.VALUES);
+        final OptionTypes.EnumType<NavMode> optionType = new OptionTypes.EnumType<NavMode>(NavMode.class);
+        navigationMode = inspection.settings().get(saveSettingsListener, MEMORY_WORDS_MODE_PREFERENCE, optionType, NavMode.WORD);
+    }
+
     /**
      * A per-instance set of view preferences, initialized to the global preferences.
      * @param defaultPreferences the global defaults for this kind of view
      */
-    public MemoryWordsViewPreferences(TableColumnVisibilityPreferences<MemoryWordsColumnKind> defaultPreferences) {
-        super(defaultPreferences);
+    public MemoryWordsViewPreferences(MemoryWordsViewPreferences globalPreferences) {
+        super(globalPreferences);
+        navigationMode = globalPreferences.navigationMode;
     }
 
-    private MemoryWordsViewPreferences(Inspection inspection) {
-        super(inspection, "memoryWordsViewPrefs", MemoryWordsColumnKind.class, MemoryWordsColumnKind.VALUES);
-    }
 
     @Override
     protected boolean canBeMadeInvisible(MemoryWordsColumnKind columnType) {
@@ -69,4 +86,63 @@ public class MemoryWordsViewPreferences extends com.sun.max.ins.gui.TableColumnV
     protected String label(MemoryWordsColumnKind columnType) {
         return columnType.label();
     }
+
+    public NavMode navigationMode() {
+        return navigationMode;
+    }
+
+    public void setNavigationMode(NavMode navigationMode) {
+        this.navigationMode = navigationMode;
+        inspection().settings().save();
+    }
+
+    @Override
+    protected void saveSettings(SaveSettingsEvent saveSettingsEvent) {
+        super.saveSettings(saveSettingsEvent);
+        saveSettingsEvent.save(MEMORY_WORDS_MODE_PREFERENCE, navigationMode.name());
+    }
+
+    @Override
+    public JPanel getPanel() {
+        final JRadioButton[] radioButtons = new JRadioButton[NavMode.VALUES.length()];
+        final ButtonGroup group = new ButtonGroup();
+
+        final ItemListener itemListener = new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                final JRadioButton button = (JRadioButton) e.getSource();
+                if (button.isSelected()) {
+                    for (int index = 0; index < radioButtons.length; index++) {
+                        if (radioButtons[index] == button) {
+                            setNavigationMode(NavMode.VALUES.get(index));
+                            inspection().settings().save();
+                            break;
+                        }
+                    }
+                }
+            }
+        };
+
+
+        final JPanel optionsPane = new InspectorPanel(inspection());
+        final TextLabel modeLabel = new TextLabel(inspection(), "Navigate by:  ");
+        modeLabel.setToolTipText("Configures the behavior of navigation instructions");
+        optionsPane.add(modeLabel);
+
+        for (NavMode mode : NavMode.VALUES) {
+            final JRadioButton radioButton = new InspectorRadioButton(inspection(), mode.label(), mode.description());
+            radioButton.setSelected(mode == navigationMode);
+            radioButton.addItemListener(itemListener);
+            radioButtons[mode.ordinal()] = radioButton;
+            group.add(radioButton);
+            optionsPane.add(radioButton);
+        }
+
+
+        final JPanel panel = new InspectorPanel(inspection(), new BorderLayout());
+        panel.add(super.getPanel(), BorderLayout.NORTH);
+        panel.add(optionsPane, BorderLayout.SOUTH);
+        return panel;
+    }
+
+
 }
