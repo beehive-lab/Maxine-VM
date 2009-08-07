c275ca5e28216f31442301d14300d4be4ca27e8d
ldayne <ldayne@localhost>
2009-08-07 10:02:08 +0200

    Fixed spelling in method name. Made TLAB VmThreadLocal variable final.

---
+++ VM/src/com/sun/max/vm/heap/BootHeapRegion.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.vm.heap;
+
+import java.lang.ref.*;
+
+import com.sun.max.annotate.*;
+import com.sun.max.lang.*;
+import com.sun.max.program.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.*;
+import com.sun.max.vm.grip.*;
+import com.sun.max.vm.object.*;
+
+/**
+ * The special region describing the heap in the boot image.
+ *
+ * @author Doug Simon
+ */
+public class BootHeapRegion extends LinearAllocatorHeapRegion {
+
+    private byte[] referenceMapBytes;
+
+    private Pointer referenceMap;
+
+    private Reference[] specialReferences;
+
+    public BootHeapRegion(Address start, Size size, String description) {
+        super(start, size, description);
+    }
+
+    @PROTOTYPE_ONLY
+    public void init(byte[] refMap, Reference[] specialRefs) {
+        ProgramError.check(Address.fromInt(refMap.length).isWordAligned(), "Boot heap reference map must have word-aligned size");
+        this.referenceMapBytes = refMap;
+        this.specialReferences = specialRefs;
+    }
+
+    @Override
+    public void visitReferences(PointerIndexVisitor pointerIndexVisitor) {
+        if (referenceMap.isZero()) {
+            referenceMap = ArrayAccess.elementPointer(referenceMapBytes, 0);
+        }
+
+        final Pointer refMap = referenceMap;
+        final int referenceMapWords = Unsigned.idiv(referenceMapBytes.length, Word.size());
+        if (Heap.traceRootScanning()) {
+            Log.print("Scanning boot heap: start=");
+            Log.print(start());
+            Log.print(", end=");
+            Log.print(end());
+            Log.print(", mutable references end=");
+            Log.println(start().plus(referenceMapWords * Word.size()));
+        }
+
+        if (Heap.traceRootScanning()) {
+            scanReferenceMap(pointerIndexVisitor, refMap, referenceMapWords, true);
+        } else {
+            scanReferenceMap(pointerIndexVisitor, refMap, referenceMapWords, false);
+        }
+
+        for (Reference specialReference : specialReferences) {
+            SpecialReferenceManager.discoverSpecialReference(Grip.fromJava(specialReference));
+        }
+    }
+}
+
---
+++ VM/src/com/sun/max/vm/heap/beltway/BeltwayCollector.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.vm.heap.beltway;
+
+import com.sun.max.annotate.*;
+import com.sun.max.vm.*;
+import com.sun.max.vm.heap.*;
+import com.sun.max.vm.monitor.*;
+import com.sun.max.vm.tele.*;
+
+/**
+ *
+ * @author Laurent Daynes
+ * @author Christos Kotselidis
+ */
+
+public class BeltwayCollector {
+
+    @CONSTANT_WHEN_NOT_ZERO
+    protected BeltwayHeapScheme heapScheme;
+
+    @CONSTANT_WHEN_NOT_ZERO
+    protected MonitorScheme monitorScheme;
+
+    protected long numCollections;
+
+    protected final String collectorName;
+
+    public BeltwayCollector(String name) {
+        numCollections = 0;
+        collectorName = name;
+    }
+
+    /**
+     * Prototyping time initialization.
+     * @param heapScheme
+     */
+    @PROTOTYPE_ONLY
+    public void initialize(BeltwayHeapScheme heapScheme) {
+        // Initialize useful short cuts.
+        this.heapScheme = heapScheme;
+        monitorScheme = VMConfiguration.target().monitorScheme();
+    }
+
+    public BeltwayHeapScheme getBeltwayHeapScheme() {
+        return heapScheme;
+    }
+
+    /**
+     * Verifies that all the references from a belt are valid, i.e., points to used area of the heap.
+     * @param belt
+     */
+    protected void verifyBelt(Belt belt) {
+        heapScheme.heapVerifier.verifyHeap(belt.start(), belt.getAllocationMark(), heapScheme.getBeltManager().getApplicationHeap());
+    }
+
+    protected void prologue() {
+        numCollections++;
+        heapScheme.resetTLABs();
+        InspectableHeapInfo.beforeGarbageCollection();
+
+        if (Heap.verbose()) {
+            Log.print(collectorName);
+            Log.print(" Collection: ");
+            Log.println(numCollections);
+        }
+    }
+
+    protected void epilogue() {
+        if (Heap.verbose()) {
+            Log.print("Finished ");
+            Log.print(collectorName);
+            Log.print(" Collection: ");
+            Log.println(numCollections);
+        }
+        InspectableHeapInfo.afterGarbageCollection();
+    }
+
+    /**
+     * Evacuate remaining objects of the "from" belt reachable from the "to" belt.
+     */
+    protected void evacuateFollowers(Belt from, Belt to) {
+        if (Heap.verbose()) {
+            Log.println("Evacuate Followers");
+        }
+        heapScheme.evacuate(from, to);
+    }
+
+    protected void printBeltInfo(String beltName, Belt belt) {
+        Log.print(beltName);
+        Log.print(" Start: ");
+        Log.println(belt.start());
+        Log.print(beltName);
+        Log.print(" Mark: ");
+        Log.println(belt.getAllocationMark());
+        Log.print(beltName);
+        Log.print(" End: ");
+        Log.println(belt.end());
+    }
+
+}
