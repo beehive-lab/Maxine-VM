bd9cadc547a5749f6083a5726ee9a038b25d3896
Doug Simon <doug.simon@sun.com>
2009-09-09 13:41:00 -0700

    Re-ordered declaration of some flags in Actor.java to more clearly see the remaining available flag values.

---
+++ CRI/src/com/sun/c1x/ci/CiArchitecture.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2009 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.c1x.ci;
+
+
+/**
+ * The <code>Architecture</code> class represents a CPU architecture that is supported by
+ * the backend of C1X.
+ *
+ * @author Ben L. Titzer
+ */
+public abstract class CiArchitecture {
+
+    public static enum BitOrdering{
+        LittleEndian,
+        BigEndian
+    }
+    /**
+     * Represents the natural size of words (typically registers and pointers) of this architecture, in bytes.
+     */
+    public final int wordSize;
+    public final int bitsPerWord;
+    public final int logBytesPerInt;
+    public final String backend;
+    public final int loWordOffsetInBytes;
+    public final int hiWordOffsetInBytes;
+    public final int stackBias = 0;
+    public final CiRegister[] registers;
+    public final String name;
+    public final BitOrdering bitOrdering;
+    public final int framePadding;
+    public final int nativeCallDisplacementOffset;
+    public final int nativeMoveConstInstructionSize;
+
+    public static CiArchitecture findArchitecture(String name) {
+        // load and instantiate the backend via reflection
+        String className = "com.sun.c1x.target." + name.toUpperCase();
+        try {
+            Class<?> javaClass = Class.forName(className);
+            return (CiArchitecture) javaClass.newInstance();
+        } catch (InstantiationException e) {
+            throw new Error("could not instantiate architecture class: " + className);
+        } catch (IllegalAccessException e) {
+            throw new Error("could not access architecture class: " + className);
+        } catch (ClassNotFoundException e) {
+            throw new Error("could not find architecture class: " + className);
+        }
+    }
+
+    protected CiArchitecture(String name, int wordSize, String backend, BitOrdering bitOrdering, CiRegister[] registers, final int framePadding, final int nativeCallDisplacementOffset, final int nativeMoveConstInstructionSize) {
+        this.name = name;
+        this.registers = registers;
+        this.wordSize = wordSize;
+        this.backend = backend;
+        this.bitsPerWord = wordSize * 8;
+        this.logBytesPerInt = (int) (Math.log(wordSize));
+        this.bitOrdering = bitOrdering;
+        this.framePadding = framePadding;
+        this.nativeCallDisplacementOffset = nativeCallDisplacementOffset;
+        this.nativeMoveConstInstructionSize = nativeMoveConstInstructionSize;
+        switch (bitOrdering) {
+            case LittleEndian:
+                loWordOffsetInBytes = 0;
+                hiWordOffsetInBytes = wordSize;
+                break;
+            case BigEndian:
+                loWordOffsetInBytes = wordSize;
+                hiWordOffsetInBytes = 0;
+                break;
+            default:
+			throw new Error("Invalid bitordering!");
+        }
+    }
+
+    /**
+     * Converts this architecture to a string.
+     * @return the string representation of this architecture
+     */
+    @Override
+    public String toString() {
+        return name.toLowerCase();
+    }
+
+    public int bitsPerWord() {
+        return bitsPerWord;
+    }
+
+    /**
+     * Checks whether this is a 32-bit architecture.
+     * @return <code>true</code> if this architecture is 32-bit
+     */
+    public boolean is32bit() {
+        return wordSize == 4;
+    }
+
+    /**
+     * Checks whether this is a 64-bit architecture.
+     * @return <code>true</code> if this architecture is 64-bit
+     */
+    public boolean is64bit() {
+        return wordSize == 8;
+    }
+
+    /**
+     * TODO: Get rid of this method. Platform specific code should be in subclasses.
+     * Checks whether the backend is x86.
+     * @return <code>true</code> if the backend of this architecture is x86
+     */
+    public boolean isX86() {
+        return "x86".equals(backend);
+    }
+
+    /**
+     * TODO: Get rid of this method. Platform specific code should be in subclasses.
+     * Checks whether the backend is SPARC.
+     * @return <code>true</code> if the backend of this architecture is SPARC
+     */
+    public boolean isSPARC() {
+        return "SPARC".equals(backend);
+    }
+}
---
+++ VM/src/com/sun/max/vm/template/ResolvedAtCompileTime.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.vm.template;
+
+import com.sun.max.unsafe.*;
+
+/**
+ * @author Laurent Daynes
+ */
+public class ResolvedAtCompileTime implements ResolvedAtCompileTimeInterface {
+
+    /*
+     * The following static variables are used solely for generating template for the ldc bytecode.
+     * A constant int value larger than what can be encoded in a short must be used to make sure
+     * access to it compiles to a ldc bytecode. Otherwise, a compiler may use a bipush or sipush instead.
+     */
+    public static final int CONSTANT_INT = 333333333;
+    public static final float CONSTANT_FLOAT = 33.33F;
+    public static final long CONSTANT_LONG = 333333333333333333L;
+    public static final double CONSTANT_DOUBLE = 33333333333333.3333D;
+
+    /*
+     * The following static variables are used solely for generating template for get/put static bytecodes
+     */
+    public static byte initializedMutableByteVar = 11;
+    public static short initializedMutableShortVar = 22;
+    public static char initializedMutableCharVar = '1';
+    public static int initializedMutableIntVar = 33;
+    public static long initializedMutableLongVar = 44;
+    public static float initializedMutableFloatVar = 55;
+    public static double initializedMutableDoubleVar = 66;
+    public static boolean initializedMutableBooleanVar = true;
+    public static  Object initializedMutableObjectVar = null;
+
+    /*
+     * The following instance variables are used solely for generating template for get/put field bytecodes
+     */
+    public int intField;
+    public byte byteField;
+    public char charField;
+    public short shortField;
+    public long longField;
+    public float floatField;
+    public boolean booleanField;
+    public double doubleField;
+    public Object objField;
+    public Word wordField;
+
+    public void parameterlessMethod() {
+        // Do enough stuff here to avoid being inlined.
+        for (int i = 0; i < 1000; i++) {
+            intField += i;
+        }
+    }
+
+    public static void parameterlessResolvedStaticMethod() {
+        // Do enough stuff here to avoid being inlined.
+        for (int i = 0; i < 1000; i++) {
+            initializedMutableIntVar += i;
+        }
+    }
+    public static int parameterlessResolvedStaticMethodReturningInt() {
+        int a = 1;
+        for (int i = 0; i < 1000; i++) {
+            a += i;
+        }
+        return a;
+    }
+
+    public static long parameterlessResolvedStaticMethodReturningLong() {
+        long a = 1;
+        for (int i = 0; i < 1000; i++) {
+            a += i;
+        }
+        return a;
+    }
+
+    public static Object parameterlessResolvedStaticMethodReturningReference() {
+        return new Object();
+    }
+
+
+    public void parameterlessResolvedInterfaceMethod() {
+        // Do enough stuff here to avoid being inlined.
+        for (int i = 0; i < 1000; i++) {
+            initializedMutableIntVar += i;
+        }
+    }
+}
