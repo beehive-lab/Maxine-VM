25d1cbf470f7c201bb08a8b1d335e51cc1c57164
Thomas Wuerthinger <thomas.wuerthinger@gmx.at>
2009-09-11 10:04:12 -0700

    Added AbstractTester to the registered JIT classes in configurations that have the JIT as caller

--- VM/test/test/com/sun/max/vm/jtrun/AbstractTester.java
+++ VM/test/test/com/sun/max/vm/jtrun/AbstractTester.java
@@ -203,8 +203,9 @@ public abstract class AbstractTester extends JavaRunScheme {
     @PROTOTYPE_ONLY
     private void registerClasses() {
         if (BootImageGenerator.callerJit) {
-            CompiledPrototype.registerJitClass(test.com.sun.max.vm.jtrun.all.JavaTesterTests.class);
             CompiledPrototype.registerJitClass(test.com.sun.max.vm.jtrun.some.JavaTesterTests.class);
+            CompiledPrototype.registerJitClass(test.com.sun.max.vm.jtrun.all.JavaTesterTests.class);
+            CompiledPrototype.registerJitClass(test.com.sun.max.vm.jtrun.AbstractTester.class);
         }
         for (Class<?> testClass : getClassList()) {
             addClassToImage(testClass);
