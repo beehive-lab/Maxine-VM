c6596d0c7a2099c0748a27a189db7b295269b677
Mick Jordan <Mick.Jordan@sun.com>
2009-09-15 10:23:18 -0700

    Fix bug in SemiSpaceHeapScheme that didn't interpret -Xmx properly

---
+++ Inspector/src/com/sun/max/ins/gui/TabbedInspector.java
@@ -0,0 +1,224 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.ins.gui;
+
+import java.awt.*;
+import java.util.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+import com.sun.max.ins.*;
+import com.sun.max.ins.InspectionSettings.*;
+import com.sun.max.lang.*;
+
+
+/**
+ * An Inspector that contains within it a collection of inspectors in a tabbed frame.
+ * Tabbed inspectors manage change explicitly: removes the contained inspectors
+ * from the update listeners managed generally and notifies only the visible one
+ * when notified of a change.
+ *
+ * @author Michael Van De Vanter
+ */
+public abstract class TabbedInspector<Inspector_Type extends Inspector, TabbedInspector_Type extends TabbedInspector<Inspector_Type, TabbedInspector_Type>>
+    extends UniqueInspector<TabbedInspector_Type>
+    implements InspectorContainer<Inspector_Type> {
+
+    private final JTabbedPane tabbedPane;
+    private final SaveSettingsListener saveSettingsListener;
+
+    protected void addChangeListener(ChangeListener listener) {
+        tabbedPane.addChangeListener(listener);
+    }
+
+    protected void removeChangeListener(ChangeListener listener) {
+        tabbedPane.removeChangeListener(listener);
+    }
+
+    protected TabbedInspector(Inspection inspection, final String settingsClientName) {
+        super(inspection);
+        tabbedPane = new JTabbedPane();
+        if (settingsClientName != null) {
+            saveSettingsListener = createGeometrySettingsClient(this, settingsClientName);
+        } else {
+            saveSettingsListener = null;
+        }
+        createFrame(null);
+        addChangeListener(tabChangeListener);
+    }
+
+    @Override
+    protected final SaveSettingsListener saveSettingsListener() {
+        return saveSettingsListener;
+    }
+
+    private final ChangeListener tabChangeListener = new ChangeListener() {
+
+        public void stateChanged(ChangeEvent event) {
+            // An Inspector tab has become visible that was not visible before.
+            final Inspector selectedInspector = getSelected();
+            if (selectedInspector != null) {
+                // Inspector may not have been getting refreshed while not visible.
+                selectedInspector.refreshView(true);
+            }
+        }
+    };
+
+    public Inspector_Type inspectorAt(int i) {
+        final Component component = tabbedPane.getComponentAt(i);
+        if (component instanceof InspectorFrame) {
+            final InspectorFrame inspectorFrame = (InspectorFrame) component;
+            final Class<Inspector_Type> type = null;
+            return StaticLoophole.cast(type, inspectorFrame.inspector());
+        }
+        return null;
+    }
+
+    public int length() {
+        return tabbedPane.getTabCount();
+    }
+
+    public int getSelectedIndex() {
+        return tabbedPane.getSelectedIndex();
+    }
+
+    public void setSelected(Inspector_Type inspector) {
+        if (inspector.frame() instanceof Component) {
+            moveToFront();
+            tabbedPane.setSelectedComponent((Component) inspector.frame());
+        }
+    }
+
+    public boolean isSelected(Inspector_Type inspector) {
+        return inspector.frame() ==  tabbedPane.getSelectedComponent();
+    }
+
+    public Inspector_Type getSelected() {
+        final Component component =  tabbedPane.getSelectedComponent();
+        for (Inspector_Type  inspector : this) {
+            if (inspector.frame() == component) {
+                return inspector;
+            }
+        }
+        return null;
+    }
+
+    public void add(Inspector_Type inspector) {
+        add(inspector, inspector.frame().getTitle());
+    }
+
+    public void add(Inspector_Type inspector, String tabTitle) {
+        add(inspector, tabTitle, null);
+    }
+
+    public void add(Inspector_Type inspector, String tabTitle, String tabToolTipText) {
+        final InspectorFrame inspectorFrame = inspector.frame();
+        if (inspectorFrame instanceof Component) {
+            final Component component = (Component) inspectorFrame;
+            tabbedPane.addTab(tabTitle, component);
+            if (tabToolTipText != null) {
+                final int index = tabbedPane.indexOfComponent(component);
+                tabbedPane.setToolTipTextAt(index, tabToolTipText);
+            }
+        }
+    }
+
+    public void add(Inspector_Type inspector, String tabTitle, String tabToolTipText, String frameTitle) {
+        final InspectorFrame inspectorFrame = inspector.frame();
+        if (inspectorFrame instanceof Component) {
+            final Component component = (Component) inspectorFrame;
+            tabbedPane.addTab(tabTitle, component);
+            if (tabToolTipText != null) {
+                tabbedPane.setToolTipTextAt(tabbedPane.indexOfComponent(component), tabToolTipText);
+            }
+            if (frameTitle != null) {
+                inspectorFrame.setTitle(frameTitle);
+            }
+        }
+    }
+
+    /**
+     * Extends the tab of this component with a small icon that, when clicked, will close the inspector.
+     * Using this has the curious side-effect that the tabbed pane has additional children that aren't the actual
+     * tabbed frames.
+     */
+    public void addCloseIconToTab(Inspector_Type inspector) {
+        final InspectorFrame inspectorFrame = inspector.frame();
+        if (inspectorFrame instanceof Component) {
+            final Component component = (Component) inspectorFrame;
+            final int index = tabbedPane.indexOfComponent(component);
+            tabbedPane.setTabComponentAt(index, new ButtonTabComponent(inspection(), this, inspector, tabbedPane));
+        }
+    }
+
+    public Iterator<Inspector_Type> iterator() {
+        return InspectorContainer.Static.iterator(this);
+    }
+
+    @Override
+    public void createView() {
+        frame().setContentPane(tabbedPane);
+    }
+
+    public void viewConfigurationChanged() {
+        // These containers generally have no view configurations that need updating.
+    }
+
+    /**
+     * Disposes of the specified inspector, presumed to be a tabbed member of this inspector.
+     * If no tabs remain, dispose of the whole thing.
+     */
+    public void close(Inspector inspector) {
+        inspector.dispose();
+        if (length() == 0) {
+            dispose();
+        }
+    }
+
+    /**
+     * Disposes of all but the specified inspector, presumed to be a tabbed member of this inspector.
+     */
+    public void closeOthers(Inspector keepInspector) {
+        for (Inspector_Type inspector : this) {
+            if (inspector != keepInspector) {
+                inspector.dispose();
+            }
+        }
+        if (length() == 0) {
+            // in case the specified frame wasn't a member.
+            dispose();
+        }
+    }
+
+    /**
+     * Receives notification that the window system is closing this inspector.
+     */
+    @Override
+    public void inspectorClosing() {
+        removeChangeListener(tabChangeListener);
+        for (Inspector_Type inspector : this) {
+            inspector.dispose();
+        }
+        super.inspectorClosing();
+    }
+
+}
---
+++ Inspector/src/com/sun/max/ins/memory/InspectorMemoryTableModel.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.ins.memory;
+
+import javax.swing.table.*;
+
+import com.sun.max.collect.*;
+import com.sun.max.ins.*;
+import com.sun.max.memory.*;
+import com.sun.max.tele.*;
+import com.sun.max.unsafe.*;
+
+
+/**
+ * A model for data tables that represent regions of memory in the VM, one region per row.
+ *
+ * @author Michael Van De Vanter
+ */
+public abstract class InspectorMemoryTableModel extends AbstractTableModel {
+
+    private final Inspection inspection;
+    private final Size wordSize;
+
+    // Memory location from which to compute offsets
+    private Address origin = Address.zero();
+
+    public InspectorMemoryTableModel(Inspection inspection, Address origin) {
+        this.inspection = inspection;
+        this.origin = origin;
+        wordSize = inspection.maxVM().wordSize();
+    }
+
+    /**
+     * Returns the memory location corresponding to a row in the model of VM memory.
+     *
+     * @param row a row in the table model of memory
+     * @return the first location in VM memory corresponding to a row in the table model
+     */
+    public Address getAddress(int row) {
+        return getMemoryRegion(row).start();
+    }
+
+    /**
+     * Updates table display in response to some change in the data contained in the model.
+     */
+    public void refresh() {
+        fireTableDataChanged();
+    }
+
+    /**
+     * Returns the region of memory corresponding to a row in the model of VM memory.
+     *
+     * @param row a row in the table model of memory
+     * @return the first location in VM memory corresponding to a row in the table model
+     */
+    public abstract MemoryRegion getMemoryRegion(int row);
+
+    /**
+     * Returns all memory watchpoints, if any, whose coverage intersects memory corresponding
+     * to a row in the model of VM memory.
+     *
+     * @param row a row in the table model of memory
+     * @return memory watchpoints whose region intersects the memory for this row in the model, empty sequence if none.
+     */
+    public Sequence<MaxWatchpoint> getWatchpoints(int row) {
+        DeterministicSet<MaxWatchpoint> watchpoints = DeterministicSet.Static.empty(MaxWatchpoint.class);
+        for (MaxWatchpoint watchpoint : inspection.maxVM().watchpoints()) {
+            if (watchpoint.overlaps(getMemoryRegion(row))) {
+                if (watchpoints.isEmpty()) {
+                    watchpoints = new DeterministicSet.Singleton<MaxWatchpoint>(watchpoint);
+                } else if (watchpoints.length() == 1) {
+                    GrowableDeterministicSet<MaxWatchpoint> newSet = new LinkedIdentityHashSet<MaxWatchpoint>(watchpoints.first());
+                    newSet.add(watchpoint);
+                    watchpoints = newSet;
+                } else {
+                    final GrowableDeterministicSet<MaxWatchpoint> growableSet = (GrowableDeterministicSet<MaxWatchpoint>) watchpoints;
+                    growableSet.add(watchpoint);
+                }
+            }
+        }
+        return watchpoints;
+    }
+
+    /**
+     * Sets the address in VM memory from which offsets for this model are computed.
+     * <br>
+     * Calls {@link #update()} after change.
+     *
+     * @param origin a memory location in the VM.
+     */
+    public void setOrigin(Address origin) {
+        this.origin = origin;
+        update();
+    }
+
+    /**
+     * Returns an address in VM memory from which offsets for this model are computed.
+     *
+     * @return a memory address understood to be the zero offset for this model
+     * @see #getOffset(int)
+     */
+    public Address getOrigin() {
+        return origin;
+    }
+
+    /**
+     * Returns an offset in bytes, from an origin in VM memory, for the beginning of the memory
+     * corresponding to this row in the model.
+     *
+     * @param row a row in the table model of memory
+     * @return location of the memory for this row, specified in a byte offset from an origin in VM memory
+     * @see InspectorMemoryTableModel#getOrigin()
+     */
+    public abstract Offset getOffset(int row);
+
+    /**
+     * Locates the row, if any, that represent a range of memory that includes a specific location in VM memory.
+     *
+     * @param address a location in VM memory.
+     * @return the row in the model corresponding to hte location, -1 if none
+     */
+    public abstract int findRow(Address address);
+
+    protected Size getWordSize() {
+        return wordSize;
+    }
+
+    /**
+     * Update internal state of the model after some parameter is set.
+     *
+     * @see #setOrigin(Address)
+     */
+    protected void update() {
+    }
+
+}
