34c6de99440ad20e39936d0038923c9155742568
Ben L. Titzer <ben.titzer@sun.com>
2009-10-05 17:08:04 -0700

    Fixed one C1X backend bug; still hunting another relating to throw/catch and inlining

---
+++ Tele/src/com/sun/max/tele/jdwputil/JavaMethodProvider.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.tele.jdwputil;
+
+import java.lang.reflect.*;
+import java.util.logging.*;
+
+import com.sun.max.jdwp.vm.data.*;
+import com.sun.max.jdwp.vm.proxy.*;
+import com.sun.max.vm.type.*;
+
+
+class JavaMethodProvider implements MethodProvider {
+
+    private static final Logger LOGGER = Logger.getLogger(JavaMethodProvider.class.getName());
+
+    private Method method;
+    private ReferenceTypeProvider holder;
+    private VMAccess vm;
+
+    JavaMethodProvider(Method method, ReferenceTypeProvider holder, VMAccess vm) {
+        this.method = method;
+        this.holder = holder;
+        this.vm = vm;
+    }
+
+    public int getFlags() {
+        return method.getModifiers();
+    }
+
+    public LineTableEntry[] getLineTable() {
+        return new LineTableEntry[0];
+    }
+
+    public String getName() {
+        // TODO: Check if this is correct.
+        return method.getName();
+    }
+
+    public int getNumberOfArguments() {
+        int count = Modifier.isStatic(method.getModifiers()) ? 0 : 1;
+        for (Class type : method.getParameterTypes()) {
+            if (type.equals(double.class) || type.equals(long.class)) {
+                count += 2;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public ReferenceTypeProvider getReferenceTypeHolder() {
+        return holder;
+    }
+
+    public String getSignature() {
+        return SignatureDescriptor.fromJava(method).toString();
+    }
+
+    public String getSignatureWithGeneric() {
+        // TODO: Check if this is correct.
+        return getSignature();
+    }
+
+    public VariableTableEntry[] getVariableTable() {
+        return new VariableTableEntry[0];
+    }
+
+    public VMValue invoke(ObjectProvider object, VMValue[] args, ThreadProvider threadProvider, boolean singleThreaded, boolean nonVirtual) {
+
+        LOGGER.info("Method " + method.getName() + " of class " + method.getDeclaringClass() + " was invoked with arguments: " + args);
+        final Object[] arguments = new Object[args.length];
+        for (int i = 0; i < arguments.length; i++) {
+            arguments[i] = args[i].asJavaObject();
+        }
+
+        final VMValue objectValue = vm.createObjectProviderValue(object);
+        Object instanceObject = null;
+        if (objectValue != null) {
+            instanceObject = objectValue.asJavaObject();
+        }
+
+        Object result = null;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(instanceObject, arguments);
+        } catch (IllegalArgumentException e) {
+            LOGGER.log(Level.SEVERE, "Exception while invoking method " + method.getName(), e);
+        } catch (IllegalAccessException e) {
+            LOGGER.log(Level.SEVERE, "Exception while invoking method " + method.getName(), e);
+        } catch (InvocationTargetException e) {
+            LOGGER.log(Level.SEVERE, "Exception while invoking method " + method.getName(), e);
+        }
+
+        if (method.getReturnType() == Void.TYPE) {
+            return vm.getVoidValue();
+        }
+
+
+        return vm.createJavaObjectValue(result, method.getReturnType());
+    }
+
+    public VMValue invokeStatic(VMValue[] args, ThreadProvider threadProvider, boolean singleThreaded) {
+        return invoke(null, args, threadProvider, singleThreaded, false);
+    }
+
+    public TargetMethodAccess[] getTargetMethods() {
+        return new TargetMethodAccess[0];
+    }
+}
---
+++ Tele/src/com/sun/max/tele/method/TeleMethodAccess.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.tele.method;
+
+import com.sun.max.program.*;
+import com.sun.max.tele.*;
+import com.sun.max.tele.interpreter.*;
+import com.sun.max.vm.actor.holder.*;
+import com.sun.max.vm.actor.member.*;
+import com.sun.max.vm.classfile.constant.*;
+import com.sun.max.vm.prototype.*;
+import com.sun.max.vm.type.*;
+import com.sun.max.vm.value.*;
+
+/**
+ * A {@code TeleMethodAccess} provides a mechanism for accessing a method in a tele VM.
+ * It includes support for {@linkplain #interpret(Value...) invoking} such a method in the
+ * context of the tele VM.
+ *
+ * @author Bernd Mathiske
+ * @author Doug Simon
+ */
+public abstract class TeleMethodAccess extends AbstractTeleVMHolder {
+
+    private static MethodActor findMethodActor(Class holder, String name, SignatureDescriptor signature) {
+        final ClassActor classActor = PrototypeClassLoader.PROTOTYPE_CLASS_LOADER.mustMakeClassActor(JavaTypeDescriptor.forJavaClass(holder));
+        return classActor.findMethodActor(SymbolTable.makeSymbol(name), signature);
+    }
+
+    private static MethodActor findMethodActor(Class holder, String name) {
+        final ClassActor classActor = PrototypeClassLoader.PROTOTYPE_CLASS_LOADER.mustMakeClassActor(JavaTypeDescriptor.forJavaClass(holder));
+        MethodActor uniqueMethodActor = null;
+        for (MethodActor methodActor : classActor.getLocalMethodActors()) {
+            if (methodActor.name.string.equals(name)) {
+                if (uniqueMethodActor != null) {
+                    ProgramError.unexpected("need to disambiguate method named '" + name + "' in " + classActor.name + " with a signature");
+                }
+                uniqueMethodActor = methodActor;
+            }
+        }
+        return uniqueMethodActor;
+    }
+
+    private final MethodActor methodActor;
+
+    protected final MethodActor methodActor() {
+        return methodActor;
+    }
+
+    protected TeleMethodAccess(TeleVM teleVM, Class holder, String name, SignatureDescriptor signature) {
+        super(teleVM);
+        if (signature != null) {
+            methodActor = findMethodActor(holder, name, signature);
+            ProgramError.check(methodActor != null, "could not find method " + name + signature + " in " + holder);
+        } else {
+            methodActor = findMethodActor(holder, name);
+            ProgramError.check(methodActor != null, "could not find method named '" + name + "' in " + holder);
+        }
+    }
+
+    /**
+     * Executes the method as if running in the VM.  Interprets (slowly) the bytecodes in an environment
+     * where memory reads are remote to the VM, and in which the IDs of classes are those of the VM.
+     * Local objects passed as arguments must be of types known as legitimate in the VM.
+     *
+     * @param arguments
+     * @return return value from the method
+     * @throws TeleInterpreterException if an uncaught exception occurs during execution of the method
+     */
+    public Value interpret(Value... arguments) throws TeleInterpreterException {
+        ProgramError.check(methodActor instanceof ClassMethodActor, "cannot interpret interface method");
+        return TeleInterpreter.execute(teleVM(), (ClassMethodActor) methodActor, arguments);
+    }
+}
