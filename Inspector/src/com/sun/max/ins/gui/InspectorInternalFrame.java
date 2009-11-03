0bb929f2413aec8f6131b815ade5ef2661b89049
Doug Simon <doug.simon@sun.com>
2009-11-02 22:17:21 -0800

    Modified -XX:DumpStackOnThrowOf=<value> option to be a negative filter if <value> starts with '\!'. This is useful to filter out all the ClassNotFoundExceptions thrown as a normal part of class loading.

---
+++ Inspector/src/com/sun/max/ins/gui/InspectorInternalFrame.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.ins.gui;
+
+import java.awt.*;
+import java.beans.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+import com.sun.max.ins.gui.Inspector.*;
+import com.sun.max.program.*;
+
+/**
+ * A internal frame controlled by an {@linkplain Inspector inspector}.
+ *
+ * @author Bernd Mathiske
+ * @author Doug Simon
+ * @author Michael Van De Vanter
+ */
+final class InspectorInternalFrame extends JInternalFrame implements InspectorFrame {
+
+    private final Inspector inspector;
+    private final InspectorMenuBar menuBar;
+
+    /**
+     * Creates an internal frame, with content pane, for an Inspector intended to be in
+     * a {@link JDesktopPane}.
+     * <br>
+     * The frame has an optional menu bar.  It is a program error to call {@link #makeMenu(MenuKind)}
+     * if no menu bar is present.
+     *
+     * @param inspector
+     * @param addMenuBar should the frame have a menu bar installed.
+     * @see #makeMenu(MenuKind)
+     */
+    public InspectorInternalFrame(Inspector inspector, boolean addMenuBar) {
+        this.inspector = inspector;
+        menuBar = addMenuBar ? new InspectorMenuBar(inspector.inspection()) : null;
+        setJMenuBar(menuBar);
+        setResizable(true);
+        setClosable(true);
+        setIconifiable(true);
+        setVisible(false);
+
+        addInternalFrameListener(new InternalFrameAdapter() {
+
+            @Override
+            public void internalFrameActivated(InternalFrameEvent e) {
+                InspectorInternalFrame.this.inspector.inspectorGetsWindowFocus();
+            }
+
+            @Override
+            public void internalFrameDeactivated(InternalFrameEvent e) {
+                InspectorInternalFrame.this.inspector.inspectorLosesWindowFocus();
+            }
+        });
+    }
+
+    public JComponent getJComponent() {
+        return this;
+    }
+
+    public void refresh(boolean force) {
+        if (menuBar != null) {
+            menuBar.refresh(force);
+        }
+    }
+
+    public void redisplay() {
+    }
+
+    public Inspector inspector() {
+        return inspector;
+    }
+
+    public InspectorMenu makeMenu(MenuKind menuKind) throws ProgramError {
+        ProgramError.check(menuBar != null);
+        return menuBar.makeMenu(menuKind);
+    }
+
+    public void setSelected() {
+        try {
+            setSelected(true);
+        } catch (PropertyVetoException e) {
+        }
+    }
+
+    public void flash(Color borderFlashColor) {
+        Component pane = getContentPane();
+        if (pane instanceof JScrollPane) {
+            final JScrollPane scrollPane = (JScrollPane) pane;
+            pane = scrollPane.getViewport();
+        }
+        final Graphics g = pane.getGraphics();
+        g.setPaintMode();
+        g.setColor(borderFlashColor);
+        for (int i = 0; i < 5; i++) {
+            g.drawRect(i, i, pane.getWidth() - (i * 2), pane.getHeight() - (i * 2));
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        g.dispose();
+        invalidate();
+        repaint();
+    }
+
+    public void setStateColor(Color color) {
+        if (menuBar != null) {
+            menuBar.setBackground(color);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        inspector.inspectorClosing();
+    }
+
+}
---
+++ Tele/src/com/sun/max/tele/page/PageDataAccess.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.tele.page;
+
+import java.nio.*;
+
+import com.sun.max.collect.*;
+import com.sun.max.lang.*;
+import com.sun.max.program.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.prototype.*;
+
+/**
+ * @author Bernd Mathiske
+ * @author Michael Van De Vanter
+ */
+public class PageDataAccess extends DataAccessAdapter {
+
+    private static final int TRACE_VALUE = 1;
+
+    protected String  tracePrefix() {
+        return "[PageDataAccess] ";
+    }
+
+    private final TeleIO teleIO;
+    private final int indexShift;
+    private final int offsetMask;
+    private final ByteBuffer writeBuffer;
+
+    public PageDataAccess(TeleIO teleProcess, DataModel dataModel) {
+        super(dataModel.wordWidth);
+        teleIO = teleProcess;
+        ProgramError.check(Ints.isPowerOfTwoOrZero(teleIO.pageSize()), "Page size is not a power of 2: " + teleIO.pageSize());
+        indexShift = Integer.numberOfTrailingZeros(teleProcess.pageSize());
+        offsetMask = teleProcess.pageSize() - 1;
+        writeBuffer = ByteBuffer.wrap(new byte[Longs.SIZE]).order(dataModel.endianness.asByteOrder());
+    }
+
+    public int pageSize() {
+        return teleIO.pageSize();
+    }
+
+    private long getIndex(Address address) {
+        return address.toLong() >>> indexShift;
+    }
+
+    private int getOffset(Address address) {
+        return address.toInt() & offsetMask;
+    }
+
+    private final VariableMapping<Long, Page> indexToPage = HashMapping.createVariableEqualityMapping();
+
+    private static void checkNullPointer(Address address) {
+        if (address.isZero()) {
+            throw new NullPointerException("Cannot access address ZERO");
+        }
+    }
+
+    private void invalidatePage(long index) {
+        final Page page = indexToPage.get(index);
+        if (page != null) {
+            page.invalidate();
+        }
+    }
+
+    public synchronized void invalidate(Address address, Size size) {
+        long numberOfPages = getIndex(size);
+        if (getOffset(address) + getOffset(size) > pageSize()) {
+            numberOfPages++;
+        }
+        final long startIndex = getIndex(address);
+        for (long index = startIndex; index <= startIndex + numberOfPages; index++) {
+            invalidatePage(index);
+        }
+    }
+
+    public void invalidateForWrite(Address address, int size) {
+        checkNullPointer(address);
+        invalidate(address, Size.fromInt(size));
+    }
+
+    private Page getPage(long index) {
+        Page page = indexToPage.get(index);
+        if (page == null) {
+            page = new Page(teleIO, index, writeBuffer.order());
+            indexToPage.put(index, page);
+            if ((indexToPage.length() % 1000) == 0) {
+                Trace.line(TRACE_VALUE, tracePrefix() + "Memory cache: " + indexToPage.length() + " pages");
+            }
+        }
+        return page;
+    }
+
+    private Page getPage(Address address) {
+        return getPage(getIndex(address));
+    }
+
+    public synchronized int read(Address address, ByteBuffer buffer, int offset, int length) {
+        final int toRead = Math.min(length, buffer.limit() - offset);
+        long pageIndex = getIndex(address);
+        int pageOffset = getOffset(address);
+        int i = 0;
+        while (i < toRead) {
+            i += getPage(pageIndex).readBytes(pageOffset, buffer, i + offset);
+            pageIndex++;
+            pageOffset = 0;
+        }
+        return toRead;
+    }
+
+    public synchronized byte readByte(Address address) {
+        checkNullPointer(address);
+        return getPage(address).readByte(getOffset(address));
+    }
+
+
+    public synchronized short readShort(Address address) {
+        checkNullPointer(address);
+        return getPage(address).readShort(getOffset(address));
+    }
+
+    public synchronized int readInt(Address address) {
+        checkNullPointer(address);
+        return getPage(address).readInt(getOffset(address));
+    }
+
+    public long readLong(Address address) {
+        checkNullPointer(address);
+        return getPage(address).readLong(getOffset(address));
+    }
+
+    public synchronized int write(ByteBuffer buffer, int offset, int length, Address address) {
+        invalidateForWrite(address, buffer.limit());
+        DataIO.Static.checkRead(buffer, offset, length);
+        return teleIO.write(buffer, offset, buffer.limit(), address);
+    }
+
+    public synchronized void writeByte(Address address, byte value) {
+        invalidateForWrite(address, Bytes.SIZE);
+        writeBuffer.put(0, value);
+        teleIO.write(writeBuffer, 0, Bytes.SIZE, address);
+    }
+
+    public synchronized void writeShort(Address address, short value) {
+        invalidateForWrite(address, Shorts.SIZE);
+        writeBuffer.putShort(0, value);
+        teleIO.write(writeBuffer, 0, Shorts.SIZE, address);
+    }
+
+    public synchronized void writeInt(Address address, int value) {
+        invalidateForWrite(address, Ints.SIZE);
+        writeBuffer.putInt(0, value);
+        teleIO.write(writeBuffer, 0, Ints.SIZE, address);
+    }
+
+    public synchronized void writeLong(Address address, long value) {
+        invalidateForWrite(address, Longs.SIZE);
+        writeBuffer.putLong(0, value);
+        teleIO.write(writeBuffer, 0, Longs.SIZE, address);
+    }
+}
