ecc9a9f69d2116fc1ce1607679906f706862241d
Doug Simon <doug.simon@sun.com>
2009-11-08 00:09:01 -0800

    Made System.loadLibrary() work.

---
+++ Native/substrate/jnitests.c
@@ -0,0 +1,268 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+/**
+ * A set of tests that need to be in the target.
+ *
+ * @author Doug Simon
+ */
+#include "log.h"
+#include "jni.h"
+
+JNIEXPORT void JNICALL
+Java_com_sun_max_vm_run_compilerTest_CompilerTestRunScheme_nativeUpdateFields(JNIEnv *env, jobject object, int n, jint i, jobject o) {
+	jclass _class = (*env)->GetObjectClass(env, object);
+    if (_class == NULL) {
+    	printf("Could not get class of object\n");
+    	return;
+    }
+
+    if (n > 0) {
+    	jmethodID mid = (*env)->GetMethodID(env, _class, "testNative", "(I)V");
+    	if (mid == NULL) {
+        	printf("Could not find method testNative(int)\n");
+        	return;
+    	}
+    	(*env)->CallVoidMethod(env, object, mid, n, i, o);
+    } else {
+        jfieldID _i;
+        jfieldID _o;
+    	_i = (*env)->GetFieldID(env, _class, "_i", "I");
+    	if (_i == NULL) {
+    		printf("Could not find field _i\n");
+    		return;
+    	}
+
+    	_o = (*env)->GetFieldID(env, _class, "_o", "Ljava/lang/Object;");
+    	if (_o == NULL) {
+    		printf("Could not find field _o\n");
+    		return;
+    	}
+
+    	(*env)->SetIntField(env, object, _i, i);
+    	(*env)->SetObjectField(env, object, _o, o);
+    }
+}
+
+
+#define BUFSIZE 8192
+JNIEXPORT jint JNICALL
+Java_jtt_jni_JNI_1OverflowArguments_read1(JNIEnv *env, jclass cls, jlong zfile,
+														jlong zentry, jlong pos, jbyteArray bytes, jint off, jint len) {
+  if (len > BUFSIZE) {
+	 len = BUFSIZE;
+  }
+  return len;
+}
+
+JNIEXPORT jint JNICALL
+Java_jtt_jni_JNI_1OverflowArguments_read2(JNIEnv *env, jclass cls, jlong zfile,
+														jlong zentry, jlong pos, jbyteArray bytes, jint off, jint len) {
+  return off;
+}
+
+JNIEXPORT jlong JNICALL
+Java_test_bench_threads_JNI_1invocations_nativework(JNIEnv *env, jclass cls, jlong workload) {
+    int i = 0;
+    int sum = 0;
+    for (i=0; i<workload; i++) {
+        sum +=i;
+    }
+    return sum;
+}
+
+JNIEXPORT void JNICALL
+Java_jtt_jni_JNI_1Nop_nop(JNIEnv *env, jclass c) {
+}
+
+JNIEXPORT void JNICALL
+Java_jtt_jni_JNI_1Nop_sync_1nop(JNIEnv *env, jclass c) {
+}
+
+JNIEXPORT jobject JNICALL
+Java_jtt_jni_JNI_1IdentityObject_id_1object (JNIEnv *env, jclass c, jobject o) {
+    return o;
+}
+JNIEXPORT jbyte JNICALL
+Java_jtt_jni_JNI_1IdentityByte_id_1byte(JNIEnv *env, jclass c, jbyte b) {
+    return b;
+}
+JNIEXPORT jboolean JNICALL
+Java_jtt_jni_JNI_1IdentityBoolean_id_1boolean(JNIEnv *env, jclass c, jboolean b) {
+    return b;
+}
+JNIEXPORT jchar JNICALL
+Java_jtt_jni_JNI_1IdentityChar_id_1char(JNIEnv *env, jclass c, jchar ch) {
+    return ch;
+}
+JNIEXPORT jshort JNICALL
+Java_jtt_jni_JNI_1IdentityShort_id_1short(JNIEnv *env, jclass c, jshort s) {
+    return s;
+}
+JNIEXPORT jint JNICALL
+Java_jtt_jni_JNI_1IdentityInt_id_1int(JNIEnv *env, jclass c, jint i) {
+    return i;
+}
+JNIEXPORT jfloat JNICALL
+Java_jtt_jni_JNI_1IdentityFloat_id_1float(JNIEnv *env, jclass c, jfloat f) {
+    return f;
+}
+JNIEXPORT jlong JNICALL
+Java_jtt_jni_JNI_1IdentityLong_id_1long(JNIEnv *env, jclass c, jlong l) {
+    return l;
+}
+
+JNIEXPORT void JNICALL
+Java_jtt_jni_JNI_1ManyParameters_manyParameters(JNIEnv *env, jclass clazz,
+        jobject reflectedMethod,
+        jobject sb,
+        jobject object1,
+        jint int1,
+        jlong long1,
+        jshort short1,
+        jchar char1,
+        jobject object2,
+        jint int2,
+        jlong long2,
+        jshort short2,
+        jchar char2)
+{
+    jmethodID methodID = (*env)->FromReflectedMethod(env, reflectedMethod);
+    (*env)->CallStaticVoidMethod(env, clazz, methodID, sb, object1, int1, long1, short1, char1, object2, int2, long2, short2, char2);
+}
+
+JNIEXPORT jobject JNICALL
+Java_jtt_jni_JNI_1ManyObjectParameters_manyObjectParameters(JNIEnv *env, jclass clazz,
+        jobject array,
+        jobject object0,
+        jobject object1,
+        jobject object2,
+        jobject object3,
+        jobject object4,
+        jobject object5,
+        jobject object6,
+        jobject object7,
+        jobject object8,
+        jobject object9,
+        jobject object10,
+        jobject object11,
+        jobject object12,
+        jobject object13,
+        jobject object14,
+        jobject object15,
+        jobject object16,
+        jobject object17,
+        jobject object18,
+        jobject object19,
+        jobject object20,
+        jobject object21,
+        jobject object22,
+        jobject object23,
+        jobject object24,
+        jobject object25,
+        jobject object26,
+        jobject object27,
+        jobject object28,
+        jobject object29,
+        jobject object30,
+        jobject object31,
+        jobject object32,
+        jobject object33,
+        jobject object34,
+        jobject object35,
+        jobject object36,
+        jobject object37,
+        jobject object38,
+        jobject object39,
+        jobject object40,
+        jobject object41,
+        jobject object42,
+        jobject object43,
+        jobject object44,
+        jobject object45,
+        jobject object46,
+        jobject object47,
+        jobject object48,
+        jobject object49,
+        jobject object50,
+        jobject object51,
+        jobject object52,
+        jobject object53,
+        jobject object54,
+        jobject object55)
+{
+    (*env)->SetObjectArrayElement(env, array, 0, object0);
+    (*env)->SetObjectArrayElement(env, array, 1, object1);
+    (*env)->SetObjectArrayElement(env, array, 2, object2);
+    (*env)->SetObjectArrayElement(env, array, 3, object3);
+    (*env)->SetObjectArrayElement(env, array, 4, object4);
+    (*env)->SetObjectArrayElement(env, array, 5, object5);
+    (*env)->SetObjectArrayElement(env, array, 6, object6);
+    (*env)->SetObjectArrayElement(env, array, 7, object7);
+    (*env)->SetObjectArrayElement(env, array, 8, object8);
+    (*env)->SetObjectArrayElement(env, array, 9, object9);
+    (*env)->SetObjectArrayElement(env, array, 10, object10);
+    (*env)->SetObjectArrayElement(env, array, 11, object11);
+    (*env)->SetObjectArrayElement(env, array, 12, object12);
+    (*env)->SetObjectArrayElement(env, array, 13, object13);
+    (*env)->SetObjectArrayElement(env, array, 14, object14);
+    (*env)->SetObjectArrayElement(env, array, 15, object15);
+    (*env)->SetObjectArrayElement(env, array, 16, object16);
+    (*env)->SetObjectArrayElement(env, array, 17, object17);
+    (*env)->SetObjectArrayElement(env, array, 18, object18);
+    (*env)->SetObjectArrayElement(env, array, 19, object19);
+    (*env)->SetObjectArrayElement(env, array, 20, object20);
+    (*env)->SetObjectArrayElement(env, array, 21, object21);
+    (*env)->SetObjectArrayElement(env, array, 22, object22);
+    (*env)->SetObjectArrayElement(env, array, 23, object23);
+    (*env)->SetObjectArrayElement(env, array, 24, object24);
+    (*env)->SetObjectArrayElement(env, array, 25, object25);
+    (*env)->SetObjectArrayElement(env, array, 26, object26);
+    (*env)->SetObjectArrayElement(env, array, 27, object27);
+    (*env)->SetObjectArrayElement(env, array, 28, object28);
+    (*env)->SetObjectArrayElement(env, array, 29, object29);
+    (*env)->SetObjectArrayElement(env, array, 30, object30);
+    (*env)->SetObjectArrayElement(env, array, 31, object31);
+    (*env)->SetObjectArrayElement(env, array, 32, object32);
+    (*env)->SetObjectArrayElement(env, array, 33, object33);
+    (*env)->SetObjectArrayElement(env, array, 34, object34);
+    (*env)->SetObjectArrayElement(env, array, 35, object35);
+    (*env)->SetObjectArrayElement(env, array, 36, object36);
+    (*env)->SetObjectArrayElement(env, array, 37, object37);
+    (*env)->SetObjectArrayElement(env, array, 38, object38);
+    (*env)->SetObjectArrayElement(env, array, 39, object39);
+    (*env)->SetObjectArrayElement(env, array, 40, object40);
+    (*env)->SetObjectArrayElement(env, array, 41, object41);
+    (*env)->SetObjectArrayElement(env, array, 42, object42);
+    (*env)->SetObjectArrayElement(env, array, 43, object43);
+    (*env)->SetObjectArrayElement(env, array, 44, object44);
+    (*env)->SetObjectArrayElement(env, array, 45, object45);
+    (*env)->SetObjectArrayElement(env, array, 46, object46);
+    (*env)->SetObjectArrayElement(env, array, 47, object47);
+    (*env)->SetObjectArrayElement(env, array, 48, object48);
+    (*env)->SetObjectArrayElement(env, array, 49, object49);
+    (*env)->SetObjectArrayElement(env, array, 50, object50);
+    (*env)->SetObjectArrayElement(env, array, 51, object51);
+    (*env)->SetObjectArrayElement(env, array, 52, object52);
+    (*env)->SetObjectArrayElement(env, array, 53, object53);
+    (*env)->SetObjectArrayElement(env, array, 54, object54);
+    (*env)->SetObjectArrayElement(env, array, 55, object55);
+    return array;
+}
---
+++ VM/src/com/sun/max/vm/compiler/c1x/C1XCompilerScheme.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2009 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.vm.compiler.c1x;
+
+import java.util.*;
+
+import com.sun.c1x.*;
+import com.sun.c1x.ci.*;
+import com.sun.c1x.ri.*;
+import com.sun.c1x.xir.*;
+import com.sun.max.asm.*;
+import com.sun.max.asm.amd64.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.util.*;
+import com.sun.max.vm.*;
+import com.sun.max.vm.actor.member.*;
+import com.sun.max.vm.compiler.*;
+import com.sun.max.vm.compiler.b.c.d.e.amd64.target.*;
+import com.sun.max.vm.compiler.target.*;
+import com.sun.max.vm.prototype.*;
+import com.sun.max.vm.runtime.*;
+import com.sun.max.vm.stack.*;
+import com.sun.max.vm.tele.*;
+
+/**
+ * @author Ben L. Titzer
+ */
+public class C1XCompilerScheme extends AbstractVMScheme implements RuntimeCompilerScheme {
+
+    private MaxRiRuntime c1xRuntime;
+    private C1XCompiler compiler;
+    private RiXirGenerator xirGenerator;
+
+    public static final VMIntOption c1xOptLevel = VMOptions.register(new VMIntOption("-XX:OptLevel=", 0,
+        "Set the optimization level of C1X.") {
+        @Override
+        public boolean parseValue(com.sun.max.unsafe.Pointer optionValue) {
+            boolean result = super.parseValue(optionValue);
+            if (result) {
+                C1XOptions.setOptimizationLevel(getValue());
+                return true;
+            }
+            return false;
+        }
+    }, MaxineVM.Phase.STARTING);
+
+    public C1XCompilerScheme(VMConfiguration vmConfiguration) {
+        super(vmConfiguration);
+    }
+
+    static {
+        VMOptions.register(new VMBooleanXXOption("-XX:-ResetC1XDefaults",
+            "Reset all C1X options to their defaults. This takes effect before any other C1X options on the command line are parsed.") {
+            @Override
+            public boolean parseValue(Pointer optionValue) {
+                if (super.parseValue(optionValue)) {
+                    if (getValue()) {
+                        C1XOptions.setDefaults();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        }, MaxineVM.Phase.PRISTINE);
+    }
+
+    @Override
+    public void initialize(MaxineVM.Phase phase) {
+        if (MaxineVM.isHosted()) {
+            if (phase == MaxineVM.Phase.BOOTSTRAPPING) {
+                VMOptions.addFieldOptions(C1XOptions.class);
+                // create the RiRuntime object passed to C1X
+                c1xRuntime = MaxRiRuntime.globalRuntime;
+                CiTarget c1xTarget = createTarget(c1xRuntime, vmConfiguration());
+                xirGenerator = new MaxXirGenerator(vmConfiguration(), c1xTarget, c1xRuntime);
+                compiler = new C1XCompiler(c1xRuntime, c1xTarget, xirGenerator);
+                compiler.init();
+            }
+            if (phase == MaxineVM.Phase.COMPILING) {
+                if (MaxineVM.isHosted()) {
+                    // can only refer to JavaPrototype while bootstrapping.
+                    JavaPrototype.javaPrototype().loadPackage("com.sun.c1x", true);
+                }
+            }
+        }
+    }
+
+    public static CiTarget createTarget(RiRuntime runtime, VMConfiguration configuration) {
+        // create the Target object passed to C1X
+        InstructionSet isa = configuration.platform().processorKind.instructionSet;
+        CiArchitecture arch = CiArchitecture.findArchitecture(isa.name().toLowerCase());
+        TargetABI targetABI = configuration.targetABIsScheme().optimizedJavaABI();
+
+        // get the unallocatable registers
+        Set<String> unallocatable = new HashSet<String>();
+        RegisterRoleAssignment roles = targetABI.registerRoleAssignment();
+        markUnallocatable(unallocatable, roles, VMRegister.Role.SAFEPOINT_LATCH);
+        markUnallocatable(unallocatable, roles, VMRegister.Role.CPU_STACK_POINTER);
+        markUnallocatable(unallocatable, roles, VMRegister.Role.CPU_FRAME_POINTER);
+        markUnallocatable(unallocatable, roles, VMRegister.Role.ABI_SCRATCH);
+        markUnallocatable(unallocatable, roles, VMRegister.Role.LITERAL_BASE_POINTER);
+
+        AMD64GeneralRegister64 stackPointer = (AMD64GeneralRegister64) targetABI.stackPointer();
+        AMD64GeneralRegister64 scratchPointer = (AMD64GeneralRegister64) targetABI.scratchRegister();
+        CiRegister stackRegister = null;
+        CiRegister scratchRegister = null;
+
+        CiRegister[] registerReferenceMapTemplate = new CiRegister[AMD64GeneralRegister64.ENUMERATOR.length()];
+        // configure the allocatable registers
+        List<CiRegister> allocatable = new ArrayList<CiRegister>(arch.registers.length);
+        int index = 0;
+        for (AMD64GeneralRegister64 reg : AMD64GeneralRegister64.ENUMERATOR) {
+            for (CiRegister r : arch.registers) {
+
+                if (r.name.toLowerCase().equals(reg.name().toLowerCase())) {
+                    if (!unallocatable.contains(r.name.toLowerCase()) && r != runtime.threadRegister()) {
+                        allocatable.add(r);
+                        registerReferenceMapTemplate[index] = r;
+                        break;
+                    }
+
+                    if (reg == stackPointer) {
+                        stackRegister = r;
+                    }
+
+                    if (reg == scratchPointer) {
+                        scratchRegister = r;
+                    }
+                }
+
+            }
+            index++;
+        }
+
+        assert stackRegister != null;
+
+        for (AMD64XMMRegister reg : AMD64XMMRegister.ENUMERATOR) {
+            for (CiRegister r : arch.registers) {
+                if (!unallocatable.contains(r.name.toLowerCase()) && r != runtime.threadRegister() && r.name.toLowerCase().equals(reg.name().toLowerCase())) {
+                    allocatable.add(r);
+                    break;
+                }
+            }
+        }
+
+        CiRegister[] allocRegs = allocatable.toArray(new CiRegister[allocatable.size()]);
+
+        // TODO (tw): Initialize target differently
+        CiTarget target = new CiTarget(arch, stackRegister, scratchRegister, allocRegs, allocRegs, registerReferenceMapTemplate, configuration.platform.pageSize, true);
+        target.stackAlignment = targetABI.stackFrameAlignment();
+        return target;
+    }
+
+    private static void markUnallocatable(Set<String> unallocatable, RegisterRoleAssignment roles, VMRegister.Role register) {
+        Symbol intReg = roles.integerRegisterActingAs(register);
+        if (intReg != null) {
+            unallocatable.add(intReg.name().toLowerCase());
+        }
+        Symbol floatReg = roles.floatingPointRegisterActingAs(register);
+        if (floatReg != null) {
+            unallocatable.add(floatReg.name().toLowerCase());
+        }
+    }
+
+    public final TargetMethod compile(ClassMethodActor classMethodActor) {
+        RiMethod method = c1xRuntime.getRiMethod(classMethodActor);
+        CiTargetMethod compiledMethod = compiler.compileMethod(method, xirGenerator).targetMethod();
+        if (compiledMethod != null) {
+            C1XTargetMethod c1xTargetMethod = new C1XTargetMethod(this, classMethodActor, compiledMethod);
+            BytecodeBreakpointMessage.makeTargetBreakpoints(c1xTargetMethod);
+            return c1xTargetMethod;
+        }
+        throw FatalError.unexpected("bailout"); // compilation failed
+    }
+
+    public boolean walkFrame(StackFrameWalker stackFrameWalker, boolean isTopFrame, TargetMethod targetMethod, TargetMethod callee, StackFrameWalker.Purpose purpose, Object context) {
+        return BcdeTargetAMD64Compiler.walkFrameHelper(stackFrameWalker, isTopFrame, targetMethod, callee, purpose, context);
+    }
+}
