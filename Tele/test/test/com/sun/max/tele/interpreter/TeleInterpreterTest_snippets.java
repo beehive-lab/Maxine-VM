4e978651b8ff7906240d1a3451977e904180ee74
Mick Jordan <Mick.Jordan@sun.com>
2009-11-10 15:02:21 -0800

    new thread test for wait/interrupt/join bug

---
+++ Tele/test/test/com/sun/max/tele/interpreter/TeleInterpreterTest_snippets.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package test.com.sun.max.tele.interpreter;
+
+import junit.framework.*;
+import test.com.sun.max.vm.compiler.*;
+
+/**
+ * Test whether the internal Snippets get translated at all.
+ * This test is subsumed by each of the other translator tests.
+ * 
+ * @author Bernd Mathiske
+ */
+public class TeleInterpreterTest_snippets extends CompilerTestCase {
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(TeleInterpreterTest_snippets.suite());
+    }
+
+    public static Test suite() {
+        final TestSuite suite = new TestSuite(TeleInterpreterTest_snippets.class.getSimpleName());
+        suite.addTestSuite(TeleInterpreterTest_snippets.class);
+        return new TeleInterpreterTestSetup(suite); // This performs the test
+    }
+
+    public TeleInterpreterTest_snippets(String name) {
+        super(name);
+    }
+
+    public void test() {
+        // new TeleInterpreterTestSetup() performs: Snippet.translateAll() and Snippet.optimizeAll()
+    }
+
+}
---
+++ VM/test/jtt/optimize/Fold_Convert04.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2009 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package jtt.optimize;
+
+/*
+ * Tests constant folding of float conversions
+ * @Harness: java
+ * @Runs: 0=1024d; 1=-1.25d
+ */
+public class Fold_Convert04 {
+    public static double test(double arg) {
+        if (arg == 0) {
+            return l2d();
+        }
+        if (arg == 1) {
+            return f2d();
+        }
+        return  0;
+    }
+    public static double l2d() {
+        long x = 1024;
+        return x;
+    }
+    public static double f2d() {
+        float x = -1.25f;
+        return x;
+    }
+}
