c67603a7ca40adb168f0964cb55c10fab1d1f36d
Michael Van De Vanter <michael.vandevanter@sun.com>
2009-11-11 12:42:25 -0800

    Inspector: Memory inspector windows now size correctly for the number of words being displayed.

---
+++ C1X/src/com/sun/c1x/ir/StoreRegister.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2009 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.c1x.ir;
+
+import com.sun.c1x.ci.CiKind;
+import com.sun.c1x.ci.CiRegister;
+
+/**
+ * The <code>StoreRegister</code> instruction represents a write of a physical register.
+ * This instruction is part of the HIR support for low-level operations, such as safepoints,
+ * stack banging, etc, and does not correspond to a Java operation.
+ *
+ * @author Ben L. Titzer
+ */
+public class StoreRegister extends Instruction {
+
+    final CiRegister register;
+    Value value;
+
+    /**
+     * Creates a new StoreReigster instance.
+     * @param kind the kind of value stored to the register
+     * @param register the register to store
+     * @param value the value to write
+     */
+    public StoreRegister(CiKind kind, CiRegister register, Value value) {
+        super(kind);
+        this.register = register;
+        this.value = value;
+        setFlag(Flag.LiveStore);
+    }
+
+    /**
+     * Implements this instruction's half of the visitor pattern.
+     * @param v the visitor to accept
+     */
+    @Override
+    public void accept(ValueVisitor v) {
+        v.visitStoreRegister(this);
+    }
+
+    public CiRegister register() {
+        return register;
+    }
+
+    public Value value() {
+        return value;
+    }
+
+    /**
+     * Iterates over the input values to this instruction. In this case,
+     * it is only the written value.
+     * @param closure the closure to apply to each value
+     */
+    @Override
+    public void inputValuesDo(ValueClosure closure) {
+        value = closure.apply(value);
+    }
+}
---
+++ VM/src/com/sun/max/vm/compiler/ir/interpreter/eir/EirStack.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.vm.compiler.ir.interpreter.eir;
+
+import java.util.*;
+
+import com.sun.max.collect.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.thread.*;
+import com.sun.max.vm.type.*;
+import com.sun.max.vm.value.*;
+
+/**
+ * Emulation of a call stack that grows downwards.
+ *
+ * @author Bernd Mathiske
+ * @author Doug Simon
+ */
+public class EirStack {
+
+    public static class StackAddressOutOfBoundsException extends IndexOutOfBoundsException {
+
+        public StackAddressOutOfBoundsException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * The address at which the stack starts.  Any attempt to read from or write
+     * to the stack at an address equal to or higher that this value results in
+     * a {@link StackAddressOutOfBoundsException}.
+     * Sufficiently high, so the stack can grow downwards.  This must also be
+     * a word aligned value as the real stack will also be word aligned.
+     */
+    public final Address ceiling;
+
+    private Address sp;
+
+    public EirStack() {
+        ceiling = VmThread.stackSize();
+        assert !ceiling.isZero();
+        sp = ceiling;
+    }
+
+    protected EirStack(EirStack copy) {
+        ceiling = copy.ceiling;
+        sp = copy.sp;
+        slots = new ArrayListSequence<Value>(copy.slots);
+    }
+
+    /**
+     * Makes a copy of this stack state for the purpose of saving a CPU context that can be restored.
+     */
+    public EirStack save() {
+        return new EirStack(this);
+    }
+
+    /**
+     * Gets the address of the "top of stack". This is
+     * the lowest address at which values can be read from or written to the stack
+     * without throwing a {@link StackAddressOutOfBoundsException}.
+     */
+    public Address sp() {
+        return sp;
+    }
+
+    public void setSP(Address address) {
+        sp = address;
+    }
+
+    private List<Value> slots = new ArrayListSequence<Value>();
+
+    private int addressToSlotIndex(Address address) {
+        if (address.lessThan(sp) || address.greaterEqual(ceiling)) {
+            final String message = address.toLong() + " < " + sp.toLong() + " || " + address.toLong() + " >= " + ceiling.toLong();
+            throw new StackAddressOutOfBoundsException(message);
+        }
+        final int index = ceiling.minus(address).dividedBy(8).toInt();
+        if (index >= slots.size()) {
+            for (int i = slots.size(); i <= index; i++) {
+                slots.add(null);
+            }
+        }
+        return index;
+    }
+
+    public Value read(Address address) {
+        return slots.get(addressToSlotIndex(address));
+    }
+
+    public byte readByte(Address address) {
+        assert read(address).kind() == Kind.WORD;
+        return read(address).unsignedToByte();
+    }
+
+    public short readShort(Address address) {
+        assert read(address).kind() == Kind.WORD;
+        return read(address).unsignedToShort();
+    }
+
+    public int readInt(Address address) {
+        assert read(address).kind() == Kind.WORD;
+        return read(address).unsignedToInt();
+    }
+
+    public long readLong(Address address) {
+        assert read(address).kind() == Kind.WORD;
+        return read(address).toLong();
+    }
+
+    public float readFloat(Address address) {
+        return read(address).asFloat();
+    }
+
+    public double readDouble(Address address) {
+        return read(address).asDouble();
+    }
+
+    public void write(Address address, Value value) {
+        final int index = addressToSlotIndex(address);
+        slots.set(index, value);
+    }
+
+    private WordValue partiallyOverwrite(Address address, int n, int mask) {
+        Address word = Address.zero();
+        final Value value = read(address);
+        if (value != null && value.kind() == Kind.WORD) {
+            word = value.asWord().asAddress().and(Address.fromInt(mask).not());
+        }
+        return new WordValue(word.or(n & mask));
+    }
+
+    public void writeByte(Address address, byte value) {
+        write(address, partiallyOverwrite(address, value, 0xff));
+    }
+
+    public void writeShort(Address address, short value) {
+        write(address, partiallyOverwrite(address, value, 0xffff));
+    }
+
+    public void writeInt(Address address, int value) {
+        write(address, partiallyOverwrite(address, value, 0xffffffff));
+    }
+
+    public void writeLong(Address address, long value) {
+        write(address, new WordValue(Address.fromLong(value)));
+    }
+
+    public void writeFloat(Address address, float value) {
+        write(address, FloatValue.from(value));
+    }
+
+    public void writeDouble(Address address, double value) {
+        write(address, DoubleValue.from(value));
+    }
+
+    public void writeWord(Address address, Word value) {
+        write(address, new WordValue(value));
+    }
+}
