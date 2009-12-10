d142b2087939711068b3568cc1e934f4683c1d29
Ben L. Titzer <ben.titzer@sun.com>
2009-12-09 16:00:13 -0800

    Made noGC the default in running dynamic benchmarks for C1X

--- bin/run-vee2010-dynamic.bash
+++ bin/run-vee2010-dynamic.bash
@@ -12,47 +12,49 @@ mkdir -p $MAXINE_HOME/vee2010-results
 
 export VEE_TEST_OPTIONS="-timing-runs=5 -fail-fast=f -tests=specjvm98,dacapo -specjvm98=$SPECJVM98_ZIP -dacapo=$DACAPO_JAR"
 
+MAXVM_CONFIGS=noGC
+
 if [ x"$1" = xjit ]; then
     shift
 
 #    echo jit-c1x0
-#    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=jit-c1x0 > $RESULTS_DIR/jit-c1x0
+#    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=jit-c1x0 > $RESULTS_DIR/jit-c1x0
 
 #    echo jit-c1x0x
-#    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=jit-c1x0x > $RESULTS_DIR/jit-c1x0x
+#    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=jit-c1x0x > $RESULTS_DIR/jit-c1x0x
 
     echo jit-c1x1
-    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=jit-c1x1 > $RESULTS_DIR/jit-c1x1
+    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=jit-c1x1 > $RESULTS_DIR/jit-c1x1
 
     echo jit-c1x1x
-    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=jit-c1x1x > $RESULTS_DIR/jit-c1x1x
+    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=jit-c1x1x > $RESULTS_DIR/jit-c1x1x
 
     echo jit-c1x2
-    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=jit-c1x2 > $RESULTS_DIR/jit-c1x2
+    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=jit-c1x2 > $RESULTS_DIR/jit-c1x2
     
     echo jit-c1x2x
-    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=jit-c1x2x > $RESULTS_DIR/jit-c1x2x
+    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=jit-c1x2x > $RESULTS_DIR/jit-c1x2x
 
 fi
 
 if [ x"$1" = xopt ]; then
 
 #    echo opt-c1x0
-#    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=opt-c1x0 > $RESULTS_DIR/opt-c1x0
+#    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=opt-c1x0 > $RESULTS_DIR/opt-c1x0
 
 #    echo opt-c1x0x
-#    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=opt-c1x0x > $RESULTS_DIR/opt-c1x0x
+#    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=opt-c1x0x > $RESULTS_DIR/opt-c1x0x
     
     echo opt-c1x1
-    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=opt-c1x1 > $RESULTS_DIR/opt-c1x1
+    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=opt-c1x1 > $RESULTS_DIR/opt-c1x1
 
     echo opt-c1x1x
-    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=opt-c1x1x > $RESULTS_DIR/opt-c1x1x
+    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=opt-c1x1x > $RESULTS_DIR/opt-c1x1x
 
     echo opt-c1x2
-    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=opt-c1x2 > $RESULTS_DIR/opt-c1x2
+    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=opt-c1x2 > $RESULTS_DIR/opt-c1x2
 
     echo opt-c1x2x
-    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=std -maxvm-config-alias=opt-c1x2x > $RESULTS_DIR/opt-c1x2x
+    $MAX gate ${VEE_TEST_OPTIONS} -maxvm-configs=${MAXVM_CONFIGS} -maxvm-config-alias=opt-c1x2x > $RESULTS_DIR/opt-c1x2x
 
 fi
