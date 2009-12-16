2823e4fe6a6219b310bc588bdd7e6a4bd80995c5
Mick Jordan <Mick.Jordan@sun.com>
2009-12-15 16:51:38 -0800

    merge

---
+++ VM/src/com/sun/max/vm/compiler/builtin/StackAllocate.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.vm.compiler.builtin;
+
+import com.sun.max.annotate.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.compiler.ir.*;
+
+/**
+ * A mechanism for allocating a block of memory within an activation frame for the lifetime of the frame.
+ *
+ * @author Doug Simon
+ * @author Paul Caprioli
+ */
+public final class StackAllocate extends SpecialBuiltin {
+
+    private StackAllocate() {
+        super(null);
+    }
+
+    @Override
+    public boolean isFoldable(IrValue[] arguments) {
+        return false;
+    }
+
+    @Override
+    public <IR_Type> void acceptVisitor(BuiltinVisitor<IR_Type> visitor, IR_Type result, IR_Type[] arguments) {
+        visitor.visitStackAllocate(this, result, arguments);
+    }
+
+    public static final StackAllocate BUILTIN = new StackAllocate();
+
+    /**
+     * Allocates a requested block of memory within the current activation frame.
+     * The allocated memory is reclaimed when the method returns.
+     *
+     * The allocation is actually performed upon method entry. That is, the compiler
+     * reserves the space in the compiled size of the frame. As such, a failure
+     * to allocate the requested space will result in a {@link StackOverflowError}
+     * when the method's prologue is executed.
+     *
+     * @param size bytes to allocate. This must be a constant.
+     * @return the address of the allocated block. <b>The contents of the block are uninitialized</b>.
+     */
+    @BUILTIN(builtinClass = StackAllocate.class)
+    public static native Pointer stackAllocate(int size);
+
+}
---
+++ VM/test/jtt/optimize/Inline02.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2009 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package jtt.optimize;
+
+/*
+ * @Harness: java
+ * @Runs: 0 = 2; 1 = 5; 2 = 8
+ */
+public class Inline02 {
+    public static int test(int arg) {
+        return arg + nobranch(true, arg) + nobranch(false, arg) + nobranch(true, arg) + nobranch(false, arg);
+    }
+
+    static int nobranch(boolean f, int v) {
+        if (f) {
+            return v;
+        }
+        return 1;
+    }
+}
