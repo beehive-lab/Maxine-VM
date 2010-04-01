07888920dd2c0969a4798af75aff74319e419136
Mick Jordan <Mick.Jordan@sun.com>
2010-03-31 17:41:40 -0700

    convert most @code's to @link's

--- C1X/src/com/sun/c1x/graph/BlockMap.java
+++ C1X/src/com/sun/c1x/graph/BlockMap.java
@@ -37,7 +37,7 @@ import com.sun.c1x.util.*;
  * compiler interface to provide a bitmap of the beginning of basic blocks. It makes two linear passes; one over the
  * bytecodes to build block starts and successor lists, and one pass over the block map to build the CFG.
  *
- * Note that the CFG built by this class is <i>not</i> connected to the actual {@code BlockBegin} instances; this class
+ * Note that the CFG built by this class is <i>not</i> connected to the actual {@link com.sun.c1x.ir.BlockBegin} instances; this class
  * does, however, compute and assign the reverse postorder number of the blocks. This comment needs refinement. (MJJ)
  *
  * <H2>More Details on {@link BlockMap#build}</H2>
@@ -45,7 +45,7 @@ import com.sun.c1x.util.*;
  * If the method has any exception handlers the {@link #exceptionMap} will be created (TBD).
  *
  * A {@link BlockBegin} node with the {@link com.sun.c1x.ir.BlockBegin.BlockFlag#StandardEntry} flag is created with bytecode index 0.
- * Note this is distinct from the similar {@code BlockBegin} node assigned to {@code startBlock} by
+ * Note this is distinct from the similar {@link com.sun.c1x.ir.BlockBegin} node assigned to {@code startBlock} by
  * {@link com.sun.c1x.graph.GraphBuilder}.
  *
  * The bytecodes are then scanned linearly looking for bytecodes that contain control transfers, e.g., {@code GOTO},
@@ -54,7 +54,7 @@ import com.sun.c1x.util.*;
  * More TBD.
  *
  * Observe that this process finds bytecodes that terminate basic blocks, so the {@link #moveSuccessorLists} method is
- * called to reassign the successors to the {@code BlockBegin} node that actually starts the block.
+ * called to reassign the successors to the {@link com.sun.c1x.ir.BlockBegin} node that actually starts the block.
  *
  * <H3>Example</H3>
  *
@@ -98,20 +98,20 @@ import com.sun.c1x.util.*;
  * </pre>
  *
  * There are seven basic blocks in this method, 0..2, 5..6, 9..12, 15..16, 19..19, 22..23 and 26..27. Therefore, before
- * the call to {@code moveSuccessorLists}, the {@code blockMap} array has {@code BlockBegin} nodes at indices 0, 5, 9,
- * 15, 19, 22 and 26. The {@code successorMap} array has entries at 2, 6, 12, 16, 23, 27 corresponding to the control
- * transfer bytecodes. The entry at index 6, for example, is a length two array of {@code BlockBegin} nodes for indices
- * 9 and 15, which are the successors for the basic block 5..6. After the call to {@code moveSuccessors}, {@code
- * successorMap} has entries at 0, 5, 9, 15, 19, 22 and 26, i.e, matching {@code blockMap}.
+ * the call to {@link #moveSuccessorLists}, the {@link #blockMap} array has {@link com.sun.c1x.ir.BlockBegin} nodes at indices 0, 5, 9,
+ * 15, 19, 22 and 26. The {@link #successorMap} array has entries at 2, 6, 12, 16, 23, 27 corresponding to the control
+ * transfer bytecodes. The entry at index 6, for example, is a length two array of {@link com.sun.c1x.ir.BlockBegin} nodes for indices
+ * 9 and 15, which are the successors for the basic block 5..6. After the call to {@link #moveSuccessorLists}, {@link
+ * #successorMap} has entries at 0, 5, 9, 15, 19, 22 and 26, i.e, matching {@link #blockMap}.
  * <p>
  * Next the blocks are numbered using <a href="http://en.wikipedia.org/wiki/Depth-first_search#Vertex_orderings">reverse
  * post-order</a>. For the above example this results in the numbering 2, 4, 7, 5, 6, 3, 8. Also loop header blocks are
  * detected during the traversal by detecting a repeat visit to a block that is still being processed. This causes the
- * block to be flagged as a loop header and also added to the {@link #loopBlocks} list. The {@code loopBlocks} list
+ * block to be flagged as a loop header and also added to the {@link #loopBlocks} list. The {@link #loopBlocks} list
  * contains the blocks at 0, 5, 9, 15, 19, 22, with 22 as the loop header. (N.B. the loop header block is added multiple
  * (4) times to this list). (Should 0 be in? It's not inside the loop).
  *
- * If the {@code computeStoresInLoops} argument to {@code build} is true, the {@code loopBlocks} list is processed to
+ * If the {@code computeStoresInLoops} argument to {@link #build} is true, the {@link #loopBlocks} list is processed to
  * mark all local variables that are stored in the blocks in the list.
  *
  *
@@ -205,7 +205,7 @@ public class BlockMap {
     private ArrayList<BlockBegin> loopBlocks;
     private ExceptionMap exceptionMap;
     private final int firstBlock;
-    /** Used for initial block ID (count up) and post-order number (count down) */
+    /** Used for initial block ID (count up) and post-order number (count down). */
     private int blockNum; //
 
     /**
--- C1X/src/com/sun/c1x/package-info.java
+++ C1X/src/com/sun/c1x/package-info.java
@@ -23,36 +23,40 @@
  * The top-level package in C1X containing options, metrics, timers and the main compiler class
  * {@link com.sun.c1x.C1XCompiler}.
  *
- * <H2>{@code C1XCompiler} Overview</H2>
+ * <H2>{@link com.sun.c1x.C1XCompiler} Overview</H2>
  *
  * C1X is intended to be used with multiple JVM's so makes no use of or reference to classes for a specific JVM, for
  * example Maxine.
  *
- * The compiler is represented by the class {@code C1XCompiler} which is a subclass of the abstract
- * {@link com.sun.c1x.ci.CiCompiler} class that defines {@link com.sun.c1x.ci.CiCompiler#compileMethod}. {@code
- * C1XCompiler} binds a specific target architecture and JVM interface to produce a usable compiler object. There are
- * two variants of {@code compileMethod}, one of which is used when doing <i>on stack replacement</i> (OSR), discussed
- * later. The main variant takes {@link com.sun.c1x.ri.RiMethod} and {@link com.sun.c1x.xir.RiXirGenerator} arguments.
- * {@code RiMethod} is C1X's representation of a Java method and {@code RiXirGenerator} represents the interface through
- * which the compiler requests the XIR for a given bytecode from the runtime system.
+ * The compiler is represented by the class {@link com.sun.c1x.C1XCompiler} which is a subclass of the abstract
+ * {@link com.sun.c1x.ci.CiCompiler} class that defines {@link com.sun.c1x.ci.CiCompiler#compileMethod}.
+ * {@link com.sun.c1x.C1XCompiler} binds a specific target architecture and JVM interface to produce a usable compiler
+ * object. There are two variants of {@link com.sun.c1x.ci.CiCompiler#compileMethod}, one of which is used when doing
+ * <i>on stack replacement</i> (OSR), discussed later. The main variant takes {@link com.sun.c1x.ri.RiMethod} and
+ * {@link com.sun.c1x.xir.RiXirGenerator} arguments. {@link com.sun.c1x.ri.RiMethod} is C1X's representation of a Java
+ * method and {@link com.sun.c1x.xir.RiXirGenerator} represents the interface through which the compiler requests the
+ * XIR for a given bytecode from the runtime system.
  *
  * <H3>The C1X Compilation Process</H3>
  *
- * {@code C1XCompiler.compileMethod} creates a {@code C1XCompilation} instance and then returns the result of calling its
- * {@link com.sun.c1x.C1XCompilation#compile} method. The {@code C1XCompilation} instance records whether {@code compileMethod}} was invoked with
- * the OSR variant, which is used later in the IR generation.
+ * {@link com.sun.c1x.ci.CiCompiler#compileMethod} creates a {@link com.sun.c1x.C1XCompilation} instance and then
+ * returns the result of calling its {@link com.sun.c1x.C1XCompilation#compile} method. The
+ * {@link com.sun.c1x.C1XCompilation} instance records whether {@link com.sun.c1x.ci.CiCompiler#compileMethod} was
+ * invoked with the OSR variant, which is used later in the IR generation.
  * <p>
- * While there is only one {@code C1XCompiler} instance, there may be several compilations proceeding concurrently, each of
- * which is represented by a unique {@code C1XCompilation} instance. The static method {@link com.sun.c1x.C1XCompilation#current}} returns the
- * {@code C1XCompilation} instance associated with the current thread, and is managed using a {@link java.lang.ThreadLocal} variable. It
- * is used when assigning the unique id that is used for tracing  output to an HIR node. Each {@code C1XCompilation} instance
- * has an associated {@link com.sun.c1x.ci.CiStatistics} object that accumulates information about the compilation process, but is also
- * used as a generator of, for example, basic block identifiers.
+ * While there is only one {@link com.sun.c1x.C1XCompiler} instance, there may be several compilations proceeding
+ * concurrently, each of which is represented by a unique {@link com.sun.c1x.C1XCompilation} instance. The static method
+ * {@link com.sun.c1x.C1XCompilation#current} returns the {@link com.sun.c1x.C1XCompilation} instance associated with
+ * the current thread, and is managed using a {@link java.lang.ThreadLocal} variable. It is used when assigning the
+ * unique id that is used for tracing output to an HIR node. Each {@link com.sun.c1x.C1XCompilation} instance has an
+ * associated {@link com.sun.c1x.ci.CiStatistics} object that accumulates information about the compilation process, but
+ * is also used as a generator of, for example, basic block identifiers.
  * <p>
- * The compilation begins by calling {@link com.sun.c1x.C1XCompilation#emitHIR}, which creates the high-level intermediate representation (HIR) from the
- * bytecodes of the method. The HIR is managed by the {@link com.sun.c1x.graph.IR} class, an instance of which is created by
- * {@code emitHR}, which then calls the {{@link com.sun.c1x.graph.IR#build}} method and returns the result. The {@code C1XCompilation} and {@code IR}
- * instances are are bi-directionally linked.
+ * The compilation begins by calling {@link com.sun.c1x.C1XCompilation#emitHIR}, which creates the high-level
+ * intermediate representation (HIR) from the bytecodes of the method. The HIR is managed by the
+ * {@link com.sun.c1x.graph.IR} class, an instance of which is created by {@link com.sun.c1x.C1XCompilation#emitHIR}, which
+ * then calls the {{@link com.sun.c1x.graph.IR#build} method and returns the result. The
+ * {@link com.sun.c1x.C1XCompilation} and {@link com.sun.c1x.graph.IR} instances are are bi-directionally linked.
  *
  *
  * <H2>Notes and Todos</H2> This is a collection of notes about the C1X compiler, including future directions,
@@ -63,26 +67,27 @@
  *
  * <ul>
  * <li>
- * The HIR nodes {@link com.sun.c1x.ir.UnsafePrefetch}, {@link com.sun.c1x.ir.UnsafePutObject}, etc should be replaced by uses of the newer
- * {@link com.sun.c1x.ir.LoadPointer} and {@link com.sun.c1x.ir.StorePointer} nodes. Currently, the unsafe nodes are only generated by
- * the creation of an OSR entry. Benefit: reduce the number of different IR nodes.</li>
+ * The HIR nodes {@link com.sun.c1x.ir.UnsafePrefetch}, {@link com.sun.c1x.ir.UnsafePutObject}, etc should be replaced
+ * by uses of the newer {@link com.sun.c1x.ir.LoadPointer} and {@link com.sun.c1x.ir.StorePointer} nodes. Currently, the
+ * unsafe nodes are only generated by the creation of an OSR entry. Benefit: reduce the number of different IR nodes.</li>
  *
  * <li>
  * Add a field to optionally store an {@link com.sun.c1x.ir.Info} object for each HIR node, and remove the
- * {@link com.sun.c1x.ir.Instruction#exceptionHandlers} field, the {@link com.sun.c1x.ir.Instruction#bci} field, and any fields to store the Java
- * frame state in subclasses. Benefit: saves space if most HIR nodes do not have exception handlers, a bci or Java frame
- * state. Removes virtual dispatch on accessing debug information for nodes. Allows any node, regardless of its type, to
- * have info attached.</li>
+ * {@link com.sun.c1x.ir.Instruction#exceptionHandlers} field, the {@link com.sun.c1x.ir.Instruction#bci} field, and any
+ * fields to store the Java frame state in subclasses. Benefit: saves space if most HIR nodes do not have exception
+ * handlers, a bci or Java frame state. Removes virtual dispatch on accessing debug information for nodes. Allows any
+ * node, regardless of its type, to have info attached.</li>
  *
  * <li>
- * Migrate all HIR nodes to use the immutable {@link com.sun.c1x.value.FrameStateInfo} for debugging information. The {@link com.sun.c1x.value.FrameState}
- * class is mutable and used throughout graph building. Benefit: {@code FrameStateInfo} would save both total space in
- * the IR graph prevent many bugs due to the mutability of {@code FrameState}.</li>
+ * Migrate all HIR nodes to use the immutable {@link com.sun.c1x.value.FrameStateInfo} for debugging information. The
+ * {@link com.sun.c1x.value.FrameState} class is mutable and used throughout graph building. Benefit: {@code
+ * FrameStateInfo} would save both total space in the IR graph prevent many bugs due to the mutability of {@code
+ * FrameState}.</li>
  *
  * <li>
- * Move the {@code FrameState} class to an inner class, or combine entirely, with the {@link com.sun.c1x.graph.GraphBuilder} class. After
- * the introduction of the {@code FrameStateInfo} into HIR nodes, the mutable value stack should only need to be
- * accessed from the graph builder.</li>
+ * Move the {@code FrameState} class to an inner class, or combine entirely, with the
+ * {@link com.sun.c1x.graph.GraphBuilder} class. After the introduction of the {@code FrameStateInfo} into HIR nodes,
+ * the mutable value stack should only need to be accessed from the graph builder.</li>
  *
  * </ul>
  *
@@ -94,14 +99,14 @@
  * <ul>
  * <li>
  * Deoptimization metadata. The locations of all local variables and stack values are not communicated back to the
- * runtime system through the {@link com.sun.c1x.ci.CiDebugInfo} class yet. Such values are known to the register allocator, and there
- * vestigial logic to compute them still there in the
+ * runtime system through the {@link com.sun.c1x.ci.CiDebugInfo} class yet. Such values are known to the register
+ * allocator, and there vestigial logic to compute them still there in the
  * {@link com.sun.c1x.alloc.LinearScan#computeDebugInfo} method. To complete this metadata, the
  * {@link com.sun.c1x.alloc.LinearScan} class must implement the {@link ValueLocator} interface and pass it to the
- * {@link com.sun.c1x.lir.LIRDebugInfo#createFrame} method after register allocation. The
- * resulting debug info will be fed back to the runtime system by the existing logic that calls
- * {@link com.sun.c1x.ci.CiTargetMethod#recordCall(int, Object, CiDebugInfo, byte[], boolean)} and other methods. Obviously the runtime
- * system will need to encode this metadata in a dense format, because it is huge.</li>
+ * {@link com.sun.c1x.lir.LIRDebugInfo#createFrame} method after register allocation. The resulting debug info will be
+ * fed back to the runtime system by the existing logic that calls
+ * {@link com.sun.c1x.ci.CiTargetMethod#recordCall(int, Object, CiDebugInfo, byte[], boolean)} and other methods.
+ * Obviously the runtime system will need to encode this metadata in a dense format, because it is huge.</li>
  *
  *
  * <li>
@@ -114,10 +119,11 @@
  *
  * </li>
  *
-  * <li>
+ * <li>
  * SPARC and other architecture support. There pretty well-delineated separation between the architecture-independent
  * part of LIR backend and the architecture-dependent, but the only implementation that current exists is the X86
- * backend ({@link com.sun.c1x.target.amd64.AMD64Backend}, {@link com.sun.c1x.target.amd64.AMD64LIRGenerator}, {@link com.sun.c1x.target.amd64.AMD64LIRAssembler}, etc).</li>
+ * backend ({@link com.sun.c1x.target.amd64.AMD64Backend}, {@link com.sun.c1x.target.amd64.AMD64LIRGenerator},
+ * {@link com.sun.c1x.target.amd64.AMD64LIRAssembler}, etc).</li>
  *
  * <li>
  * XIR for safepoints. The C1X backend should use XIR to get the code for safepoints, but currently it still uses the
@@ -142,26 +148,26 @@
  * entry sequence was never tested.</li>
  *
  * <li>
- * {@link com.sun.c1x.C1XIntrinsic Intrinsification} is the mechanism by which the compiler recognizes calls to special JDK or
- * runtime methods and replaces them with custom code. It is enabled by the {@link com.sun.c1x.C1XOptions#OptIntrinsify} compiler
- * option. The C1X backend has never been tested with intrinsified arithmetic or floating point operations. For best
- * performance, it should generate specialized machine code for arithmetic and floating point, perhaps using global
- * stubs for complex floating point operations. <br>
+ * {@link com.sun.c1x.C1XIntrinsic Intrinsification} is the mechanism by which the compiler recognizes calls to special
+ * JDK or runtime methods and replaces them with custom code. It is enabled by the
+ * {@link com.sun.c1x.C1XOptions#OptIntrinsify} compiler option. The C1X backend has never been tested with intrinsified
+ * arithmetic or floating point operations. For best performance, it should generate specialized machine code for
+ * arithmetic and floating point, perhaps using global stubs for complex floating point operations. <br>
  * <i>Note</i>: Folding of special intrinsified methods is supported, tested, and working. The runtime system may
  * register methods to be folded by using the
- * {@link com.sun.c1x.C1XIntrinsic#registerFoldableMethod(RiMethod, java.lang.reflect.Method)} call. When the compiler encounters a
- * call to such a registered method where the parameters are all constants, it invokes the supplied method with
- * reflection. If the reflective call produces a value and does not throw an exception, C1X replaces the call to the
- * method with the result.</li>
+ * {@link com.sun.c1x.C1XIntrinsic#registerFoldableMethod(RiMethod, java.lang.reflect.Method)} call. When the compiler
+ * encounters a call to such a registered method where the parameters are all constants, it invokes the supplied method
+ * with reflection. If the reflective call produces a value and does not throw an exception, C1X replaces the call to
+ * the method with the result.</li>
  * </ul>
  *
  * <h3>Broken features</h3>
  *
  * <ul>
  * <li>
- * {@link com.sun.c1x.opt.LoopPeeler Loop peeling} was written by Marcelo Cintra near the end of his internship. It was never completed
- * and should be considered broken. It only remains as a sketch of how loop peeling would be implemented in C1X, or in
- * case he would finish the implementation and test it.</li>
+ * {@link com.sun.c1x.opt.LoopPeeler Loop peeling} was written by Marcelo Cintra near the end of his internship. It was
+ * never completed and should be considered broken. It only remains as a sketch of how loop peeling would be implemented
+ * in C1X, or in case he would finish the implementation and test it.</li>
  *
  * <li>
  * Calls to global stubs should allocate space on the caller's stack. On AMD64 currently, calls to global stubs poke the
@@ -169,10 +175,10 @@
  * uninterruptible and works fine in the VM, signal handlers triggered when debugging or inspecting this code sequence
  * may destroy these values when the OS calls the signal handler. This requires knowing which global stubs are called
  * before finalizing the frame size; currently only the calls to
- * {@link com.sun.c1x.target.amd64.AMD64MacroAssembler#callRuntimeCalleeSaved}
- * do not fit this pattern. This needs to be fixed so that all global stubs that are called by the assembled code are
- * known before beginning assembling. The {@link com.sun.c1x.target.amd64.AMD64GlobalStubEmitter} controls how the global stubs accept their
- * parameters. See {@link com.sun.c1x.target.amd64.AMD64GlobalStubEmitter#callerFrameContainsArguments} and its usages.
+ * {@link com.sun.c1x.target.amd64.AMD64MacroAssembler#callRuntimeCalleeSaved} do not fit this pattern. This needs to be
+ * fixed so that all global stubs that are called by the assembled code are known before beginning assembling. The
+ * {@link com.sun.c1x.target.amd64.AMD64GlobalStubEmitter} controls how the global stubs accept their parameters. See
+ * {@link com.sun.c1x.target.amd64.AMD64GlobalStubEmitter#callerFrameContainsArguments} and its usages.
  *
  * </li>
  * </ul>
