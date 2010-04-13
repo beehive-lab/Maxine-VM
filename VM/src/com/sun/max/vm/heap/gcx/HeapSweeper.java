adbe4c759f31c32269e5e3f4df7a87ed3b52d51f
ldayne <ldayne@localhost>
2010-04-13 10:08:41 +0200

    Missing file from last commit.

---
+++ VM/src/com/sun/max/vm/heap/gcx/HeapSweeper.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.vm.heap.gcx;
+
+import com.sun.max.unsafe.*;
+
+/**
+ * Heap Sweeper abstract class. Passed to HeapMarker instances to sweep the heap.
+ *
+ * @author Laurent Daynes.
+ */
+public abstract class HeapSweeper {
+    /**
+     * Invoked by the heap marker on the first black object following the pointer last returned by this method.
+     * @param liveObject a pointer to a live cell in the heap
+     * @return a pointer to the position in the heap where to resume sweeping.
+     */
+    public abstract Pointer processLiveObject(Pointer liveObject);
+
+    /**
+     * Process potential interesting gap in heap.
+     * Imprecise heap sweeping ignores any space before two live objects smaller than a specified amount of space.
+     * When the distance between two live marks is large enough to indicate a potentially large chunk of free space,
+     * the sweeper invoke this method.
+     *
+     * @param leftLiveObject
+     * @param rightLiveObject
+     * @return
+     */
+    public abstract Pointer processLargeGap(Pointer leftLiveObject, Pointer rightLiveObject);
+
+    /**
+     *
+     * @param freeChunk
+     * @param size
+     */
+    public abstract void recordFreeSpace(Address freeChunk, Size size);
+}
