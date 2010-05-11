6078b2d0ba126bf800b9e349fab3578f879b2044
Puneet Lakhina <Puneet.Lakhina@sun.com>
2010-05-11 16:36:27 -0700

    Merge

--- Native/platform/guestvmXen_db.h
+++ Native/platform/guestvmXen_db.h
@@ -43,53 +43,53 @@
 struct db_thread {
     int id;
     int flags;
-    unsigned long stack;
-    unsigned long stack_size;
+    uint64_t stack;
+    uint64_t stack_size;
 };
 
 struct db_regs {
-    unsigned long xmm0;
-    unsigned long xmm1;
-    unsigned long xmm2;
-    unsigned long xmm3;
-    unsigned long xmm4;
-    unsigned long xmm5;
-    unsigned long xmm6;
-    unsigned long xmm7;
-    unsigned long xmm8;
-    unsigned long xmm9;
-    unsigned long xmm10;
-    unsigned long xmm11;
-    unsigned long xmm12;
-    unsigned long xmm13;
-    unsigned long xmm14;
-    unsigned long xmm15;
-    unsigned long r15;
-    unsigned long r14;
-    unsigned long r13;
-    unsigned long r12;
-    unsigned long rbp;
-    unsigned long rbx;
-    unsigned long r11;
-    unsigned long r10;
-    unsigned long r9;
-    unsigned long r8;
-    unsigned long rax;
-    unsigned long rcx;
-    unsigned long rdx;
-    unsigned long rsi;
-    unsigned long rdi;
-    unsigned long rip;
-    unsigned long flags;
-    unsigned long rsp;
+    uint64_t xmm0;
+    uint64_t xmm1;
+    uint64_t xmm2;
+    uint64_t xmm3;
+    uint64_t xmm4;
+    uint64_t xmm5;
+    uint64_t xmm6;
+    uint64_t xmm7;
+    uint64_t xmm8;
+    uint64_t xmm9;
+    uint64_t xmm10;
+    uint64_t xmm11;
+    uint64_t xmm12;
+    uint64_t xmm13;
+    uint64_t xmm14;
+    uint64_t xmm15;
+    uint64_t r15;
+    uint64_t r14;
+    uint64_t r13;
+    uint64_t r12;
+    uint64_t rbp;
+    uint64_t rbx;
+    uint64_t r11;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+    uint64_t rax;
+    uint64_t rcx;
+    uint64_t rdx;
+    uint64_t rsi;
+    uint64_t rdi;
+    uint64_t rip;
+    uint64_t flags;
+    uint64_t rsp;
 };
 
 int db_attach(int domain_id);
 int db_detach(void);
-uint64_t read_u64(unsigned long address);
-void write_u64(unsigned long address, uint64_t value);
-uint16_t readbytes(unsigned long address, char *buffer, uint16_t n);
-uint16_t writebytes(unsigned long address, char *buffer, uint16_t n);
+uint64_t read_u64(uint64_t address);
+void write_u64(uint64_t address, uint64_t value);
+uint16_t readbytes(uint64_t address, char *buffer, uint16_t n);
+uint16_t writebytes(uint64_t address, char *buffer, uint16_t n);
 uint16_t multibytebuffersize(void);
 struct db_thread* gather_threads(int *num);
 int suspend(uint16_t thread_id);
@@ -99,10 +99,10 @@ int resume_all(void);
 int single_step(uint16_t thread_id);
 struct db_regs* get_regs(uint16_t thread_id);
 struct thread_state* get_thread_state(uint16_t thread_id);
-int set_ip(uint16_t thread_id, unsigned long ip);
+int set_ip(uint16_t thread_id, uint64_t ip);
 int get_thread_stack(uint16_t thread_id,
-                     unsigned long *stack_start,
-                     unsigned long *stack_size);
+                     uint64_t *stack_start,
+                     uint64_t *stack_size);
 uint64_t app_specific1(uint64_t arg);
 int db_debug(int level);
 void db_signoff(void);
@@ -111,8 +111,8 @@ void db_signoff(void);
 #define WRITE_W 2
 #define EXEC_W 4
 #define AFTER_W 8
-int activate_watchpoint(unsigned long address, unsigned long size, int kind);
-int deactivate_watchpoint(unsigned long address, unsigned long size);
-unsigned long watchpoint_info(uint16_t thread_id, int *kind);
+int activate_watchpoint(uint64_t address, uint64_t size, int kind);
+int deactivate_watchpoint(uint64_t address, uint64_t size);
+uint64_t watchpoint_info(uint16_t thread_id, int *kind);
 
 #endif /* __LIB_GUESTVMXEN_DB__ */
