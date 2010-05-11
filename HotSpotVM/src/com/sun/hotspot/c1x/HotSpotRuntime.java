456e327bd2488187511038f5109978bb94ea71f1
Thomas Wuerthinger <thomas.wuerthinger@gmail.com>
2010-05-11 19:23:00 +0200

    Can now call the compiler from the VM. See HotSpotVM/README.txt for details about the command line arguments.

--- HotSpotVM/src/com/sun/hotspot/c1x/HotSpotRuntime.java
+++ HotSpotVM/src/com/sun/hotspot/c1x/HotSpotRuntime.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2009 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
 package com.sun.hotspot.c1x;
 
 import java.io.OutputStream;
@@ -11,6 +31,13 @@ import com.sun.cri.ri.RiRuntime;
 import com.sun.cri.ri.RiSnippets;
 import com.sun.cri.ri.RiType;
 
+/**
+ * 
+ * @author Thomas Wuerthinger
+ * 
+ * CRI runtime implementation for the HotSpot VM.
+ *
+ */
 public class HotSpotRuntime implements RiRuntime {
 
 	@Override
