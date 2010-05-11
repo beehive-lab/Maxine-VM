1813026dadf8c49d2d31b59c8e18d688edf4770e
ldayne <ldayne@localhost>
2010-05-11 11:19:57 +0200

    Added more GC traces for MS debugging.

---
+++ CPS/src/com/sun/max/vm/cps/cir/gui/CirStyledDocument.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.vm.cps.cir.gui;
+
+import static com.sun.max.collect.SequenceBag.MapType.*;
+
+import javax.swing.text.*;
+
+import com.sun.max.collect.*;
+import com.sun.max.util.*;
+import com.sun.max.vm.cps.cir.*;
+
+/**
+ * A styled document derived from an annotated CIR trace.
+ *
+ * @author Doug Simon
+ * @author Sumeet Panchal
+ */
+class CirStyledDocument extends DefaultStyledDocument {
+
+    public int collapsedOffset;
+    public CirAnnotatedTrace.ParenthesisElement collapsedDual;
+    public CirAnnotatedTrace cirAnnotatedTrace;
+    public IntHashMap<CirAnnotatedTrace.Element> offsetToElement = new IntHashMap<CirAnnotatedTrace.Element>();
+    public Bag<CirNode, CirAnnotatedTrace.Element, Sequence<CirAnnotatedTrace.Element>> elementsPerNode = new SequenceBag<CirNode, CirAnnotatedTrace.Element>(IDENTITY);
+
+    public CirStyledDocument(CirAnnotatedTrace cirAnnotatedTrace) {
+        this.collapsedOffset = -1;
+        this.collapsedDual = null;
+        this.cirAnnotatedTrace = cirAnnotatedTrace;
+        if (cirAnnotatedTrace == null) {
+            try {
+                insertString(0, "NO VISUALIZATION AVAILABLE", null);
+            } catch (BadLocationException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        for (final CirAnnotatedTrace.Element element : cirAnnotatedTrace) {
+            final CirNode node = element.node();
+            if (node != null) {
+                elementsPerNode.add(node, element);
+            }
+            element.visitRanges(new RangeVisitor() {
+                public void visitRange(Range range) {
+                    for (int i = range.start(); i != range.end(); ++i) {
+                        offsetToElement.put(i, element);
+                    }
+                }
+            });
+        }
+    }
+
+    public CirAnnotatedTrace.Element elementAt(int offset) {
+        return offsetToElement.get(offset);
+    }
+
+    public Sequence<CirAnnotatedTrace.Element> occurrences(CirAnnotatedTrace.Element element) {
+        if (element.node() == null) {
+            return new ArraySequence<CirAnnotatedTrace.Element>(element);
+        }
+        final Sequence<CirAnnotatedTrace.Element> occurrences = elementsPerNode.get(element.node());
+        return occurrences;
+    }
+}
---
+++ Tele/src/com/sun/max/tele/MaxStackFrame.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
+ *
+ * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
+ * that is described in this document. In particular, and without limitation, these intellectual property
+ * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
+ * more additional patents or pending patent applications in the U.S. and in other countries.
+ *
+ * U.S. Government Rights - Commercial software. Government users are subject to the Sun
+ * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
+ * supplements.
+ *
+ * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
+ * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
+ * U.S. and other countries.
+ *
+ * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
+ * Company, Ltd.
+ */
+package com.sun.max.tele;
+
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.compiler.target.*;
+import com.sun.max.vm.stack.*;
+import com.sun.max.vm.stack.StackFrameWalker.*;
+
+/**
+ * Description of a stack frame in the VM.
+ *
+ * @author Michael Van De Vanter
+ */
+public interface MaxStackFrame extends MaxEntity<MaxStackFrame> {
+
+    /**
+     * Gets the stack containing this frame.
+     * <br>
+     * Thread-safe
+     *
+     * @return the stack that contains this frame
+     */
+    MaxStack stack();
+
+    /**
+     * Gets the position of this frame in the stack.
+     * <br>
+     * Thread-safe
+     *
+     * @return the position of this frame in its stack. The top frame is at position 0;
+     * @see #stack()
+     */
+    int position();
+
+    /**
+     * Determines whether this frame is at the top of it's stack, position.
+     * <br>
+     * Thread-safe
+     *
+     * @return whether this frame is at the top of the stack.
+     */
+    boolean isTop();
+
+    /**
+     * Gets this frame's Instruction Pointer.
+     * <br>
+     * Thread-safe
+     *
+     * @return the address of the next instruction to be executed in this frame.
+     * @see Cursor#ip()
+     */
+    Pointer ip();
+
+    /**
+     * Gets this frame's Stack Pointer.
+     * <br>
+     * Thread-safe
+     *
+     * @return the current stack pointer
+     * @see Cursor#sp()
+     */
+    Pointer sp();
+
+    /**
+     * Gets this frame's Frame Pointer.
+     * <br>
+     * Thread-safe
+     *
+     * @return the current frame pointer
+     * @see Cursor#fp()
+     */
+    Pointer fp();
+
+    /**
+     * Get's the conceptual location in code for each frame, independent of underlying
+     * stack representation.
+     * <br>
+     * The location for the top frame in a stack is the instruction pointer.
+     * <br>
+     * The location for other frames in the stack is the return address:  the next address to
+     * be executed when control returns to this frame.
+     *
+     * @return the conceptual code location for this frame:  IP or return pointer
+     */
+    MaxCodeLocation codeLocation();
+
+    // TODO (mlvdv) eliminate use of the VM class return type TargetMethod
+    /**
+     * Gets the compiled method enclosing the {@linkplain #ip() execution point} in this frame, if any.
+     * <br>
+     * Thread-safe
+     *
+     * @return null if this is a frame of a native function or other special frame not associated with a method
+     */
+    TargetMethod targetMethod();
+
+
+    // TODO (mlvdv) temporary method; replace with a call to get memory region on the method class
+    // when there is one to replace use of TargetMethod.
+    MaxMemoryRegion getTargetMethodMemoryRegion();
+
+    /**
+     * Determines if this frame and another refer to the same frame.
+     * <br>
+     * Thread-safe
+     *
+     * @param stackFrame another stack frame.
+     * @return whether the two refer to the same frame
+     */
+    boolean isSameFrame(MaxStackFrame stackFrame);
+
+    /**
+     * A stack frame holding the activation of a compiled method.
+     */
+    public static interface Compiled extends MaxStackFrame {
+
+        CompiledStackFrameLayout layout();
+
+        /**
+         * Gets the base address of all stack slots. This provides a convenience for stack frame visitors that need to see all stack slot as
+         * located at a positive offset from some base pointer (e.g., stack inspectors etc...)
+         * By default this is the frame pointer.
+         */
+        Pointer slotBase();
+
+        /**
+         * Computes the biased offset from the slot base to the ABI's frame pointer register.
+         * <br>
+         * Some platforms (e.g., Solaris SPARC v9 in 64-bit mode) use a bias from the frame pointer to access stack slot.
+         * <br>
+         * The default is no bias, but the offset must be adjusted by frame size to be relative to the frame pointer.
+         *
+         * @param offset a offset relative to the address of the stack pointer
+         * @return the biased offset, relative to the frame pointer register.
+         */
+        Offset biasedFPOffset(Offset offset);
+
+        /**
+         * Returns the stack bias used by the stack frame. By default, it returns {@link StackBias#NONE}.
+         * @return a stack bias.
+         */
+        StackBias bias();
+
+    }
+
+    /**
+     * A stack frame holding the activation of native code about which little is known.
+     */
+    public static interface Native extends MaxStackFrame {
+    }
+
+    /**
+     * A synthetic stack frame (non-VM) used to record an error
+     * encountered during stack walking.
+     */
+    public static interface Error extends MaxStackFrame {
+
+        /**
+         * Description of an error encountered during stack walking.
+         *
+         * @return a message describing the nature of the error
+         */
+        String errorMessage();
+    }
+}
