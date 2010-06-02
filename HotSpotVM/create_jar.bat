f2be9c2cb3b9d9309a03dc03023348101f6be2be
Doug Simon <doug.simon@oracle.com>
2010-06-02 15:43:36 +0200

    Removed MockVM and HotSpotVM projects. They are now in a separate repository.

--- HotSpotVM/create_jar.bat
+++
@@ -1,5 +0,0 @@
-jar cf c1x4hotspot.jar -C bin .
-jar uf c1x4hotspot.jar -C ../C1X/bin .
-jar uf c1x4hotspot.jar -C ../CRI/bin .
-jar uf c1x4hotspot.jar -C ../Assembler/bin .
-jar uf c1x4hotspot.jar -C ../Base/bin .
