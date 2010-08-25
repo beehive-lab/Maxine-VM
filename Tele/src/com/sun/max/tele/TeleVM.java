af6aca4581cbe77a53cce9c1a2b04868e5b9f40d
Michael Van De Vanter <michael.van.de.vanter@oracle.com>
2010-08-25 15:39:16 -0700

    Inspector: fix spelling errors

--- Tele/src/com/sun/max/tele/TeleVM.java
+++ Tele/src/com/sun/max/tele/TeleVM.java
@@ -246,7 +246,7 @@ public abstract class TeleVM implements MaxVM {
     private static TargetLocation targetLocation;
 
     /**
-     * Where the meta-data associated with the targte VM is located {@see #vmDirectoryOption}.
+     * Where the meta-data associated with the target VM is located {@see #vmDirectoryOption}.
      */
     private static File vmDirectory;
 
@@ -309,8 +309,8 @@ public abstract class TeleVM implements MaxVM {
     }
 
     /**
-     * Create the correctr instance of {@link TeleChannelProtocol} based on {@link #targetLocation} and
-     * {@link PperatingSystem}.
+     * Create the correct instance of {@link TeleChannelProtocol} based on {@link #targetLocation} and
+     * {@link OperatingSystem}.
      *
      * @param operatingSystem
      */
