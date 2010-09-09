ba5c1c13386f882c56e2ec40c4121856f82cf3c8
Michael Duller <michael.duller@oracle.com>
2010-09-08 23:27:16 -0700

    Removed debug output from assumeLeafMethod as it caused problems within a Maxine image due to circularity.

--- C1X/src/com/sun/c1x/graph/GraphBuilder.java
+++ C1X/src/com/sun/c1x/graph/GraphBuilder.java
@@ -2428,9 +2428,7 @@ public final class GraphBuilder {
                 return true;
             }
             if (C1XOptions.UseDeopt && C1XOptions.OptLeafMethods) {
-                System.err.println("assumeLeafMethod for " + method.holder().name() + method.name() + ": isOverridden: " + method.isOverridden() + ", holder().isInterface: " + method.holder().isInterface());
                 if (!method.isOverridden() && !method.holder().isInterface()) {
-                    System.err.println("WE ARE HERE");
                     return compilation.recordLeafMethodAssumption(method);
                 }
             }
