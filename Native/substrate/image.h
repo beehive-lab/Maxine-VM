b7152ecf56ead1c099e9a97ad6d1a762e83ae838
Mick Jordan <Mick.Jordan@sun.com>
2010-09-09 21:13:13 -0700

    Merge

--- Native/substrate/image.h
+++ Native/substrate/image.h
@@ -52,6 +52,9 @@
     f(codeSize) /* multiple of 'pageSize'  */ \
     f(dynamicHeapRegionsArrayOffset) \
     f(auxiliarySpaceSize) /* Some extra space that the substrate allocates by malloc(). Used e.g. for the primordial card table.  */ \
+    f(reservedVirtualSpaceSize) /* Amount of contiguous virtual space to reserve at boot image load-time  */ \
+    f(reservedVirtualSpaceFieldOffset) /* offset where to store the address of the reserved contiguous virtual space, if any*/ \
+    f(bootRegionMappingConstraint) \
     f(threadLocalsListHeadOffset) /* See the comment for the 'threadLocalsListHead' field in the VmThreadMap class.  */ \
     f(primordialThreadLocalsOffset) \
     f(threadLocalsAreaSize) /* The size of a thread locals area.  */ \
