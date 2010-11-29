7f8a7e85dde1d41738f2f14b87ec25f18d869385
Thomas Wuerthinger <wuerthinger@ssw.jku.at>
2010-11-29 17:48:52 +0100

    Fixed a critical bug in the phi simplifier (dead phis were not marked as such causing compiler bailouts in the register allocator).

--- C1X/src/com/sun/c1x/opt/PhiSimplifier.java
+++ C1X/src/com/sun/c1x/opt/PhiSimplifier.java
@@ -77,6 +77,7 @@ public class PhiSimplifier implements BlockClosure {
             phi.setFlag(Value.Flag.PhiVisited);
             Value phiSubst = null;
             int max = phi.inputCount();
+            boolean cannotSimplify = false;
             for (int i = 0; i < max; i++) {
                 Value oldInstr = phi.inputAt(i);
 
@@ -88,19 +89,25 @@ public class PhiSimplifier implements BlockClosure {
                 }
 
                 // attempt to simplify this operand
-                Value newInstr = simplify(oldInstr);
+                if (!cannotSimplify) {
+                    Value newInstr = simplify(oldInstr);
 
-                if (newInstr != phi && newInstr != phiSubst) {
-                    if (phiSubst == null) {
-                        phiSubst = newInstr;
-                        continue;
+                    if (newInstr != phi && newInstr != phiSubst) {
+                        if (phiSubst == null) {
+                            phiSubst = newInstr;
+                            continue;
+                        }
+                        // this phi cannot be simplified
+                        cannotSimplify = true;
                     }
-                    // this phi cannot be simplified
-                    phi.setFlag(Value.Flag.PhiCannotSimplify);
-                    phi.clearFlag(Value.Flag.PhiVisited);
-                    return phi;
                 }
             }
+            if (cannotSimplify) {
+                phi.setFlag(Value.Flag.PhiCannotSimplify);
+                phi.clearFlag(Value.Flag.PhiVisited);
+                return phi;
+            }
+
             // successfully simplified the phi
             assert phiSubst != null : "illegal phi function";
             phi.clearFlag(Value.Flag.PhiVisited);
