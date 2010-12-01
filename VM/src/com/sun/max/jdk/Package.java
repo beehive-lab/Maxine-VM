092742ab199155bf652aaf434dd1b9c1bb8db9dc
Doug Simon <doug.simon@oracle.com>
2010-12-01 11:11:25 +0100

    Merge.

--- VM/src/com/sun/max/jdk/Package.java
+++ VM/src/com/sun/max/jdk/Package.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright (c) 2007 Sun Microsystems, Inc.  All rights reserved.
  *
  * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
  * that is described in this document. In particular, and without limitation, these intellectual property
@@ -18,13 +18,11 @@
  * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
  * Company, Ltd.
  */
-package com.sun.max.ext.c1x;
+package com.sun.max.jdk;
 
-import com.sun.max.ext.*;
 
-public class Package extends ExtPackage {
-
+public class Package extends JDKPackage {
     public Package() {
-        super("com.sun.cri", "com.sun.c1x");
+
     }
 }
