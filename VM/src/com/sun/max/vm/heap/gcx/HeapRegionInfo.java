724d6edc849334bea3515f21f2e3a1f062544135
Laurent Daynes <laurent.daynes@oracle.com>
2010-12-02 17:32:02 +0100

    Fixed missing name in new vm option.

--- VM/src/com/sun/max/vm/heap/gcx/HeapRegionInfo.java
+++ VM/src/com/sun/max/vm/heap/gcx/HeapRegionInfo.java
@@ -38,7 +38,7 @@ import com.sun.max.vm.runtime.*;
  * @author Laurent Daynes
  */
 public class HeapRegionInfo {
-    public static final VMSizeOption regionSizeOption = register(new VMSizeOption("-XX:", Size.K.times(256), "Heap Region Size"), MaxineVM.Phase.PRISTINE);
+    public static final VMSizeOption regionSizeOption = register(new VMSizeOption("-XX:HeapRegionSize", Size.K.times(256), "Heap Region Size"), MaxineVM.Phase.PRISTINE);
 
     @CONSTANT_WHEN_NOT_ZERO
     static int regionSizeInBytes;
