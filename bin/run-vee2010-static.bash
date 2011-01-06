d8eb77d5cf0ed2f50396a1d28fcbf0d280f05094
Mick Jordan <Mick.Jordan@sun.com>
2011-01-05 19:17:53 -0800

    fix last modified copyright dates

--- bin/run-vee2010-static.bash
+++ bin/run-vee2010-static.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-# Copyright (c) 2009, 2009, Oracle and/or its affiliates. All rights reserved.
+# ----------------------------------------------------------------------------------------------------
+#
+# Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # This code is free software; you can redistribute it and/or modify it
@@ -20,6 +22,8 @@
 # Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
 # or visit www.oracle.com if you need additional information or have any
 # questions.
+#
+# ----------------------------------------------------------------------------------------------------
 #
 
 test -n "$JUNIT4_CP"         || export JUNIT4_CP=/proj/maxwell/bin/junit4.jar
