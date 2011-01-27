4f29466b80d32574832fd96b757f39c6b679b1f7
Michael Haupt <michael.haupt@hpi.uni-potsdam.de>
2011-01-27 18:37:46 +0100

    Copyright fix

--- VM/src/com/sun/max/vm/hosted/BootImageObjectTree.java
+++ VM/src/com/sun/max/vm/hosted/BootImageObjectTree.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,16 +22,11 @@
  */
 package com.sun.max.vm.hosted;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GraphicsEnvironment;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Rectangle;
-
+import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
+import java.util.List;
 
 import javax.swing.*;
 import javax.swing.event.*;
