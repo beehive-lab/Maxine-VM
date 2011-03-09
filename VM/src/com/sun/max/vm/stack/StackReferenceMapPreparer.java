f3945422b5dc56489ce7a4cc30f3182f2c4cc4e3
Doug Simon <doug.simon@oracle.com>
2011-03-09 22:17:41 +0100

    Copyright fix.

--- VM/src/com/sun/max/vm/stack/StackReferenceMapPreparer.java
+++ VM/src/com/sun/max/vm/stack/StackReferenceMapPreparer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -102,7 +102,7 @@ public final class StackReferenceMapPreparer {
         return Heap.traceRootScanning() || (TraceSRS && TraceSRSSuppressionCount <= 0);
     }
 
-    public static boolean VerifyRefMaps = true;
+    public static boolean VerifyRefMaps;
     static {
         VMOptions.addFieldOption("-XX:", "VerifyRefMaps", StackReferenceMapPreparer.class,
             "Verify reference maps by performing a stack walk and checking plausibility of reference roots in " +
