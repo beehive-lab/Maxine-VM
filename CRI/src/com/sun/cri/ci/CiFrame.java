4a1de152cf1b17f580a5257288fcc9d8a6e60f79
Doug Simon <doug.simon@oracle.com>
2011-03-21 15:43:04 +0100

    Removed CPS compiler and derived JIT compiler.
    Removed canonicalization of resolution guards in ConstantPool.
    
    Removed com.sun.max.vm.compiler.builtin package.
    
    Removed com.sun.max.vm.compiler.snippet package.
    
    Removed @UNSAFE, @SNIPPET and @BUILTIN annotations.
    
    Removed ClassMethodActor.originalCodeAttribute field.
    
    Added explicit null checks to T1X templates for resolved non-static invokes.

---
+++ CRI/src/com/sun/cri/ci/CiFrame.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.cri.ci;
+
+import java.util.*;
+
+import com.sun.cri.ri.*;
+
+/**
+ * Represents the Java bytecode frame(s) at a given position
+ * including {@link CiValue locations} where to find the values of each local variable
+ * and stack slot of the bytecode frame(s).
+ */
+public class CiFrame extends CiCodePos {
+    /**
+     * An array of values representing how to reconstruct the state of the Java frame.
+     * Entries
+     * {@code [0 - numLocals)} represent the Java local variables,
+     * {@code [numLocals, numLocals + numStack)} the Java operand stack, and entries
+     * {@code [numLocals + numStack, values.length)} the list of acquired monitors.
+     * Note that the number of locals and the number of stack slots may be smaller than the
+     * maximum number of locals and stack slots as specified in the compiled method.
+     */
+    public final CiValue[] values;
+
+    /**
+     * The number of locks in the values array.
+     */
+    public final int numLocks;
+
+    /**
+     * The number of locals in the values array.
+     */
+    public final int numLocals;
+
+    /**
+     * The number of stack slots in the values array.
+     */
+    public final int numStack;
+
+    public CiFrame(CiFrame caller, RiMethod method, int bci, CiValue[] values, int numLocals, int numStack, int numLocks) {
+        super(caller, method, bci);
+        this.values = values;
+        this.numLocks = numLocks;
+        this.numLocals = numLocals;
+        this.numStack = numStack;
+    }
+
+    /**
+     * Gets the value representing the specified local variable.
+     * @param i the local variable index
+     * @return the value that can be used to reconstruct the local's current value
+     */
+    public CiValue getLocalValue(int i) {
+        return values[i];
+    }
+
+    /**
+     * Gets the value representing the specified stack slot.
+     * @param i the stack index
+     * @return the value that can be used to reconstruct the stack slot's current value
+     */
+    public CiValue getStackValue(int i) {
+        return values[i + numLocals];
+    }
+
+    /**
+     * Gets the value representing the specified lock.
+     * @param i the lock index
+     * @return the value that can be used to reconstruct the lock's current value
+     */
+    public CiValue getLockValue(int i) {
+        return values[i + numLocals + numStack];
+    }
+
+    /**
+     * Gets the caller of this frame.
+     * 
+     * @return {@code null} if this frame has no caller
+     */
+    public CiFrame caller() {
+        return (CiFrame) caller;
+    }
+    
+    /**
+     * Deep equality test.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof CiFrame) {
+            CiFrame other = (CiFrame) obj;
+            if (method.equals(other.method) &&
+                other.bci == bci &&
+                numLocals == other.numLocals &&
+                numStack == other.numStack &&
+                numLocks == other.numLocks &&
+                Arrays.equals(values, other.values)) {
+                if (caller == null) {
+                    return other.caller == null;
+                }
+                return caller.equals(other.caller);
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    public String toString() {
+        return CiUtil.append(new StringBuilder(100), this).toString();
+    }
+}
---
+++ VM/src/com/sun/max/vm/compiler/target/amd64/AMD64TargetMethodUtil.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.vm.compiler.target.amd64;
+
+import com.sun.c1x.target.amd64.*;
+import com.sun.max.lang.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.*;
+import com.sun.max.vm.compiler.*;
+import com.sun.max.vm.compiler.target.*;
+import com.sun.max.vm.runtime.*;
+
+/**
+ * A utility class factoring out code common to all AMD64 target method.
+ *
+ * @author Bernd Mathiske
+ * @author Paul Caprioli
+ */
+public final class AMD64TargetMethodUtil {
+    private static final int RCALL = 0xe8;
+    private static final int RJMP = 0xe9;
+
+    /**
+     * Lock to avoid race on concurrent icache invalidation when patching target methods.
+     */
+    private static final Object PatchingLock = new Object();
+
+    public static int registerReferenceMapSize() {
+        return Unsigned.idiv(AMD64.cpuRegisters.length, Bytes.WIDTH);
+    }
+
+    public static boolean isPatchableCallSite(Address callSite) {
+        // We only update the disp of the call instruction.
+        // C1X imposes that disp of the call be aligned to a word boundary.
+        // This may cause up to 7 nops to be inserted before a call.
+        final Address endOfCallSite = callSite.plus(DIRECT_METHOD_CALL_INSTRUCTION_LENGTH - 1);
+        return callSite.plus(1).isWordAligned() ? true :
+        // last byte of call site:
+        callSite.roundedDownBy(8).equals(endOfCallSite.roundedDownBy(8));
+    }
+
+    /**
+     * Patches the offset operand of a 32-bit relative CALL instruction.
+     *
+     * @param targetMethod the method containing the CALL instruction
+     * @param callOffset the offset within the code of {@code targetMethod} of the CALL to be patched
+     * @param destination the absolute target address of the CALL
+     */
+    public static void fixupCall32Site(TargetMethod targetMethod, int callOffset, Address destination) {
+        fixupCode(targetMethod, callOffset, destination.asAddress(), RCALL);
+    }
+
+    /**
+     * Set up a forwarding jump from an old copy of a method to a new one.
+     * @param oldTargetMethod
+     * @param newTargetMethod
+     */
+    public static void forwardTo(TargetMethod oldTargetMethod, TargetMethod newTargetMethod) {
+        assert oldTargetMethod != newTargetMethod;
+        assert oldTargetMethod.callEntryPoint != CallEntryPoint.C_ENTRY_POINT;
+
+        final Address newOptEntry = CallEntryPoint.OPTIMIZED_ENTRY_POINT.in(newTargetMethod).asAddress();
+        final Address newJitEntry = CallEntryPoint.JIT_ENTRY_POINT.in(newTargetMethod).asAddress();
+
+        // FIXME: Shouldn't these be made mt-safe using some variant of mtSafePatchCallSite ?
+        fixupCode(oldTargetMethod, CallEntryPoint.OPTIMIZED_ENTRY_POINT.offset(), newOptEntry, RJMP);
+        fixupCode(oldTargetMethod, CallEntryPoint.JIT_ENTRY_POINT.offset(), newJitEntry, RJMP);
+    }
+
+    private static final long DIRECT_METHOD_CALL_INSTRUCTION_LENGTH = 5L;
+
+    private static void fixupCode(TargetMethod targetMethod, int offset, Address target, int controlTransferOpcode) {
+        final Pointer callSite = targetMethod.codeStart().plus(offset);
+        long displacement = target.minus(callSite.plus(DIRECT_METHOD_CALL_INSTRUCTION_LENGTH)).toLong();
+        FatalError.check((int) displacement == displacement, "Code displacement out of 32-bit range");
+        displacement = displacement & 0xFFFFFFFFL;
+        if (MaxineVM.isHosted()) {
+            final byte[] code = targetMethod.code();
+            code[offset] = (byte) controlTransferOpcode;
+            code[offset + 1] = (byte) displacement;
+            code[offset + 2] = (byte) (displacement >> 8);
+            code[offset + 3] = (byte) (displacement >> 16);
+            code[offset + 4] = (byte) (displacement >> 24);
+        } else {
+            // Don't care about any particular alignment here. Can fixup any control of transfer code as there isn't concurrency issues.
+            callSite.writeByte(0, (byte) controlTransferOpcode);
+            callSite.writeByte(1, (byte) displacement);
+            callSite.writeByte(2, (byte) (displacement >> 8));
+            callSite.writeByte(3, (byte) (displacement >> 16));
+            callSite.writeByte(4, (byte) (displacement >> 24));
+        }
+    }
+
+    // MT-safe replacement of the displacement of a direct call.
+    public static void mtSafePatchCallDisplacement(TargetMethod targetMethod, Pointer callSite, Address target) {
+        if (!isPatchableCallSite(callSite)) {
+            FatalError.unexpected(" invalid patchable call site:  " + callSite.toHexString());
+        }
+        long displacement = target.minus(callSite.plus(DIRECT_METHOD_CALL_INSTRUCTION_LENGTH)).toLong();
+        FatalError.check((int) displacement == displacement, "Code displacement out of 32-bit range");
+        displacement = displacement & 0xFFFFFFFFL;
+        synchronized (PatchingLock) {
+            // Just to prevent concurrent writing and invalidation to the same instruction cache line
+            // (although the lock excludes ALL concurrent patching)
+            callSite.writeInt(1,  (int) displacement);
+            // Don't need icache invalidation to be correct (see AMD64's Architecture Programmer Manual Vol.2, p173 on self-modifying code)
+        }
+    }
+
+    // Disable instance creation.
+    private AMD64TargetMethodUtil() {
+    }
+}
