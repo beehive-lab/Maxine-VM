9d5a734c480b4e31950a9ee25aab3f261db02419
ldayne <Laurent.Daynes@oracle.com>
2011-05-17 19:02:31 +0200

    Fixed incorrect implementation for removeHead.

---
+++ com.oracle.max.base/src/com/sun/max/util/ArrayValueHistory.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.util;
+
+import java.util.*;
+
+import com.sun.max.program.*;
+
+/**
+ * An array-based recording of the history of a value, with
+ * time expressed as the number of generations back from the current generation (0).
+ */
+public class ArrayValueHistory<E> {
+
+    private final ArrayDeque<E> generations;
+    private final int limit;
+    private int age = -1;
+
+    public ArrayValueHistory(int limit) {
+        this.generations = new ArrayDeque<E>();
+        this.limit = limit;
+    }
+
+    public ArrayValueHistory() {
+        this (Integer.MAX_VALUE);
+    }
+
+    /**
+     * Adds a new value, which becomes the current generation.
+     * The generation of all previously recorded values increases by 1.
+     */
+    public void add(E newValue) {
+        if (generations.size() > 0) {
+            if (newValue.equals(generations.getFirst())) {
+                if (age >= 0) {
+                    age++;
+                }
+            } else {
+                age = 0;
+            }
+        }
+        generations.addFirst(newValue);
+        if (generations.size() > limit) {
+            generations.removeLast();
+        }
+    }
+
+    /**
+     * @return the "current" value (at generation 0).
+     * Error if no values have been recorded.
+     */
+    public E get() {
+        if (generations.size() > 0) {
+            return generations.getFirst();
+        }
+        ProgramError.unexpected("empty history");
+        return null;
+    }
+
+    /**
+     * @return The value at a specified generation.
+     * Error if generation does not exist.
+     */
+    public E get(int generation) {
+        final Iterator<E> iterator = generations.iterator();
+        int index = 0;
+        while (iterator.hasNext()) {
+            if (index == generation) {
+                return iterator.next();
+            }
+            index++;
+        }
+        ProgramError.unexpected("exceeded history");
+        return null;
+    }
+
+    /**
+     * @return the age, in generations, of the current value, since recording began.
+     * 0 if different from immediate predecessor; -1 if no different value ever recorded
+     * Comparison uses {@linkplain Object#equals(Object) equals}.
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @return the maximum number of generations that can be recorded.
+     */
+    public int getLimit() {
+        return limit;
+    }
+
+    /**
+     * @return the number of generations recorded; initially 0.
+     */
+    public int getSize() {
+        return generations.size();
+    }
+
+    /**
+     * @return iteration of the values recorded in the history, starting with the current
+     * generation and proceeding backward in time.
+     */
+    public Iterator<E> values() {
+        return generations.iterator();
+    }
+
+}
---
+++ com.oracle.max.elf/src/com/oracle/max/elf/ELFLoader.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+/**
+ * Copyright (c) 2006, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the University of California, Los Angeles nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Created Sep 30, 2006
+ */
+package com.oracle.max.elf;
+
+import java.io.RandomAccessFile;
+import java.io.IOException;
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * The <code>ELFLoader</code> class presents a facade to users of the ELF
+ * classes that permits a simpler usage model. This class contains a number of
+ * utility methods to load sections and symbol tables from ELF files.
+ *
+ * @author Ben L. Titzer
+ */
+public final class ELFLoader {
+
+    private ELFLoader() {
+    }
+
+    /**
+     * The <code>readELFHeader()</code> method loads an ELF header data structure
+     * from the specified random access file. This method checks for consistency; i.e.
+     * that the header contains the special ELF magic, and throws a format error if
+     * the file does not match the ELF specification.
+     * @param fis the random access file from which to read the header
+     * @return a reference to a new <code>ELFHeader</code> instance representing the header
+     * @throws IOException if an IO exception occurred
+     * @throws ELFHeader.FormatError if the header does not comply with the ELF specification
+     */
+    public static ELFHeader readELFHeader(RandomAccessFile fis) throws IOException, ELFHeader.FormatError {
+        final ELFHeader header = new ELFHeader();
+        header.read(fis);
+        return header;
+    }
+
+    /**
+     * The <code>readPHT()</code> method reads the program header table from the specified file.
+     * @param fis the file from which to read the program header table
+     * @param header the ELFHeader instance already loaded from this file
+     * @return a reference to a new object representing the program header table for this ELF file
+     * @throws IOException if an IO exception occurs
+     */
+    public static ELFProgramHeaderTable readPHT(RandomAccessFile fis, ELFHeader header) throws IOException {
+        final ELFProgramHeaderTable pht = new ELFProgramHeaderTable(header);
+        pht.read(fis);
+        return pht;
+    }
+
+    /**
+     * The <code>readSHT()</code> method loads the section header table from the specified file.
+     * @param fis the file from which to load the section header table
+     * @param header the ELF header corresponding to this file
+     * @return a reference to a new object that represents the section header table
+     * @throws IOException if an IO exception occurs
+     */
+    public static ELFSectionHeaderTable readSHT(RandomAccessFile fis, ELFHeader header) throws IOException {
+        final ELFSectionHeaderTable sht = new ELFSectionHeaderTable(header);
+        sht.read(fis);
+
+        // read the ELF string table that contains the section names
+        if (header.e_shstrndx < sht.entries.length) {
+            final ELFSectionHeaderTable.Entry e = sht.entries[header.e_shstrndx];
+            final ELFStringTable srttab = new ELFStringTable(header, e);
+            srttab.read(fis);
+            sht.setStringTable(srttab);
+        }
+        return sht;
+    }
+
+    /**
+     * The <code>readSymbolTables()</code> method reads a list of symbol tables from the ELF
+     * file, if any exist.
+     * @param fis the file from which to load the symbol tables
+     * @param header the ELF header for this file
+     * @param sht the section header table for this file, which is used to locate string tables
+     * @return a list of symbol tables that are contained in this ELF file
+     * @throws IOException if an IO exception occurs
+     */
+    public static List<ELFSymbolTable> readSymbolTables(RandomAccessFile fis, ELFHeader header, ELFSectionHeaderTable sht) throws IOException {
+        final List<ELFSymbolTable> symbolTables = new LinkedList<ELFSymbolTable>();
+        for (int cntr = 0; cntr < sht.entries.length; cntr++) {
+            final ELFSectionHeaderTable.Entry e1 = sht.entries[cntr];
+            if (e1.isSymbolTable()) {
+                final ELFSymbolTable stab = new ELFSymbolTable(header, e1);
+                stab.read(fis);
+                symbolTables.add(stab);
+                final ELFSectionHeaderTable.Entry strent = sht.entries[e1.getLink()];
+                if (strent.isStringTable()) {
+                    final ELFStringTable str = new ELFStringTable(header, strent);
+                    str.read(fis);
+                    stab.setStringTable(str);
+                }
+            }
+        }
+        return symbolTables;
+    }
+}
