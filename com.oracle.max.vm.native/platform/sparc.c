0fc34a9cdab5982dfd95d4bca408016e5d64febf
Christian Wimmer <Christian.Wimmer@Oracle.com>
2011-06-15 16:14:15 -0700

    Cache for foldable methods yielded wrong results: When a cache hit for the method was found, but the arguments were different, then the wrong cache entry was updated after computing the result.  Subsequent cache lookups thus returned the wrong result.  Additionally, the cache was not thread-safe because the result was written into the cache entry outside of the synchronized block, i.e., for a short time frame the cache contained an element with an invalid result.

---
+++ com.oracle.max.vm.native/platform/sparc.c
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+#include "isa.h"
+#include "log.h"
+
+void isa_canonicalizeTeleIntegerRegisters(isa_OsTeleIntegerRegisters os, isa_CanonicalIntegerRegisters c) {
+#if os_SOLARIS
+    // See procfs_isa.h
+    int r = R_G0;
+    Word *p = &c->g0;
+    while (r <= R_I7) {
+        p[r] = (Word) os[r];
+        r++;
+    }
+#else
+    c_UNIMPLEMENTED();
+#endif
+}
+
+void isa_canonicalizeTeleStateRegisters(isa_OsTeleStateRegisters os, isa_CanonicalStateRegisters c) {
+#if os_SOLARIS
+    c->ccr = (Word) os[R_CCR];
+    c->pc = (Word) os[R_PC];
+    c->npc = (Word) os[R_nPC];
+#else
+    c_UNIMPLEMENTED();
+#endif
+}
+
+void isa_canonicalizeTeleFloatingPointRegisters(isa_OsTeleFloatingPointRegisters os, isa_CanonicalFloatingPointRegisters c) {
+#if os_SOLARIS
+    int r = 0;
+    while (r < 32) {
+        c->dRegs[r] = (Word) os->pr_fr.pr_regs[r];
+        r++;
+    }
+#else
+    c_UNIMPLEMENTED();
+#endif
+}
+
+void isa_printCanonicalIntegerRegisters(isa_CanonicalIntegerRegisters c) {
+#if os_SOLARIS
+    // See procfs_isa.h
+    static char registerNames[] = "GOLI";
+    int r = R_G0;
+    Word *p = &c->g0;
+    while (r <= R_I7) {
+        char rn = registerNames[r / 8];
+        for (int i = 0; i < 8; i++, r++) {
+            log_println("%%%c%d = %p [%ld]", rn, i, p[r], p[r]);
+        }
+    }
+#else
+    c_UNIMPLEMENTED();
+#endif
+}
+
+void isa_printCanonicalFloatingPointRegisters(isa_CanonicalFloatingPointRegisters canonicalFloatingPointRegisters) {
+#if os_SOLARIS
+#define PRINT_FPR(r) log_println("F%-2d = %p [%lf]", r, canonicalFloatingPointRegisters->dRegs[r], canonicalFloatingPointRegisters->dRegs[r])
+    int r = 0;
+    while (r < 32) {
+        PRINT_FPR(r);
+        r++;
+    }
+#else
+    c_UNIMPLEMENTED();
+#endif
+}
+
+void isa_printCanonicalStatePointRegisters(isa_CanonicalStateRegisters canonicalStateRegisters) {
+#if os_SOLARIS
+    log_println("%%ccr = %p [%ld]", canonicalStateRegisters->ccr, canonicalStateRegisters->ccr);
+    log_println("%%pc  = %p [%ld]", canonicalStateRegisters->pc, canonicalStateRegisters->pc);
+    log_println("%%npc = %p [%ld]", canonicalStateRegisters->npc, canonicalStateRegisters->npc);
+#else
+    c_UNIMPLEMENTED();
+#endif
+}
---
+++ com.oracle.max.vm/src/com/sun/max/vm/monitor/modal/modehandlers/HashableLockword64.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.vm.monitor.modal.modehandlers;
+
+import static com.sun.cri.bytecode.Bytecodes.*;
+
+import com.sun.cri.bytecode.*;
+import com.sun.max.annotate.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.*;
+
+/**
+ * Abstracts access to a lock word's hashcode bit field.
+ *
+ * @see ModalLockword64
+ */
+public abstract class HashableLockword64 extends ModalLockword64 {
+
+    /*
+     * Field layout:
+     *
+     * bit [63............................... 1  0]
+     *
+     *     [     Undefined      ][ hashcode ][m][s]
+     *
+     */
+
+    protected static final int HASH_FIELD_WIDTH = 32;
+    protected static final int HASHCODE_SHIFT = NUMBER_OF_MODE_BITS;
+    protected static final Address HASHCODE_SHIFTED_MASK = Word.allOnes().asAddress().unsignedShiftedRight(64 - HASH_FIELD_WIDTH);
+
+    protected HashableLockword64() {
+    }
+
+    /**
+     * Prints the monitor state encoded in a {@code HashableLockword64} to the {@linkplain Log log} stream.
+     */
+    public static void log(HashableLockword64 lockword) {
+        Log.print("HashableLockword64: ");
+        if (lockword.isInflated()) {
+            Log.print("inflated=true");
+        } else {
+            Log.print("inflated=false");
+            Log.print(" hash=");
+            Log.print(lockword.getHashcode());
+        }
+    }
+
+    /**
+     * Boxing-safe cast of a {@code Word} to a {@code HashableLockword64}.
+     *
+     * @param word the word to cast
+     * @return the cast word
+     */
+    @INTRINSIC(UNSAFE_CAST)
+    public static HashableLockword64 from(Word word) {
+        return new BoxedHashableLockword64(word);
+    }
+
+    /**
+     * Gets the value of this lock word's hashcode field.
+     *
+     * @return the hashcode
+     */
+    @INLINE
+    public final int getHashcode() {
+        return asAddress().unsignedShiftedRight(HASHCODE_SHIFT).and(HASHCODE_SHIFTED_MASK).toInt();
+    }
+
+    /**
+     * Installs the given hashcode into a <i>copy</i> of this {@code HashableLockword64}. The copied
+     * lock word is returned.
+     *
+     * Note: It is assumed that this lock word does not contain an existing hashcode.
+     *
+     * @param hashcode the hashcode to install
+     * @return a copy of this {@code HashableLockword64} with the installed hashcode
+     */
+    @INLINE
+    public final HashableLockword64 setHashcode(int hashcode) {
+        return HashableLockword64.from(asAddress().or(Address.fromUnsignedInt(hashcode).shiftedLeft(HASHCODE_SHIFT)));
+    }
+}
