a655804b6559d11e36d425a26843976e5bace975
Christian Wimmer <Christian.Wimmer@Oracle.com>
2011-08-03 11:57:47 -0700

    Preserve system property os.version from boot image creation time so that there is at least a decent default value (until we recompute the real value during Maxine startup).

---
+++ com.oracle.max.vm/src/com/sun/max/unsafe/BoxedAddress.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.unsafe;
+
+import java.math.*;
+
+import com.sun.max.annotate.*;
+
+/**
+ * Boxed version of Address.
+ *
+ * @see Address
+ */
+@HOSTED_ONLY
+public final class BoxedAddress extends Address implements Boxed {
+
+    private long nativeWord;
+
+    public static final BoxedAddress ZERO = new BoxedAddress(0);
+    public static final BoxedAddress MAX = new BoxedAddress(-1L);
+
+    private static final class Cache {
+        private Cache() {
+        }
+
+        static final int HIGHEST_VALUE = 1000;
+
+        static final BoxedAddress[] cache = new BoxedAddress[HIGHEST_VALUE + 1];
+
+        static {
+            for (int i = 0; i < cache.length; i++) {
+                cache[i] = new BoxedAddress(i);
+            }
+        }
+    }
+
+    public static BoxedAddress from(long value) {
+        if (value == 0) {
+            return ZERO;
+        }
+        if (value >= 0 && value <= Cache.HIGHEST_VALUE) {
+            return Cache.cache[(int) value];
+        }
+        if (value == -1L) {
+            return MAX;
+        }
+        return new BoxedAddress(value);
+    }
+
+    public static BoxedAddress from(int value) {
+        return from(value & BoxedWord.INT_MASK);
+    }
+
+    private BoxedAddress(long value) {
+        if (Word.width() == 64) {
+            nativeWord = value;
+        } else {
+            nativeWord = value & BoxedWord.INT_MASK;
+        }
+    }
+
+    public long value() {
+        return nativeWord;
+    }
+
+    private static BigInteger bi(long unsigned) {
+        if (unsigned < 0) {
+            long signBit = 0x8000000000000000L;
+            long low63Bits = unsigned & ~signBit;
+            return BigInteger.valueOf(low63Bits).setBit(63);
+        }
+        return BigInteger.valueOf(unsigned);
+    }
+
+
+    private static long unsignedDivide(long dividend, long divisor) {
+        if (dividend >= 0 && divisor >= 0) {
+            return dividend / divisor;
+        }
+        return bi(dividend).divide(bi(divisor)).longValue();
+    }
+
+    @Override
+    public Address dividedByAddress(Address divisor) {
+        final BoxedAddress box = (BoxedAddress) divisor.asAddress();
+        if (box.nativeWord == 0L) {
+            throw new ArithmeticException();
+        }
+        return new BoxedAddress(unsignedDivide(nativeWord, box.nativeWord));
+    }
+
+    @Override
+    public Address dividedByInt(int divisor) {
+        if (divisor == 0) {
+            throw new ArithmeticException();
+        }
+        return new BoxedAddress(unsignedDivide(nativeWord, divisor & BoxedWord.INT_MASK));
+    }
+
+    private static long unsignedRemainder(long dividend, long divisor) {
+        if (dividend >= 0 && divisor >= 0) {
+            return dividend % divisor;
+        }
+        return bi(dividend).remainder(bi(divisor)).longValue();
+    }
+
+    @Override
+    public Address remainderByAddress(Address divisor) {
+        final BoxedAddress box = (BoxedAddress) divisor.asAddress();
+        if (box.nativeWord == 0L) {
+            throw new ArithmeticException();
+        }
+        return new BoxedAddress(unsignedRemainder(nativeWord, box.nativeWord));
+    }
+
+    @Override
+    public int remainderByInt(int divisor) {
+        if (divisor == 0) {
+            throw new ArithmeticException();
+        }
+        return (int) unsignedRemainder(nativeWord, divisor & BoxedWord.INT_MASK);
+    }
+}
---
+++ com.oracle.max.vma.tools/src/com/oracle/max/vma/tools/gen/vma/runtime/TransientVMAdviceHandlerTypesGenerator.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.max.vma.tools.gen.vma.runtime;
+
+import static com.sun.max.vm.t1x.T1XTemplateGenerator.*;
+import static com.oracle.max.vma.tools.gen.vma.AdviceGeneratorHelper.*;
+
+import java.lang.reflect.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+import com.oracle.max.vm.ext.vma.*;
+
+
+public class TransientVMAdviceHandlerTypesGenerator {
+
+    private static final String ADVISE_BEFORE = "adviseBefore";
+    private static final String ADVISE_AFTER = "adviseAfter";
+    private static final String ADVICE_RECORD = "AdviceRecord";
+    private static boolean first = true;
+    public static SortedMap<String, Method> enumtoMethod = new TreeMap<String, Method>();
+    public static Map<Method, String> methodToEnum = new HashMap<Method, String>();
+    public static Map<String, String> enumToRecordName = new HashMap<String, String>();
+    public static Map<String, ArrayList<String>> recordToEnumList = new HashMap<String, ArrayList<String>>();
+
+    public static void main(String[] args) {
+        createGenerator(TransientVMAdviceHandlerTypesGenerator.class);
+        generateAutoComment();
+        for (Method m : VMAdviceHandler.class.getMethods()) {
+            if (m.getName().startsWith("advise")) {
+                generateEnum(m);
+                first = false;
+            }
+        }
+        out.printf(";%n%n");
+        generateNewAdviceRecord();
+    }
+
+    public static void createEnumMaps() {
+        for (Method m : VMAdviceHandler.class.getMethods()) {
+            if (m.getName().startsWith("advise")) {
+                createEnum(m);
+            }
+        }
+    }
+
+    private static String createEnum(Method m) {
+        String name = removeAdvise(m.getName());
+        String lastParam = getLastParameterName(m);
+        if (name.equals("ConstLoad") || name.endsWith("Store") || name.endsWith("Conversion") ||
+                        name.contains("Return") ||  name.contains("PutField") ||
+                        name.contains("PutStatic") || name.contains("Operation")) {
+            if (lastParam != null) {
+                name += toFirstUpper(lastParam);
+            }
+        } else if (name.contains("GetField") || name.contains("GetStatic")) {
+            if (m.getParameterTypes().length > 2) {
+                name += toFirstUpper(lastParam);
+            }
+        } else if (name.endsWith("If")) {
+            name += toFirstUpper(lastParam);
+        }
+        methodToEnum.put(m, name);
+        if (enumtoMethod.get(name) != null) {
+            return null;
+        }
+        enumtoMethod.put(name, m);
+        return name;
+    }
+
+    private static void generateEnum(Method m) {
+        String name = createEnum(m);
+        if (name == null) {
+            return;
+        }
+        if (!first) {
+            out.printf(",%n");
+        }
+        out.printf("        %s", name);
+    }
+
+    public static void generateRecordToEnumList() {
+        for (Entry<String, Method> e : enumtoMethod.entrySet()) {
+            String name = e.getKey();
+            Method m = e.getValue();
+            String lastParam = getLastParameterName(m);
+            String uLastParam = lastParam == null ? "" : toFirstUpper(lastParam);
+
+            String adviceRecordName = "";
+            if (name.contains("ConstLoad") || name.startsWith("Store") || name.contains("Conversion") ||
+                            name.contains("Return")) {
+                adviceRecordName = uLastParam;
+            } else if (name.contains("GetField") || name.contains("GetStatic")) {
+                adviceRecordName = "Object";
+            } else if (name.contains("PutField") || name.contains("PutStatic") ||
+                            name.startsWith("ArrayStore")) {
+                adviceRecordName = "Object" + uLastParam;
+            } else if (name.contains("Operation")) {
+                adviceRecordName = uLastParam + uLastParam;
+            } else if (name.equals("IfInt") || name.equals("IInc")) {
+                adviceRecordName = "LongLong";
+            } else if (name.equals("IfObject") || name.equals("CheckCast") || name.equals("InstanceOf") ||
+                            name.endsWith("MultiNewArray")) {
+                adviceRecordName = "ObjectObject";
+            } else if (name.endsWith("ArrayLength") || name.endsWith("Throw") ||
+                            name.contains("Monitor") || name.contains("New") || name.startsWith("ArrayLoad")) {
+                adviceRecordName = "Object";
+            } else if (name.contains("Invoke") || name.contains("MethodEntry")) {
+                adviceRecordName = "ObjectMethod";
+            }
+            adviceRecordName += ADVICE_RECORD;
+            ArrayList<String> list = recordToEnumList.get(adviceRecordName);
+            if (list == null) {
+                list = new ArrayList<String>();
+                recordToEnumList.put(adviceRecordName, list);
+            }
+            list.add(name);
+            enumToRecordName.put(name, adviceRecordName);
+        }
+    }
+
+    private static void generateNewAdviceRecord() {
+        generateRecordToEnumList();
+        out.printf("        public AdviceRecord newAdviceRecord() {%n");
+        out.printf("            switch (this) {%n");
+        for (Map.Entry<String, ArrayList<String>> entry : recordToEnumList.entrySet()) {
+            for (String record : entry.getValue()) {
+                out.printf("                case %s:%n", record);
+            }
+            out.printf("                    return new %s();%n", entry.getKey());
+        }
+    }
+
+    private static String removeAdvise(String name) {
+        int ix = name.indexOf(ADVISE_BEFORE);
+        if (ix >= 0) {
+            return name.substring(ADVISE_BEFORE.length());
+        }
+        ix = name.indexOf(ADVISE_AFTER);
+        if (ix >= 0) {
+            return name.substring(ADVISE_AFTER.length());
+        }
+        return name;
+    }
+}
