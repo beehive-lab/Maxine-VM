ffbdf2e35baaa00acebcf96c04d360c0b84de80f
Doug Simon <doug.simon@oracle.com>
2011-09-05 21:18:44 +0200

    Redesigned T1X to separate operand stack and local variable access out of the templates. This has a number of consequences: o T1XFrameOps is gone. o The intrinsics for manipulating the stack and frame pointer could be deleted. o A number of templates have been removed. This includes:   - bytecodes that simply move data within a stack frame (e.g. *LOAD, *STORE, DUP*, SWAP)   - bytecodes that push constants onto the stack (e.g. ICONST*, LDC)   - the IINC bytecode   - the branch bytecodes. Removing these meant the ICMP and WCMP intrinsics could be deleted as     they existed purely for being able to set condition codes from within Java. o The invoke* templates now simply do method resolution and/or selection. The call instruction   is emitted directly by T1X. This meant the TEMPLATE_CALL intrinsic could be deleted. o The representation of the frame state can now be changed without having to modify the templates. o The templates are now closer to snippets. o Bytecode instrumentation/interposition/advising is now no longer possible for all bytecodes by simply implementing different templates. o The templates no longer need to worry about maintaining complicated GC invariants (i.e. T1XTemplateChecker is gone).
    Most of the code in T1XTemplateSource is (still) generated by T1XTemplateGenerator. There is a new 'max t1xgen' command to
    (re)run the generator. The generator is also run during boot image generation to ensure that the auto-generated
    content in T1XTemplateSource is in sync.
    
    T1XCompilation is now an abstract class with all platform-specific functionality delegated to a subclass.

---
+++ com.oracle.max.jdwp/src/com/sun/max/jdwp/data/JDWPInputStream.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.sun.max.jdwp.data;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Stream class for reading JDWP values. It has a child input stream to which the actual reads are delegated.
+ *
+ * @author Thomas Wuerthinger
+ */
+public class JDWPInputStream {
+
+    private DataInputStream dataInputStream;
+    private CommandHandler<IncomingData, OutgoingData> commandHandler;
+    private IncomingData incomingData;
+
+    public JDWPInputStream(InputStream inputStream, CommandHandler<IncomingData, OutgoingData> handler, IncomingData incomingData) {
+        this.dataInputStream = new DataInputStream(inputStream);
+        this.commandHandler = handler;
+        this.incomingData = incomingData;
+    }
+
+    public boolean readBoolean() throws IOException {
+        return dataInputStream.readBoolean();
+    }
+
+    public byte readByte() throws IOException {
+        return dataInputStream.readByte();
+    }
+
+    public int readInt() throws IOException {
+        return dataInputStream.readInt();
+    }
+
+    public long readLong() throws IOException {
+        return dataInputStream.readLong();
+    }
+
+    public InputStream getInputStream() {
+        return dataInputStream;
+    }
+
+    /**
+     * Reads a string according to JDWP syntax.
+     * @return the string read from the JDWP stream
+     * @throws IOException this exception is thrown, when there was a problem reading the raw bytes
+     */
+    public String readString() throws IOException {
+
+        final int length = dataInputStream.readInt();
+
+        final byte[] data = new byte[length];
+        dataInputStream.read(data);
+
+        final String s = new String(data);
+        return s;
+    }
+
+    public JDWPLocation readLocation() throws IOException {
+        return new JDWPLocation(dataInputStream);
+    }
+
+    /**
+     * Reads a object identifier with a preceding tag (see {@link com.sun.max.jdwp.constants.Tag}).
+     * @return the read object identifier
+     * @throws IOException
+     */
+    public ID.ObjectID readTaggedObjectReference() throws IOException {
+
+        // Read over tag, this information is currently not used.
+        dataInputStream.readByte();
+
+        return ID.read(dataInputStream, ID.ObjectID.class);
+    }
+
+    public JDWPValue readValue() throws IOException {
+        return new JDWPValue(dataInputStream);
+    }
+
+    /**
+     * Reads a JDWPValue that is not preceded with a tag (see {@link com.sun.max.jdwp.constants.Tag}). The registered command handler
+     * is used to resolve this information based on the semantics of the command.
+     *
+     * @return the read JDWPValue object
+     * @throws IOException this exception is thrown, when there was a problem reading the raw bytes of the value
+     * @throws JDWPException this exception is thrown, when the command handler had a problem resolving the tag
+     */
+    public JDWPValue readUntaggedValue() throws IOException, JDWPException {
+        return new JDWPValue(dataInputStream, commandHandler.helpAtDecodingUntaggedValue(incomingData));
+    }
+
+    /**
+     * Reads a list of values from the stream. This function is not yet implemented, because it is currently not needed in the protocol.
+     * @return a list of values read from the stream
+     * @throws IOException this exception is thrown, when there was a problem reading the raw bytes of the value
+     * @throws JDWPException this exception is thrown, when the command handler had a problem resolving tags
+     */
+    public List<? extends JDWPValue> readArrayRegion() throws IOException, JDWPException {
+        throw new JDWPNotImplementedException();
+    }
+}
---
+++ com.oracle.max.vm.native/javatest/tests.c
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+/**
+ * JNI code for any of the JavaTester tests that use native methods.
+ */
+#include "os.h"
+
+#include <pthread.h>
+#include "jni.h"
+
+JNIEXPORT void JNICALL
+Java_test_output_MixedFrames_nativeUpdateFields(JNIEnv *env, jobject object, int n, jint i, jobject o) {
+    jclass thisClass = (*env)->GetObjectClass(env, object);
+    if (thisClass == NULL) {
+        printf("Could not get class of object\n");
+        return;
+    }
+    //printf("[native] n = %d\n", n);
+    //fflush(stdout);
+
+    jmethodID mid = (*env)->GetMethodID(env, thisClass, "testNative", "(I)V");
+    if (mid == NULL) {
+        printf("Could not find method testNative(int)\n");
+        return;
+    }
+    if (n == 0) {
+        jfieldID iField;
+        jfieldID oField;
+        iField = (*env)->GetFieldID(env, thisClass, "i", "I");
+        if (iField == NULL) {
+            printf("Could not find field i\n");
+            return;
+        }
+
+        oField = (*env)->GetFieldID(env, thisClass, "o", "Ljava/lang/Object;");
+        if (oField == NULL) {
+            printf("Could not find field o\n");
+            return;
+        }
+
+        (*env)->SetIntField(env, object, iField, i);
+        (*env)->SetObjectField(env, object, oField, o);
+    }
+    (*env)->CallVoidMethod(env, object, mid, n, i, o);
+}
+
+void upcall(jclass cls) {
+    JavaVM *vm;
+    jsize vmBufLen = 1;
+    jsize nVMs;
+    JNIEnv *env;
+    JavaVMAttachArgs attachArgs;
+    jmethodID mid;
+    jstring jstr;
+
+    attachArgs.version = JNI_VERSION_1_2;
+    attachArgs.name = "pthread";
+    attachArgs.group = NULL;
+
+    JNI_GetCreatedJavaVMs(&vm, vmBufLen, &nVMs);
+    int result = (*vm)->AttachCurrentThread(vm, (void **)&env, &attachArgs);
+    if (result != JNI_OK) {
+        fprintf(stderr, "Could not attach to VM: error=%d\n", result);
+        return;
+    }
+
+    mid = (*env)->GetStaticMethodID(env, cls, "helloWorld", "(Ljava/lang/String;)V");
+    if (mid == 0) {
+        fprintf(stderr, "Can't find method helloWorld(String)\n");
+        return;
+    }
+
+    jstr = (*env)->NewStringUTF(env, "(from upcall)");
+    (*env)->CallStaticVoidMethod(env, cls, mid, jstr);
+
+    (*env)->DeleteGlobalRef(env, cls);
+    (*vm)->DetachCurrentThread(vm);
+}
+
+void *thread_function(void *arguments) {
+    upcall((jclass) arguments);
+    return NULL;
+}
+
+JNIEXPORT void JNICALL
+Java_test_output_AttachThread_callHelloWorldOnAttachedThread(JNIEnv *env, jclass clazz) {
+    pthread_t thread_id;
+    pthread_attr_t attributes;
+
+    /* Convert argument to be a global handle as it is going to the new thread */
+    clazz = (*env)->NewGlobalRef(env, clazz);
+    void *arguments = clazz;
+
+    pthread_attr_init(&attributes);
+    pthread_attr_setdetachstate(&attributes, PTHREAD_CREATE_JOINABLE);
+    pthread_create(&thread_id, &attributes, thread_function, arguments);
+    pthread_attr_destroy(&attributes);
+}
