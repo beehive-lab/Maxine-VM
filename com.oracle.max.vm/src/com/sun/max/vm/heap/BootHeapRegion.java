6a39d222439ad03b3e25f93a399b77a0ffa3bbe1
ldayne <Laurent.Daynes@oracle.com>
2011-09-08 15:21:54 +0200

    Solaris too requires a 16-bit stack alignment for native function that pass floating point... This fixes batik on Solaris which crashed in  JNI call to NewObject.

---
+++ com.oracle.max.vm/src/com/sun/max/vm/heap/BootHeapRegion.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.vm.heap;
+
+import com.sun.max.annotate.*;
+import com.sun.max.lang.*;
+import com.sun.max.program.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.*;
+import com.sun.max.vm.object.*;
+import com.sun.max.vm.reference.*;
+
+/**
+ * The special region describing the heap in the boot image.
+ */
+public class BootHeapRegion extends LinearAllocatorHeapRegion {
+
+    private byte[] referenceMapBytes;
+
+    private Pointer referenceMap;
+
+    private java.lang.ref.Reference[] specialReferences = {};
+
+    public BootHeapRegion(Address start, Size size, String description) {
+        super(start, size, description);
+    }
+
+    @HOSTED_ONLY
+    public void init(byte[] refMap, java.lang.ref.Reference[] specialRefs) {
+        ProgramError.check(Address.fromInt(refMap.length).isWordAligned(), "Boot heap reference map must have word-aligned size");
+        this.referenceMapBytes = refMap;
+        this.specialReferences = specialRefs;
+    }
+
+    public void visitReferences(PointerIndexVisitor pointerIndexVisitor) {
+        if (referenceMap.isZero()) {
+            referenceMap = ArrayAccess.elementPointer(referenceMapBytes, 0);
+        }
+
+        final Pointer refMap = referenceMap;
+        final int referenceMapWords = Unsigned.idiv(referenceMapBytes.length, Word.size());
+        if (Heap.traceRootScanning()) {
+            Log.print("Scanning boot heap: start=");
+            Log.print(start());
+            Log.print(", end=");
+            Log.print(end());
+            Log.print(", mutable references end=");
+            Log.println(start().plus(referenceMapWords * Word.size()));
+            scanReferenceMap(pointerIndexVisitor, refMap, referenceMapWords, true);
+        } else {
+            scanReferenceMap(pointerIndexVisitor, refMap, referenceMapWords, false);
+        }
+
+        for (java.lang.ref.Reference specialReference : specialReferences) {
+            SpecialReferenceManager.discoverSpecialReference(Reference.fromJava(specialReference).toOrigin());
+        }
+    }
+}
+
---
+++ com.oracle.max.vm/test/jtt/except/Catch_NPE_07.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package jtt.except;
+
+/*
+ * @Harness: java
+ * @Runs: 0 = 0; 1 = 1
+ */
+
+public class Catch_NPE_07 {
+    public static class MyThrowable extends Throwable {
+    }
+
+    public static int foo(Throwable t) {
+        try {
+            throw t;
+        } catch (Throwable t1) {
+            if (t1 == null) {
+                return -1;
+            }
+            if (t1 instanceof NullPointerException) {
+                return 0;
+            }
+            if (t1 instanceof MyThrowable) {
+                return 1;
+            }
+            return -2;
+        }
+    }
+
+    public static int test(int i) {
+        Throwable t = (i == 0) ? null : new MyThrowable();
+        try {
+            return foo(t);
+        } catch (Throwable t1) {
+            return -3;
+        }
+    }
+}
