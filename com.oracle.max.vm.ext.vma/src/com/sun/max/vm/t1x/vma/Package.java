edfae993eecbd9d2ec5c9a1cc1b33851ff43f5d0
Doug Simon <doug.simon@oracle.com>
2011-09-08 22:35:29 +0200

    Updated MaxineProcess-8000 launch configuration to include com.oracle.max.vm.ext.maxri source path.

---
+++ com.oracle.max.vm.ext.vma/src/com/sun/max/vm/t1x/vma/Package.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2011, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.vm.t1x.vma;
+
+import com.sun.max.config.BootImagePackage;
+import com.sun.max.vm.VMConfiguration;
+import com.sun.max.vm.compiler.adaptive.AdaptiveCompilationScheme;
+
+public class Package extends BootImagePackage {
+    public Package() {
+        super();
+    }
+
+    private boolean isOAT1X(String className) {
+        return className != null && className.startsWith(this.name());
+    }
+
+    @Override
+    public boolean isPartOfMaxineVM(VMConfiguration vmConfiguration) {
+        if (vmConfiguration.compilationPackage.getClass().equals(com.sun.max.vm.compiler.adaptive.Package.class)) {
+            return isOAT1X(AdaptiveCompilationScheme.optName()) || isOAT1X(AdaptiveCompilationScheme.baselineName());
+        }
+        return false;
+    }
+
+}
