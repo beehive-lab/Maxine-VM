6fae138662b5c6bd15bbd000c1d7e9bf32f9ee02
ldayne <Laurent.Daynes@oracle.com>
2011-09-12 18:35:46 +0200

    Cannot use ClassActor in static initializer of AtomicPinnedCounter as ClassActor isn't defined yet. Instead, must explicitly define and call hosted-only method toavoid  bootstrap issue when prototyping in debug mode.

---
+++ com.oracle.max.jdwp/src/com/sun/max/jdwp/data/ReplyPacket.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.jdwp.data;
+
+/**
+ * This class represents a JDWP reply packet. It is always associated with the incoming packet for which this packet is a reply.
+ *
+ * @author Thomas Wuerthinger
+ */
+public final class ReplyPacket<IncomingData_Type extends IncomingData, OutgoingData_Type extends OutgoingData> {
+
+    private IncomingPacket<IncomingData_Type, OutgoingData_Type> incomingPacket;
+    private short errorCode;
+    private OutgoingData_Type data;
+
+    public ReplyPacket(IncomingPacket<IncomingData_Type, OutgoingData_Type> incomingPacket, short errorCode) {
+        this.incomingPacket = incomingPacket;
+        this.errorCode = errorCode;
+    }
+
+    public ReplyPacket(IncomingPacket<IncomingData_Type, OutgoingData_Type> incomingPacket, OutgoingData_Type data) {
+        this.incomingPacket = incomingPacket;
+        this.data = data;
+    }
+
+    /**
+     * Flags of the packet, a reply packet always has the same constant flags.
+     * @return 0x80 according to the JDWP definition of the flags for a reply packet
+     */
+    public byte getFlags() {
+        return (byte) 0x80;
+    }
+
+    /**
+     * The outgoing data to be sent in reply or null, if it is an error packet.
+     * @return the outgoing data
+     */
+    public OutgoingData_Type getData() {
+        return data;
+    }
+
+    public int getId() {
+        return incomingPacket.getId();
+    }
+
+    /**
+     * The error code of the packet, if this is an error reply packet, or zero otherwise.
+     * @return the error code
+     */
+    public short getErrorCode() {
+        return errorCode;
+    }
+
+    public IncomingPacket<IncomingData_Type, OutgoingData_Type> getOriginalPacket() {
+        return incomingPacket;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("ReplyPacket(" + getId() + ")[flags=" + getFlags() + ", error=" + getErrorCode() + ", data=" + getData() + "]");
+        return sb.toString();
+    }
+}
---
+++ com.oracle.max.vm/test/jtt/max/Invoke_except01.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package jtt.max;
+
+import com.sun.max.vm.actor.member.ClassMethodActor;
+import com.sun.max.vm.value.Value;
+import com.sun.max.vm.value.ReferenceValue;
+
+import java.lang.reflect.*;
+
+/*
+* @Harness: java
+* @Runs: 0=0; 1=3; 2=!java.lang.reflect.InvocationTargetException; 3=!java.lang.IllegalArgumentException; 4=!java.lang.IllegalArgumentException
+*/
+public class Invoke_except01 {
+
+    public static int test(int arg) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Value[] args;
+        if (arg == 0) {
+            args = new Value[] {ReferenceValue.from(new int[0])};
+        } else if (arg == 1) {
+            args = new Value[] {ReferenceValue.from(new int[3])};
+        } else if (arg == 2) {
+            args = new Value[] {ReferenceValue.NULL};
+        } else if (arg == 3) {
+            args = new Value[] {ReferenceValue.from(new char[3])};
+        } else {
+            args = null;
+        }
+        for (Method m : Invoke_except01.class.getDeclaredMethods()) {
+            if ("method".equals(m.getName())) {
+                ClassMethodActor cma = ClassMethodActor.fromJava(m);
+                return cma.invoke(args).toInt();
+            }
+        }
+        return 42;
+    }
+
+    public static int method(int[] arg) {
+        return arg.length;
+    }
+}
