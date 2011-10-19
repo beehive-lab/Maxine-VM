acaf795e143122dec206424cc357cbef39d58293
Laurent Daynes <laurent.daynes@oracle.com>
2011-10-19 23:01:17 +0200

    Temporary traces to chase sweeping bug.

--- com.oracle.max.vm/src/com/sun/max/vm/heap/gcx/Sweeper.java
+++ com.oracle.max.vm/src/com/sun/max/vm/heap/gcx/Sweeper.java
@@ -40,6 +40,7 @@ public abstract class Sweeper {
 
     public static boolean DoImpreciseSweep = true;
     static boolean TraceSweep;
+    static boolean TraceGap = false;
 
     static {
         VMOptions.addFieldOption("-XX:", "DoImpreciseSweep", Sweeper.class, "Use an imprecise sweeping phase", Phase.PRISTINE);
