b81c5823c61b6604792453f3c09209a1dac495b1
Christian Wimmer <Christian.Wimmer@Oracle.com>
2011-10-19 09:47:20 -0700

    Fix LIR refactoring commit: Handling of temporary operands was missing.

---
+++ com.oracle.max.vm/test/test/bench/package-info.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2010, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+/**
+ * Sub packages of test.bench should contain micro-benchmarks that measure aspects of the VM and JDK platform.
+ * Each micro-benchmark should be written to use the framework in {@link test.bench.util.RunBench}, following the
+ * style of one of the existing micro-benchmarks. The intent is that the micro-benchmarks can be run stand-alone or
+ * under the standard {@link test.com.sun.max.vm.jtrun.JTAbstractRunScheme test framework}.
+ */
+package test.bench;
