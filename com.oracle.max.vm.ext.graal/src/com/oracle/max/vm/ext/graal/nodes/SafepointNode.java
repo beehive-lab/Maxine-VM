3e335bfa5b3ce527ec0d08380d5232fa21cd2071
Lukas Stadler <lukas.stadler@jku.at>
2011-10-21 16:05:35 +0200

    first part of work on Maxine intrinsic support

---
+++ com.oracle.max.vm.ext.graal/src/com/oracle/max/vm/ext/graal/nodes/SafepointNode.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.max.vm.ext.graal.nodes;
+
+import com.oracle.max.graal.nodes.*;
+import com.oracle.max.graal.nodes.spi.*;
+import com.sun.cri.ci.*;
+
+/**
+ * Adds a Safepoint to the generated code and possibly create a safepoint.
+ */
+public final class SafepointNode extends StateSplit implements LIRLowerable {
+
+    public static enum Op {
+        SAFEPOINT_POLL, HERE, INFO, UNCOMMON_TRAP
+    }
+
+    @Data private final Op op;
+
+    public SafepointNode(Op op) {
+        super(CiKind.Illegal);
+        this.op = op;
+    }
+
+    @Override
+    public void accept(ValueVisitor v) {
+        // nothing to do
+    }
+
+    @Override
+    public void generate(LIRGeneratorTool generator) {
+        throw new RuntimeException();
+    }
+}
