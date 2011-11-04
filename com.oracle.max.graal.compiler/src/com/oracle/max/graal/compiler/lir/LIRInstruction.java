f86b4e7ecf3bb538b9816f7bfdf6f9fa6564d52c
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2011-11-04 12:49:40 +0100

    Modified state-before calculation for InvokeNode to use the method's signature instead of the argument's kinds.

---
+++ com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/lir/LIRInstruction.java
@@ -0,0 +1,314 @@
+/*
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.max.graal.compiler.lir;
+
+import com.oracle.max.graal.compiler.asm.*;
+import com.oracle.max.graal.compiler.util.*;
+import com.sun.cri.ci.*;
+import com.sun.cri.ci.CiValue.Formatter;
+
+/**
+ * The {@code LIRInstruction} class definition.
+ */
+public abstract class LIRInstruction {
+
+    public static final CiValue[] NO_OPERANDS = {};
+
+    public static final OperandMode[] OPERAND_MODES = OperandMode.values();
+
+    /**
+     * Constants denoting how a LIR instruction uses an operand. Any combination of these modes
+     * can be applied to an operand as long as every operand has at least one mode applied to it.
+     */
+    public enum OperandMode {
+        /**
+         * An operand that is defined by this LIR instruction and is live after the code emitted for a LIR instruction.
+         */
+        Output,
+
+        /**
+         * An operand that is used by this LIR instruction and is live before the code emitted for this LIR instruction.
+         */
+        Input,
+
+        /**
+         * An operand that is used internally by this LIR instruction, i.e., it must get different register than any output operand.
+         */
+        Temp
+    }
+
+    /**
+     * The opcode of this instruction.
+     */
+    public final LIROpcode code;
+
+    /**
+     * The result operand for this instruction (modified by the register allocator).
+     */
+    protected CiValue result;
+
+    /**
+     * The input operands for this instruction (modified by the register allocator).
+     */
+    protected final CiValue[] inputs;
+
+    /**
+     * The temp operands for this instruction (modified by the register allocator).
+     */
+    protected final CiValue[] temps;
+
+    /**
+     * Used to emit debug information.
+     */
+    public final LIRDebugInfo info;
+
+    /**
+     * Instruction id for register allocation.
+     */
+    private int id;
+
+    /**
+     * Constructs a new LIR instruction that has input operands, but no temp operands.
+     */
+    public LIRInstruction(LIROpcode opcode, CiValue result, LIRDebugInfo info, CiValue[] inputs) {
+        this (opcode, result, info, inputs, NO_OPERANDS);
+    }
+
+    /**
+     * Constructs a new LIR instruction that has input and temp operands.
+     *
+     * @param opcode the opcode of the new instruction
+     * @param result the operand that holds the operation result of this instruction. This will be
+     *            {@link CiValue#IllegalValue} for instructions that do not produce a result.
+     * @param info the {@link LIRDebugInfo} info that is to be preserved for the instruction. This will be {@code null} when no debug info is required for the instruction.
+     * @param inputs the input operands for the instruction.
+     * @param temps the temp operands for the instruction.
+     */
+    public LIRInstruction(LIROpcode opcode, CiValue result, LIRDebugInfo info, CiValue[] inputs, CiValue[] temps) {
+        this.code = opcode;
+        this.result = result;
+        this.inputs = inputs;
+        this.temps = temps;
+        this.info = info;
+        this.id = -1;
+    }
+
+    public abstract void emitCode(TargetMethodAssembler tasm);
+
+
+    public final int id() {
+        return id;
+    }
+
+    public final void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets an input operand of this instruction.
+     *
+     * @param index the index of the operand requested.
+     * @return the {@code index}'th input operand.
+     */
+    public final CiValue input(int index) {
+        return inputs[index];
+    }
+
+    /**
+     * Gets a temp operand of this instruction.
+     *
+     * @param index the index of the operand requested.
+     * @return the {@code index}'th temp operand.
+     */
+    public final CiValue temp(int index) {
+        return temps[index];
+    }
+
+    /**
+     * Gets the result operand for this instruction.
+     *
+     * @return return the result operand
+     */
+    public final CiValue result() {
+        return result;
+    }
+
+    /**
+     * Gets the instruction name.
+     */
+    public String name() {
+        return code.toString();
+    }
+
+    public boolean hasOperands() {
+        return inputs.length > 0 || temps.length > 0 || info != null || hasCall();
+    }
+
+    public final int operandCount(OperandMode mode) {
+        switch (mode) {
+            case Output: return result.isLegal() ? 1 : 0;
+            case Input:  return inputs.length;
+            case Temp:   return temps.length;
+            default:     throw Util.shouldNotReachHere();
+        }
+    }
+
+    public final CiValue operandAt(OperandMode mode, int index) {
+        assert index < operandCount(mode);
+        switch (mode) {
+            case Output: return result;
+            case Input:  return inputs[index];
+            case Temp:   return temps[index];
+            default:     throw Util.shouldNotReachHere();
+        }
+    }
+
+    public final void setOperandAt(OperandMode mode, int index, CiValue location) {
+        assert index < operandCount(mode);
+        assert location.kind != CiKind.Illegal;
+        assert operandAt(mode, index).isVariable();
+        switch (mode) {
+            case Output: result = location; break;
+            case Input:  inputs[index] = location; break;
+            case Temp:   temps[index] = location; break;
+            default:     throw Util.shouldNotReachHere();
+        }
+    }
+
+    /**
+     * Returns true when this instruction is a call instruction that destroys all caller-saved registers.
+     */
+    public boolean hasCall() {
+        return false;
+    }
+
+    /**
+     * Used by the register allocator.  The result operand of this instruction should get
+     * the same register assigned as the input operand with returned number.
+     * @return The number of the input operand that is used as a register hint for the output operand,
+     *         or -1 if no register hint should be defined.
+     */
+    public int registerHint() {
+        return -1;
+    }
+
+    /**
+     * Used by the register allocator to decide whether an input operand can be assigned a stack slot.
+     * Subclasses should override this method when an input can be memory.
+     * @param index The index of the operand in {@link #inputs}.
+     * @return true if the input operand with the given index can be assigned a stack slot.
+     */
+    public boolean inputCanBeMemory(int index) {
+        return false;
+    }
+
+
+    @Override
+    public String toString() {
+        return toString(Formatter.DEFAULT);
+    }
+
+    public final String toStringWithIdPrefix() {
+        if (id != -1) {
+            return String.format("%4d %s", id, toString());
+        }
+        return "     " + toString();
+    }
+
+    /**
+     * Gets the operation performed by this instruction in terms of its operands as a string.
+     */
+    public String operationString(Formatter operandFmt) {
+        StringBuilder buf = new StringBuilder();
+        if (result.isLegal()) {
+            buf.append(operandFmt.format(result)).append(" = ");
+        }
+        if (inputs.length > 1) {
+            buf.append("(");
+        }
+        String sep = "";
+        for (CiValue input : inputs) {
+            buf.append(sep).append(operandFmt.format(input));
+            sep = ", ";
+        }
+        if (inputs.length > 1) {
+            buf.append(")");
+        }
+
+        if (temps.length > 1) {
+            buf.append("{");
+        }
+        sep = "";
+        for (CiValue temp : temps) {
+            buf.append(sep).append(operandFmt.format(temp));
+            sep = ", ";
+        }
+        if (temps.length > 1) {
+            buf.append("}");
+        }
+        return buf.toString();
+    }
+
+    protected static String refMapToString(CiDebugInfo debugInfo, Formatter operandFmt) {
+        StringBuilder buf = new StringBuilder();
+        if (debugInfo.hasStackRefMap()) {
+            CiBitMap bm = debugInfo.frameRefMap;
+            for (int slot = bm.nextSetBit(0); slot >= 0; slot = bm.nextSetBit(slot + 1)) {
+                if (buf.length() != 0) {
+                    buf.append(", ");
+                }
+                buf.append(operandFmt.format(CiStackSlot.get(CiKind.Object, slot)));
+            }
+        }
+        if (debugInfo.hasRegisterRefMap()) {
+            CiBitMap bm = debugInfo.registerRefMap;
+            for (int reg = bm.nextSetBit(0); reg >= 0; reg = bm.nextSetBit(reg + 1)) {
+                if (buf.length() != 0) {
+                    buf.append(", ");
+                }
+                buf.append("r").append(reg);
+            }
+        }
+        return buf.toString();
+    }
+
+    protected void appendDebugInfo(StringBuilder buf, Formatter operandFmt, LIRDebugInfo info) {
+        if (info != null) {
+            buf.append(" [bci:").append(info.state.bci);
+            if (info.hasDebugInfo()) {
+                CiDebugInfo debugInfo = info.debugInfo();
+                String refmap = refMapToString(debugInfo, operandFmt);
+                if (refmap.length() != 0) {
+                    buf.append(", refmap(").append(refmap.trim()).append(')');
+                }
+            }
+            buf.append(']');
+        }
+    }
+
+    public String toString(Formatter operandFmt) {
+        StringBuilder buf = new StringBuilder(name()).append(' ').append(operationString(operandFmt));
+        appendDebugInfo(buf, operandFmt, info);
+        return buf.toString();
+    }
+}
---
+++ com.oracle.max.vm/src/com/sun/max/vm/layout/ohm/OhmArrayLayout.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.vm.layout.ohm;
+
+import java.lang.reflect.*;
+
+import com.sun.max.annotate.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.actor.holder.*;
+import com.sun.max.vm.layout.*;
+import com.sun.max.vm.layout.Layout.HeaderField;
+import com.sun.max.vm.object.*;
+import com.sun.max.vm.reference.*;
+import com.sun.max.vm.type.*;
+import com.sun.max.vm.value.*;
+
+/**
+ */
+public class OhmArrayLayout extends OhmGeneralLayout implements ArrayLayout {
+
+    /**
+     * The cell offset of the word in the header containing the array length.
+     */
+    public final int lengthOffset;
+
+    public final int headerSize;
+
+    public final Kind elementKind;
+
+    @INLINE
+    public final int headerSize() {
+        return headerSize;
+    }
+
+    public HeaderField[] headerFields() {
+        return new HeaderField[] {HeaderField.HUB, HeaderField.MISC, HeaderField.LENGTH};
+    }
+
+    OhmArrayLayout(Kind elementKind) {
+        lengthOffset = miscOffset + Word.size();
+        headerSize = lengthOffset + Word.size();
+        this.elementKind = elementKind;
+    }
+
+    public boolean isArrayLayout() {
+        return true;
+    }
+
+    @INLINE
+    public final Size getArraySize(Kind kind, int length) {
+        return Size.fromInt(kind.width.numberOfBytes).times(length).plus(headerSize).wordAligned();
+    }
+
+    @Override
+    public Offset getOffsetFromOrigin(HeaderField headerField) {
+        if (headerField == HeaderField.LENGTH) {
+            return Offset.fromInt(lengthOffset);
+        }
+        return super.getOffsetFromOrigin(headerField);
+    }
+
+    public int arrayLengthOffset() {
+        return lengthOffset;
+    }
+
+    @INLINE
+    public final int readLength(Accessor accessor) {
+        return accessor.readInt(lengthOffset);
+    }
+
+    @INLINE
+    public final void writeLength(Accessor accessor, int length) {
+        accessor.writeInt(lengthOffset, length);
+    }
+
+    @INLINE
+    public final Kind elementKind() {
+        return elementKind;
+    }
+
+    public Layout.Category category() {
+        return Layout.Category.ARRAY;
+    }
+
+    @Override
+    public final boolean isReferenceArrayLayout() {
+        final Kind rawKind = elementKind;
+        return rawKind.isReference;
+    }
+
+    @INLINE
+    public final int elementSize() {
+        return elementKind().width.numberOfBytes;
+    }
+
+    @INLINE
+    protected final int originDisplacement() {
+        return headerSize();
+    }
+
+    @INLINE
+    public final Offset getElementOffsetFromOrigin(int index) {
+        return getElementOffsetInCell(index);
+    }
+
+    @INLINE
+    public final Offset getElementOffsetInCell(int index) {
+        // Converting to 'Offset' before multiplication to avoid overflow:
+        return Offset.fromInt(index).times(elementSize()).plus(headerSize());
+    }
+
+    @INLINE
+    public final Size getArraySize(int length) {
+        return getElementOffsetInCell(length).aligned().asSize();
+    }
+
+    @INLINE
+    public final Size specificSize(Accessor accessor) {
+        return getArraySize(readLength(accessor));
+    }
+
+    @HOSTED_ONLY
+    @Override
+    public void visitHeader(ObjectCellVisitor visitor, Object array) {
+        super.visitHeader(visitor, array);
+        visitor.visitHeaderField(lengthOffset, "length", JavaTypeDescriptor.INT, IntValue.from(ArrayAccess.readArrayLength(array)));
+    }
+
+    @HOSTED_ONLY
+    private void visitElements(ObjectCellVisitor visitor, Object array) {
+        final int length = Array.getLength(array);
+        final Hub hub = ObjectAccess.readHub(array);
+        final Kind elementKind = hub.classActor.componentClassActor().kind;
+        if (elementKind.isReference) {
+            for (int i = 0; i < length; i++) {
+                final Object object = Array.get(array, i);
+                visitor.visitElement(getElementOffsetInCell(i).toInt(), i, ReferenceValue.from(object));
+            }
+        } else {
+            for (int i = 0; i < length; i++) {
+                final Object boxedJavaValue = Array.get(array, i);
+                final Value value = elementKind.asValue(boxedJavaValue);
+                visitor.visitElement(getElementOffsetInCell(i).toInt(), i, value);
+            }
+        }
+    }
+
+    @HOSTED_ONLY
+    public void visitObjectCell(Object array, ObjectCellVisitor visitor) {
+        visitHeader(visitor, array);
+        visitElements(visitor, array);
+    }
+
+    @HOSTED_ONLY
+    public Value readValue(Kind kind, ObjectMirror mirror, int offset) {
+        if (offset == lengthOffset) {
+            return IntValue.from(mirror.readArrayLength());
+        }
+        final Value value = readHeaderValue(mirror, offset);
+        if (value != null) {
+            return value;
+        }
+        assert kind.isPrimitiveOfSameSizeAs(elementKind);
+        final int index = (offset - headerSize()) / kind.width.numberOfBytes;
+        return mirror.readElement(kind, index);
+    }
+
+    @HOSTED_ONLY
+    public void writeValue(Kind kind, ObjectMirror mirror, int offset, Value value) {
+        assert kind.isPrimitiveOfSameSizeAs(value.kind());
+        if (offset == lengthOffset) {
+            mirror.writeArrayLength(value);
+            return;
+        }
+        if (writeHeaderValue(mirror, offset, value)) {
+            return;
+        }
+        assert kind.isPrimitiveOfSameSizeAs(elementKind);
+        final int index = (offset - headerSize()) / elementSize();
+        mirror.writeElement(kind, index, value);
+    }
+
+    public void copyElements(Accessor src, int srcIndex, Object dst, int dstIndex, int length) {
+        src.copyElements(originDisplacement(), srcIndex, dst, dstIndex, length);
+    }
+
+    @INLINE public final boolean   getBoolean(Accessor accessor, int index) { return accessor.getBoolean(originDisplacement(), index); }
+    @INLINE public final byte      getByte(Accessor accessor, int index) { return accessor.getByte(originDisplacement(), index); }
+    @INLINE public final char      getChar(Accessor accessor, int index) { return accessor.getChar(originDisplacement(), index); }
+    @INLINE public final short     getShort(Accessor accessor, int index) { return accessor.getShort(originDisplacement(), index);  }
+    @INLINE public final int       getInt(Accessor accessor, int index) { return accessor.getInt(originDisplacement(), index); }
+    @INLINE public final float     getFloat(Accessor accessor, int index) { return accessor.getFloat(originDisplacement(), index); }
+    @INLINE public final long      getLong(Accessor accessor, int index) { return accessor.getLong(originDisplacement(), index); }
+    @INLINE public final double    getDouble(Accessor accessor, int index) { return accessor.getDouble(originDisplacement(), index); }
+    @INLINE public final Word      getWord(Accessor accessor, int index) { return accessor.getWord(originDisplacement(), index); }
+    @INLINE public final Reference getReference(Accessor accessor, int index) { return accessor.getReference(originDisplacement(), index); }
+
+    @INLINE public final void setBoolean(Accessor accessor, int index, boolean value) { accessor.setBoolean(originDisplacement(), index, value); }
+    @INLINE public final void setByte(Accessor accessor, int index, byte value) {  accessor.setByte(originDisplacement(), index, value); }
+    @INLINE public final void setChar(Accessor accessor, int index, char value) { accessor.setChar(originDisplacement(), index, value); }
+    @INLINE public final void setShort(Accessor accessor, int index, short value) { accessor.setShort(originDisplacement(), index, value); }
+    @INLINE public final void setInt(Accessor accessor, int index, int value) { accessor.setInt(originDisplacement(), index, value); }
+    @INLINE public final void setFloat(Accessor accessor, int index, float value) { accessor.setFloat(originDisplacement(), index, value); }
+    @INLINE public final void setLong(Accessor accessor, int index, long value) { accessor.setLong(originDisplacement(), index, value); }
+    @INLINE public final void setDouble(Accessor accessor, int index, double value) { accessor.setDouble(originDisplacement(), index, value); }
+    @INLINE public final void setWord(Accessor accessor, int index, Word value) { accessor.setWord(originDisplacement(), index, value); }
+    @INLINE public final void setReference(Accessor accessor, int index, Reference element) { accessor.setReference(originDisplacement(), index, element); }
+}
