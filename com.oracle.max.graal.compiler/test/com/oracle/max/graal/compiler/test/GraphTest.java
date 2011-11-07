b0bdab2bcc8212a3fb791e93afe0db5365ac67d5
Doug Simon <doug.simon@oracle.com>
2011-11-07 22:24:41 +0100

    Fixed regression in C1X and Graal runtime initialization.

---
+++ com.oracle.max.graal.compiler/test/com/oracle/max/graal/compiler/test/GraphTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.max.graal.compiler.test;
+
+import java.lang.reflect.*;
+
+import junit.framework.Assert;
+
+import com.oracle.max.graal.compiler.*;
+import com.oracle.max.graal.compiler.debug.*;
+import com.oracle.max.graal.compiler.graphbuilder.*;
+import com.oracle.max.graal.nodes.*;
+import com.sun.cri.ri.*;
+
+public abstract class GraphTest {
+
+    private static String[] graalCompilerFactoryClasses = {
+        "com.oracle.max.graal.hotspot.CompilerImpl",
+        "com.oracle.max.vm.ext.c1xgraal.C1XGraal"
+    };
+
+    private static GraalCompiler getGraalCompiler() {
+        for (String className : graalCompilerFactoryClasses) {
+            Class<?> c = null;
+            try {
+                c = Class.forName(className);
+                Method m = c.getDeclaredMethod("getGraalCompiler");
+                return (GraalCompiler) m.invoke(null);
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        }
+        throw new InternalError("Could not create a GraalCompiler instance");
+    }
+
+    private GraalCompiler graalCompiler;
+
+    public GraphTest() {
+        this.graalCompiler = getGraalCompiler();
+    }
+
+    protected void assertEquals(StructuredGraph expected, StructuredGraph graph) {
+        if (expected.getNodeCount() != graph.getNodeCount()) {
+            print("Node count not matching", expected, graph);
+            Assert.fail("Graphs do not have the same number of nodes");
+        }
+    }
+
+    protected RiRuntime runtime() {
+        return graalCompiler.runtime;
+    }
+
+    protected StructuredGraph parse(String methodName) {
+        Method found = null;
+        for (Method m : this.getClass().getMethods()) {
+            if (m.getName().equals(methodName)) {
+                Assert.assertNull(found);
+                found = m;
+            }
+        }
+        return parse(found);
+    }
+
+    protected StructuredGraph parse(Method m) {
+        RiRuntime runtime = graalCompiler.runtime;
+        RiResolvedMethod riMethod = runtime.getRiMethod(m);
+        StructuredGraph graph = new StructuredGraph();
+        new GraphBuilderPhase(runtime, riMethod).apply(graph);
+        return graph;
+    }
+
+    protected void print(String title, StructuredGraph... graphs) {
+        IdealGraphPrinterObserver observer = new IdealGraphPrinterObserver(GraalOptions.PrintIdealGraphAddress, GraalOptions.PrintIdealGraphPort);
+        observer.printGraphs(getClass().getSimpleName() + ": " + title, graphs);
+    }
+
+    protected void print(StructuredGraph graph) {
+        IdealGraphPrinterObserver observer = new IdealGraphPrinterObserver(GraalOptions.PrintIdealGraphAddress, GraalOptions.PrintIdealGraphPort);
+        observer.printSingleGraph(getClass().getSimpleName(), graph);
+    }
+}
---
+++ com.oracle.max.vm/src/com/sun/max/vm/heap/CallbackCellVisitor.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.vm.heap;
+
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.actor.holder.*;
+import com.sun.max.vm.layout.*;
+import com.sun.max.vm.reference.*;
+
+/**
+ * Encapsulates the basic logic for walking over cells in a heap region, and
+ * invokes {@link CallbackCellVisitor#callback} for each object found.
+ */
+public abstract class CallbackCellVisitor implements CellVisitor {
+
+    @Override
+    public Pointer visitCell(Pointer cell) {
+        final Pointer origin = Layout.cellToOrigin(cell);
+        final Hub hub = UnsafeCast.asHub(Layout.readHubReference(origin).toJava());
+        final SpecificLayout specificLayout = hub.specificLayout;
+
+        Object object = Reference.fromOrigin(origin).toJava();
+        if (!callback(object)) {
+            return Pointer.zero();
+        }
+
+        if (specificLayout.isTupleLayout()) {
+            return cell.plus(hub.tupleSize);
+        } else {
+            return cell.plus(Layout.size(origin));
+        }
+    }
+
+    /**
+     * Callback on a visited object.
+     * @param object
+     * @return {@code true} to continue then walk, {@code false} to abort.
+     */
+    protected abstract boolean callback(Object object);
+
+}
