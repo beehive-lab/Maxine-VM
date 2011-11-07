37cda345149cba84540f85aa41ec33ac03fd6a55
Lukas Stadler <lukas.stadler@jku.at>
2011-11-07 16:55:51 +0100

    fix EscapeAnalysisPhase for new InvokeNode layout

---
+++ com.oracle.max.vm/test/jtt/bytecode/BC_ladd.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package jtt.bytecode;
+
+/*
+ * @Harness: java
+ * @Runs: (1L, 2L) = 3L; (0L, -1L) = -1L; (33L, 67L) = 100L; (1L, -1L) = 0L;
+ * @Runs: (-2147483648L, 1L) = -2147483647L; (2147483647L, 1L) = 2147483648L
+ * @Runs: (-2147483647L,-2L) = -2147483649L
+ */
+public class BC_ladd {
+    public static long test(long a, long b) {
+        return a + b;
+    }
+}
