6d041ff59caca0c89627b7929cbf38d9250b1b63
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2011-11-10 11:57:05 +0100

    Cache MUST_INLINE methods in compiler storage.

---
+++ com.oracle.max.tele.vm/src/com/sun/max/tele/debug/TeleStackFrameWalker.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.tele.debug;
+
+import java.util.*;
+
+import com.sun.max.tele.*;
+import com.sun.max.tele.method.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.compiler.target.*;
+import com.sun.max.vm.runtime.*;
+import com.sun.max.vm.stack.*;
+import com.sun.max.vm.thread.*;
+
+/**
+ * Specialization of a StackFrameWalker for use with a {@link TeleVM}.
+ */
+public final class TeleStackFrameWalker extends StackFrameWalker {
+
+    private final TeleVM teleVM;
+    private final TeleThreadLocalsArea teleEnabledVmThreadLocalValues;
+    private final TeleNativeThread teleNativeThread;
+
+    private final Pointer cpuInstructionPointer;
+    private final Pointer cpuStackPointer;
+    private final Pointer cpuFramePointer;
+
+    public TeleStackFrameWalker(TeleVM teleVM, TeleNativeThread teleNativeThread) {
+        super();
+        this.teleVM = teleVM;
+        this.teleNativeThread = teleNativeThread;
+        this.teleEnabledVmThreadLocalValues = teleNativeThread.localsBlock().tlaFor(SafepointPoll.State.ENABLED);
+        final TeleRegisterSet registers = teleNativeThread.registers();
+        this.cpuInstructionPointer = registers.instructionPointer();
+        this.cpuStackPointer = registers.stackPointer();
+        this.cpuFramePointer = registers.framePointer();
+    }
+
+    /**
+     * A pseudo frame informing the stack walker client of a condition
+     * that prevented a complete stack walk.
+     */
+    public static class TruncatedStackFrame extends StackFrame {
+
+        /**
+         * Denotes the error causing the stack walk to stop. If {@code null}, the stack walk
+         * was stopped due to the frame limit specified to {@link TeleStackFrameWalker#frames(int)}
+         * being hit.
+         */
+        public final Throwable error;
+
+        /**
+         * The number of frames above the frame limit. This will be -1 if an error stopped the stack walk.
+         */
+        public final int omitted;
+
+        TruncatedStackFrame(StackFrame callee, Throwable error) {
+            super(callee, Pointer.zero(), Pointer.zero(), Pointer.zero());
+            this.error = error;
+            this.omitted = -1;
+        }
+
+        TruncatedStackFrame(StackFrame callee, int omitted) {
+            super(callee, Pointer.zero(), Pointer.zero(), Pointer.zero());
+            this.error = null;
+            this.omitted = omitted;
+        }
+
+        @Override
+        public boolean isSameFrame(StackFrame stackFrame) {
+            if (stackFrame instanceof TruncatedStackFrame) {
+                final TruncatedStackFrame other = (TruncatedStackFrame) stackFrame;
+                final StackFrame calleeFrame = calleeFrame();
+                if (calleeFrame == null) {
+                    return other.calleeFrame() == null;
+                }
+                return calleeFrame.isSameFrame(other.calleeFrame());
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            if (error == null) {
+                return "<truncated: omitted " + omitted + " frames>";
+            }
+            return "<error: " + error + ">";
+        }
+    }
+
+    /**
+     * If the stack walk is stopped due to an exception or {@code maxDepth} being reached,
+     * a {@link TruncatedStackFrame} is appended the end of the returned list.
+     *
+     * @param maxDepth
+     * @return
+     */
+    public List<StackFrame> frames(final int maxDepth) {
+        if (maxDepth <= 0) {
+            return Collections.emptyList();
+        }
+        final ArrayList<StackFrame> frames = new ArrayList<StackFrame>();
+        try {
+            final boolean[] hitMaxDepth = {false};
+            final StackFrameVisitor visitor = new StackFrameVisitor() {
+                @Override
+                public boolean visitFrame(StackFrame stackFrame) {
+                    frames.add(stackFrame);
+                    if (frames.size() == maxDepth) {
+                        hitMaxDepth[0] = true;
+                        return false;
+                    }
+                    return true;
+                }
+            };
+            inspect(cpuInstructionPointer, cpuStackPointer, cpuFramePointer, visitor);
+            if (hitMaxDepth[0]) {
+                StackFrame last = frames.get(frames.size() - 1);
+                class Counter extends RawStackFrameVisitor {
+                    int count;
+                    @Override
+                    public boolean visitFrame(Cursor current, Cursor callee) {
+                        count++;
+                        return true;
+                    }
+                }
+                Counter c = new Counter();
+                if (maxDepth != 1) {
+                    inspect(last.ip, last.sp, last.fp, c);
+                }
+                frames.add(new TruncatedStackFrame(last, c.count));
+            }
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+            final StackFrame parentFrame = frames.isEmpty() ? null : frames.get(frames.size() - 1);
+            frames.add(new TruncatedStackFrame(parentFrame, e));
+        }
+        return frames;
+    }
+
+    @Override
+    public TargetMethod targetMethodFor(Pointer instructionPointer) {
+        final TeleCompilation compiledCode = teleVM.codeCache().findCompiledCode(instructionPointer);
+        if (compiledCode != null) {
+            return compiledCode.teleTargetMethod().targetMethod();
+        }
+        return null;
+    }
+
+    @Override
+    public byte readByte(Address address, int offset) {
+        return teleVM.dataAccess().readByte(address, offset);
+    }
+
+    @Override
+    public Word readWord(Address address, int offset) {
+        return teleVM.dataAccess().readWord(address, offset);
+    }
+
+    @Override
+    public int readInt(Address address, int offset) {
+        return teleVM.dataAccess().readInt(address, offset);
+    }
+
+    @Override
+    public Pointer readPointer(VmThreadLocal local) {
+        return teleEnabledVmThreadLocalValues == null ? Pointer.zero() : teleEnabledVmThreadLocalValues.getWord(local).asPointer();
+    }
+}
---
+++ com.oracle.max.tele.vm/src/com/sun/max/tele/jdwputil/JavaReferenceTypeProvider.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.tele.jdwputil;
+
+import java.lang.reflect.*;
+
+import com.sun.max.jdwp.vm.proxy.*;
+import com.sun.max.vm.type.*;
+
+abstract class JavaReferenceTypeProvider implements ReferenceTypeProvider {
+
+    private Class clazz;
+    private ClassProvider superClass;
+    private ClassLoaderProvider classLoader;
+    private InterfaceProvider[] implementedInterfaces;
+    private MethodProvider[] methodProviders;
+    private VMAccess vm;
+
+    protected JavaReferenceTypeProvider(Class c, VMAccess vm, ClassLoaderProvider classLoader) {
+        this.clazz = c;
+        this.vm = vm;
+        initReferencedClasses();
+        initMethods();
+    }
+
+    public ClassProvider getSuperClass() {
+        return superClass;
+    }
+
+    private void initReferencedClasses() {
+
+        if (clazz.getSuperclass() != null) {
+            final ReferenceTypeProvider referenceTypeProvider = vm.getReferenceType(clazz.getSuperclass());
+            assert referenceTypeProvider instanceof ClassProvider;
+            superClass = (ClassProvider) referenceTypeProvider;
+        }
+
+        final Class[] interfaces = clazz.getInterfaces();
+        implementedInterfaces = new InterfaceProvider[interfaces.length];
+        for (int i = 0; i < interfaces.length; i++) {
+            final ReferenceTypeProvider referenceTypeProvider = vm.getReferenceType(interfaces[i]);
+            assert referenceTypeProvider instanceof InterfaceProvider;
+            implementedInterfaces[i] = (InterfaceProvider) referenceTypeProvider;
+        }
+
+    }
+
+    private void initMethods() {
+        final Method[] methods = clazz.getDeclaredMethods();
+        methodProviders = new MethodProvider[methods.length];
+        for (int i = 0; i < methods.length; i++) {
+            methodProviders[i] = new JavaMethodProvider(methods[i], this, vm);
+        }
+    }
+
+    public ClassLoaderProvider classLoader() {
+        return classLoader;
+    }
+
+    public ClassObjectProvider classObject() {
+        // TODO: Consider implementing otherwise.
+        return null;
+    }
+
+    public FieldProvider[] getFields() {
+        // Currently only methods are supported.
+        return new FieldProvider[0];
+    }
+
+    public int getFlags() {
+        return clazz.getModifiers();
+    }
+
+    public InterfaceProvider[] getImplementedInterfaces() {
+        return implementedInterfaces;
+    }
+
+    public ObjectProvider[] getInstances() {
+        // TODO: Consider implementing this otherwise.
+        return new ObjectProvider[0];
+    }
+
+    public MethodProvider[] getMethods() {
+        return methodProviders;
+    }
+
+    public String getName() {
+        return clazz.getSimpleName();
+    }
+
+    public ReferenceTypeProvider[] getNestedTypes() {
+        // Currently no nested types are supported.
+        return new ReferenceTypeProvider[0];
+    }
+
+    public String getSignature() {
+        return JavaTypeDescriptor.forJavaClass(clazz).toString();
+    }
+
+    public String getSignatureWithGeneric() {
+        return clazz.getName();
+    }
+
+    public String getSourceFileName() {
+        // Currently no source file name is supported.
+        return "";
+    }
+
+    public int getStatus() {
+        return ClassStatus.INITIALIZED;
+    }
+
+    public VMValue.Type getType() {
+        if (this.clazz == Boolean.TYPE) {
+            return VMValue.Type.BOOLEAN;
+        } else if (this.clazz == Byte.TYPE) {
+            return VMValue.Type.BYTE;
+        } else if (this.clazz == Character.TYPE) {
+            return VMValue.Type.CHAR;
+        } else if (this.clazz == Double.TYPE) {
+            return VMValue.Type.DOUBLE;
+        } else if (this.clazz == Float.TYPE) {
+            return VMValue.Type.FLOAT;
+        } else if (this.clazz == Integer.TYPE) {
+            return VMValue.Type.INT;
+        } else if (this.clazz == Long.TYPE) {
+            return VMValue.Type.LONG;
+        } else if (this.clazz == Short.TYPE) {
+            return VMValue.Type.SHORT;
+        } else if (this.clazz == Void.TYPE) {
+            return VMValue.Type.VOID;
+        }
+
+        return VMValue.Type.PROVIDER;
+    }
+
+    public int majorVersion() {
+        // TODO: Check if this is correct.
+        return 1;
+    }
+
+    public int minorVersion() {
+        // TODO: Check if this is correct.
+        return 5;
+    }
+
+    public ReferenceTypeProvider getReferenceType() {
+        // TODO: Check if this is correct.
+        return null;
+    }
+}
