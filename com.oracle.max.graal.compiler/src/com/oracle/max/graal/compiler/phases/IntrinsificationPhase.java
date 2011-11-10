a6a0c3f29fa7774e0653ca209a96933e03a28e14
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2011-11-10 12:21:33 +0100

    Enable null checks for intrinsics again.

--- com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/phases/IntrinsificationPhase.java
+++ com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/phases/IntrinsificationPhase.java
@@ -62,7 +62,8 @@ public class IntrinsificationPhase extends Phase {
             // TODO (cwi): I disabled receiver null checks for intrinsics, because I believe that is the intended semantics.
             // when an intrinsic really needs to do a null check, it should do it itself.
             // This behavior is required at least for intrinsics that have also the @Inline annotation.
-            InliningUtil.inline(invoke, intrinsicGraph, false);
+            // TODO (tw): Find another way to fix the issue with Maxine intrinsics semantics.
+            InliningUtil.inline(invoke, intrinsicGraph, true);
         }
     }
 }
