d1f8c54918621a63c696c5ead8773c0a4f116c5a
Gilles Duboscq <gilles.m.duboscq@gmail.com>
2011-11-10 15:24:32 +0100

    fix merge regression

--- com.oracle.max.graal.nodes/src/com/oracle/max/graal/nodes/calc/FloatAddNode.java
+++ com.oracle.max.graal.nodes/src/com/oracle/max/graal/nodes/calc/FloatAddNode.java
@@ -77,7 +77,7 @@ public final class FloatAddNode extends FloatArithmeticNode implements Canonical
 
     public static boolean livesLonger(ValueNode after, ValueNode value, LIRGeneratorTool gen) {
         for (Node usage : value.usages()) {
-            if (usage != after && gen.operand((ValueNode) usage) != null) {
+            if (usage != after && ((ValueNode) usage).operand() != null) {
                 return true;
             }
         }
