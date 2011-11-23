63266a8937f659622b020db881eeb0bde41b93b1
Lukas Stadler <lukas.stadler@jku.at>
2011-11-23 14:39:09 +0100

    fix NodeList.clear(), small changes to some node classes

--- com.oracle.max.graal.nodes/src/com/oracle/max/graal/nodes/PhiNode.java
+++ com.oracle.max.graal.nodes/src/com/oracle/max/graal/nodes/PhiNode.java
@@ -94,6 +94,10 @@ public final class PhiNode extends FloatingNode implements Canonicalizable, Node
         return values.size();
     }
 
+    public void clearValues() {
+        values.clear();
+    }
+
     @Override
     public String toString(Verbosity verbosity) {
         if (verbosity == Verbosity.Name) {
