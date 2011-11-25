759cde1e44ab22af4dd6cbcf10265326c7664fd3
Lukas Stadler <lukas.stadler@jku.at>
2011-11-25 19:42:54 +0100

    some documentation and more useful asserts

---
+++ com.oracle.max.tele.vm/src/com/sun/max/tele/method/VmMachineCodeAccess.java
@@ -0,0 +1,305 @@
+/*
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.tele.method;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+import com.sun.max.lang.*;
+import com.sun.max.program.*;
+import com.sun.max.tele.*;
+import com.sun.max.tele.interpreter.*;
+import com.sun.max.tele.object.*;
+import com.sun.max.tele.util.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.reference.*;
+import com.sun.max.vm.value.*;
+
+/**
+ * The singleton manager for representations of machine code locations, both VM method
+ * compilations in the VM's code cache and blocks of external native code about which less is known.
+ */
+public final class VmMachineCodeAccess extends AbstractVmHolder implements MaxMachineCode, TeleVMCache {
+
+    private static final int TRACE_VALUE = 1;
+
+    private static VmMachineCodeAccess vmMachineCodeAccess;
+
+    public static VmMachineCodeAccess make(TeleVM vm) {
+        if (vmMachineCodeAccess == null) {
+            vmMachineCodeAccess = new VmMachineCodeAccess(vm);
+        }
+        return vmMachineCodeAccess;
+    }
+
+    private final TimedTrace updateTracer;
+
+    private long lastUpdateEpoch = -1L;
+
+    private final String entityName = "Machine Code";
+
+    private final String entityDescription;
+
+    private final ExternalMachineCodeAccess externalMachineCodeAccess;
+
+    /**
+     * A collection of {@link TeleTargetMethod} instances that
+     * have been created for inspection (and registered here) before having been
+     * allocated memory in the VM.  The registration of these will be completed during the
+     * next update cycle after their location and size become known.
+     */
+    private final Set<TeleTargetMethod> unallocatedTeleTargetMethods = new HashSet<TeleTargetMethod>();
+
+    private final Object statsPrinter = new Object() {
+        @Override
+        public String toString() {
+            final StringBuilder msg = new StringBuilder();
+            // TODO (mlvdv) add some stats?
+            return msg.toString();
+        }
+    };
+
+    public VmMachineCodeAccess(TeleVM vm) {
+        super(vm);
+        final TimedTrace tracer = new TimedTrace(TRACE_VALUE, tracePrefix() + " creating");
+        tracer.begin();
+        this.entityDescription = "Remote code pointer creation and management for the " + vm.entityName();
+        this.externalMachineCodeAccess = new ExternalMachineCodeAccess(vm);
+        this.updateTracer = new TimedTrace(TRACE_VALUE, tracePrefix() + " updating");
+        tracer.end(statsPrinter);
+    }
+
+    /** {@inheritDoc}
+     * <p>
+     * Updates the representation of every <strong>method compilation</strong> surrogate
+     * (represented as instances of subclasses of {@link TeleTargetMethod},
+     * in case any of the information in the VM's representation has changed since the previous update.  This should not be
+     * attempted until all information about allocated regions that might contain objects has been updated.
+     */
+    public void updateCache(long epoch) {
+        if (epoch > lastUpdateEpoch) {
+            updateTracer.begin();
+            assert vm().lockHeldByCurrentThread();
+            externalMachineCodeAccess.updateCache(epoch);
+            for (TeleTargetMethod teleTargetMethod : unallocatedTeleTargetMethods) {
+                if (teleTargetMethod.getRegionStart().isNotZero() && teleTargetMethod.getRegionNBytes() != 0) {
+                    // The compilation has been allocated memory in the VM since the last time we looked; complete its registration.
+                    unallocatedTeleTargetMethods.remove(teleTargetMethod);
+                    registerCompilation(teleTargetMethod);
+                }
+            }
+            for (VmCodeCacheRegion region : codeCache().vmCodeCacheRegions()) {
+                region.updateCache(epoch);
+            }
+            lastUpdateEpoch = epoch;
+            updateTracer.end(null);
+        } else {
+            Trace.line(TRACE_VALUE, tracePrefix() + "redundant update epoch=" + epoch);
+        }
+    }
+
+    public String entityName() {
+        return entityName;
+    }
+
+    public String entityDescription() {
+        return entityDescription;
+    }
+
+    public MaxEntityMemoryRegion<MaxMachineCode> memoryRegion() {
+        // This represents no memory allocation; code resides in regions managed by the code cache.
+        return null;
+    }
+
+    public boolean contains(Address address) {
+        // There's no real notion of containing an address here.
+        return false;
+    }
+
+    public TeleObject representation() {
+        // No distinguished object in VM runtime represents this.
+        return null;
+    }
+
+    public MaxMachineCodeRoutine<? extends MaxMachineCodeRoutine> findMachineCode(Address address) {
+        TeleCompilation compilation = findCompilation(address);
+        return (compilation != null) ? compilation : findExternalCode(address);
+    }
+
+    /**
+     * Get the method compilation, if any, whose code cache allocation includes
+     * a given address in the VM, whether or not there is target code at the
+     * specific location.
+     *
+     * @param address memory location in the VM
+     * @return a  method compilation whose code cache allocation includes the address, null if none
+     */
+    private TeleCompilation findCompilationByAllocation(Address address) {
+        TeleCompilation teleCompilation = null;
+        for (VmCodeCacheRegion codeCacheRegion : codeCache().vmCodeCacheRegions()) {
+            teleCompilation = codeCacheRegion.findCompilation(address);
+            if (teleCompilation != null) {
+                break;
+            }
+        }
+        if (teleCompilation == null) {
+            // Not a known method compilation.
+            if (!codeCache().contains(address)) {
+                // The address is not in the code cache.
+                return null;
+            }
+            // Not a known method compilation, but in a code cache region.
+            // Use the interpreter to see if the code manager in the VM knows about it.
+            try {
+                final Reference targetMethodReference = methods().Code_codePointerToTargetMethod.interpret(new WordValue(address)).asReference();
+                // Possible that the address points to an unallocated area of a code region.
+                if (targetMethodReference != null && !targetMethodReference.isZero()) {
+                    objects().makeTeleObject(targetMethodReference);  // Constructor will register the compiled method if successful
+                }
+            } catch (MaxVMBusyException maxVMBusyException) {
+            } catch (TeleInterpreterException e) {
+                // This sometimes happens when the VM process terminates; ignore in those cases
+                if (vm().state().processState() != MaxProcessState.TERMINATED) {
+                    throw TeleError.unexpected(e);
+                }
+            }
+            // If a new method was discovered, the cache will now know about it
+            for (VmCodeCacheRegion codeCacheRegion : codeCache().vmCodeCacheRegions()) {
+                teleCompilation = codeCacheRegion.findCompilation(address);
+                if (teleCompilation != null) {
+                    break;
+                }
+            }
+        }
+        return teleCompilation;
+    }
+
+    public TeleCompilation findCompilation(Address address) {
+        TeleCompilation teleCompilation = findCompilationByAllocation(address);
+        if (teleCompilation != null && teleCompilation.isValidCodeLocation(address)) {
+            return teleCompilation;
+        }
+        return null;
+    }
+
+    public List<MaxCompilation> compilations(TeleClassMethodActor teleClassMethodActor) {
+        final List<MaxCompilation> compilations = new ArrayList<MaxCompilation>(teleClassMethodActor.compilationCount());
+        for (TeleTargetMethod teleTargetMethod : teleClassMethodActor.compilations()) {
+            compilations.add(findCompilationByAllocation(teleTargetMethod.getRegionStart()));
+        }
+        return Collections.unmodifiableList(compilations);
+    }
+
+    public TeleCompilation latestCompilation(TeleClassMethodActor teleClassMethodActor) throws MaxVMBusyException {
+        if (!vm().tryLock()) {
+            throw new MaxVMBusyException();
+        }
+        try {
+            final TeleTargetMethod teleTargetMethod = teleClassMethodActor.getCurrentCompilation();
+            return teleTargetMethod == null ? null : findCompilationByAllocation(teleTargetMethod.getRegionStart());
+        } finally {
+            vm().unlock();
+        }
+    }
+
+    public TeleExternalCodeRoutine registerExternalCode(Address codeStart, long nBytes, String name) throws MaxVMBusyException, IllegalArgumentException, MaxInvalidAddressException {
+        return externalMachineCodeAccess.registerExternalCode(codeStart, nBytes, name);
+    }
+
+    public MaxExternalCodeRoutine findExternalCode(Address address) {
+        return externalMachineCodeAccess.findExternalCode(address);
+    }
+
+    /**
+     * Adds a {@link MaxCompilation} to the registration of compilations for the code region containing it.
+     * This should only be called from a constructor of a {@link TeleTargetMethod} subclass.
+     *
+     * @param teleTargetMethod the compiled method whose memory region is to be added to this registry
+     * @throws IllegalArgumentException when the memory region of {@link teleTargetMethod} overlaps one already in this registry.
+     */
+    public void registerCompilation(TeleTargetMethod teleTargetMethod) {
+        if (teleTargetMethod.getRegionStart().isZero() || teleTargetMethod.getRegionNBytes() == 0) {
+            // The compilation is being constructed, which is to say it exists in the VM but has not
+            // had an allocation of memory assigned to it.
+            unallocatedTeleTargetMethods.add(teleTargetMethod);
+            TeleWarning.message(tracePrefix() + " unallocated TargetMethod registered");
+        } else {
+            // Find the code cache region in which the compilation has been allocated, and add it to
+            // the registry we keep for that code region.
+            final VmCodeCacheRegion codeCacheRegion = codeCache().findCodeCacheRegion(teleTargetMethod.getRegionStart());
+            assert codeCacheRegion != null;
+            teleTargetMethod.setCodeCacheRegion(codeCacheRegion);
+            codeCacheRegion.register(teleTargetMethod);
+        }
+    }
+
+    /**
+     * @see RemoteCodePointerManager#makeCodePointer(Address)
+     */
+    public RemoteCodePointer makeCodePointer(Address address) throws TeleError {
+        final VmCodeCacheRegion codeCacheRegion = codeCache().findCodeCacheRegion(address);
+        if (codeCacheRegion != null) {
+            return codeCacheRegion.codePointerManager().makeCodePointer(address);
+        }
+        return externalMachineCodeAccess.codePointerManager().makeCodePointer(address);
+    }
+
+
+    public TeleCompilation findCompilation(RemoteCodePointer codePointer) {
+        return findCompilation(codePointer.getAddress());
+    }
+
+    public void printSessionStats(PrintStream printStream, int indent, boolean verbose) {
+        final String indentation = Strings.times(' ', indent);
+        final NumberFormat formatter = NumberFormat.getInstance();
+        int compilationCount = 0;
+        int loadedCompilationCount = 0;
+        for (VmCodeCacheRegion codeCacheRegion : codeCache().vmCodeCacheRegions()) {
+            compilationCount += codeCacheRegion.compilationCount();
+            loadedCompilationCount += codeCacheRegion.loadedCompilationCount();
+        }
+        printStream.print(indentation + "Total compilations: " + formatter.format(compilationCount));
+        if (!unallocatedTeleTargetMethods.isEmpty()) {
+            printStream.print(" (" + formatter.format(unallocatedTeleTargetMethods.size()) + " unallocated");
+        }
+        printStream.print(" (code loaded: " + formatter.format(loadedCompilationCount) + ")\n");
+        printStream.println(indentation + "By region:");
+        for (VmCodeCacheRegion codeCacheRegion : codeCache().vmCodeCacheRegions()) {
+            final StringBuffer sb = new StringBuffer();
+            sb.append(codeCacheRegion.entityName() + ": ");
+            sb.append("Compilations=" + formatter.format(codeCacheRegion.compilationCount()));
+            sb.append(", code loaded=" + formatter.format(codeCacheRegion.loadedCompilationCount()));
+            printStream.println(indentation + "    " + sb.toString());
+        }
+        externalMachineCodeAccess.printSessionStats(printStream, indent + 4, verbose);
+    }
+
+    public void writeSummary(PrintStream printStream) {
+        for (VmCodeCacheRegion codeCacheRegion : codeCache().vmCodeCacheRegions()) {
+            codeCacheRegion.writeSummary(printStream);
+        }
+        externalMachineCodeAccess.writeSummary(printStream);
+    }
+
+}
---
+++ com.oracle.max.vm/src/com/sun/max/vm/heap/gcx/OverflowScanState.java
@@ -0,0 +1,225 @@
+/*
+ * Copyright (c) 2010, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.sun.max.vm.heap.gcx;
+
+import static com.sun.max.vm.heap.gcx.TricolorHeapMarker.*;
+
+import com.sun.max.annotate.*;
+import com.sun.max.unsafe.*;
+import com.sun.max.vm.*;
+import com.sun.max.vm.heap.gcx.TricolorHeapMarker.ColorMapScanState;
+import com.sun.max.vm.heap.gcx.TricolorHeapMarker.MarkingStackFlusher;
+
+/**
+ * State of an overflow scan of the tricolor map. A scan of the tricolor map enter this state when the marking stack overflow.
+ * The overflow state remembers the bound of the forward scan to resume it once the overflow is dealt with, in addition to state necessary to
+ * deal with the overflow of the marking stack. This consists in rescanning a part of the tricolor map already scanned to find grey references
+ * dropped from the marking stack.
+ * Sub-class of this class implements various rescan strategies depending on information recorded during forward scan  to increase the precision of
+ * overflow scan.
+ */
+abstract class OverflowScanState extends ColorMapScanState {
+    /**
+     * Specifies where the end of the overflow scan should end. Typically, this is saving the finger of the forward scan.
+     */
+    protected Address endOfScan;
+    /**
+     * Specifies how the marking stack is flushed.
+     */
+    protected MarkingStackFlusher markingStackFlusher;
+    /**
+     * Specifies the discontinuous regions to iterate over.
+     */
+    protected HeapRegionRangeIterable regionsRanges;
+
+    OverflowScanState(TricolorHeapMarker heapMarker) {
+        super(heapMarker);
+    }
+
+    protected final void setMarkingStackFlusher(MarkingStackFlusher flusher) {
+        markingStackFlusher = flusher;
+    }
+
+    /**
+     * VM startup initialization.
+     */
+    void initialize() {
+        heapMarker.markingStack.initialize(markingStackFlusher);
+    }
+
+    void setHeapRegionsRanges(HeapRegionRangeIterable regionsRanges) {
+        this.regionsRanges = regionsRanges;
+    }
+
+    @Override
+    public int rightmostBitmapWordIndex() {
+        return heapMarker.bitmapWordIndex(endOfScan);
+    }
+
+    @INLINE
+    final boolean isRecovering() {
+        return heapMarker.currentScanState == this;
+    }
+
+    final MarkingStackFlusher markingStackFlusher() {
+        return markingStackFlusher;
+    }
+
+    abstract void recoverFromOverflow();
+
+    /**
+     * Visit all grey objects whose mark is within the specified range of words of the color map.
+     * The forward scan finger may be anywhere within the rightmost bitmap word of the range, so
+     * we must test every grey cell against it to stop correctly the recovery scan.
+     *
+     * @param bitmapWordIndex
+     * @param rightmostBitmapWordIndex
+     */
+    void visitGreyObjectsToEndOfScan(int bitmapWordIndex, int rightmostBitmapWordIndex) {
+        // This is slightly different from the forward scan:
+        // The scan ends when reaching the forward scan finger. This one may be located in word shared with some
+        // object visited during overflow scan. So we must check that precise condition to avoid visiting
+        // objects already visited by the forward scan.
+        // We do that only for the last mark bitmap word, the one the forward scan finger points to.
+        if (bitmapWordIndex < rightmostBitmapWordIndex) {
+            final int beforeRightmostBitmapWordIndex = rightmostBitmapWordIndex - 1;
+            // Avoid systematic testing of marked object against the end of scan for all but the last bitmap word.
+            visitGreyObjects(bitmapWordIndex, beforeRightmostBitmapWordIndex);
+            bitmapWordIndex = beforeRightmostBitmapWordIndex;
+        }
+
+        final Pointer colorMapBase = heapMarker.colorMapBase();
+        while (bitmapWordIndex <= rightmostBitmapWordIndex) {
+            long bitmapWord = colorMapBase.getLong(bitmapWordIndex);
+            if (bitmapWord != 0L) {
+                final long greyMarksInWord = bitmapWord & (bitmapWord >>> 1);
+                if (greyMarksInWord != 0L) {
+                    // First grey mark is the least set bit.
+                    final int bitIndexInWord = Pointer.fromLong(greyMarksInWord).leastSignificantBitSet();
+                    final int bitIndexOfGreyCell = (bitmapWordIndex << Word.widthValue().log2numberOfBits) + bitIndexInWord;
+                    Pointer p = heapMarker.addressOf(bitIndexOfGreyCell).asPointer();
+                    if (p.greaterEqual(endOfScan)) {
+                        return;
+                    }
+                    p = markAndVisitCell(p);
+                    // Get bitmap word index at the end of the object. This may avoid reading multiple mark bitmap words
+                    // when marking objects crossing multiple mark bitmap words.
+                    bitmapWordIndex = heapMarker.bitmapWordIndex(p);
+                    continue;
+                } else if ((bitmapWord >>> TricolorHeapMarker.LAST_BIT_INDEX_IN_WORD) == 1L) {
+                    // Mark span two words. Check first bit of next word to decide if mark is grey.
+                    bitmapWord = colorMapBase.getLong(bitmapWordIndex + 1);
+                    if ((bitmapWord & 1) != 0) {
+                        // it is a grey object.
+                        final int bitIndexOfGreyCell = (bitmapWordIndex << Word.widthValue().log2numberOfBits) + TricolorHeapMarker.LAST_BIT_INDEX_IN_WORD;
+                        Pointer p = heapMarker.addressOf(bitIndexOfGreyCell).asPointer();
+                        if (p.greaterEqual(endOfScan)) {
+                            return;
+                        }
+                        p = markAndVisitCell(p);
+                        bitmapWordIndex = heapMarker.bitmapWordIndex(p);
+                        continue;
+                    }
+                }
+            }
+            bitmapWordIndex++;
+        }
+    }
+
+    /**
+     * Visit grey objects between the overflow scan's finger and the forward scan finger. The grey objects are located in the potentially discontinuous
+     * ranges of regions identified by the {@link HeapRegionInfoIterable} arguments.
+     *
+     * @param regionsRanges an iterable over the regions ranges that comprises the grey objects to visit.
+     */
+    protected  void visitGreyObjects(HeapRegionRangeIterable regionsRanges) {
+        final int log2RegionToBitmapWord = HeapRegionConstants.log2RegionSizeInBytes - heapMarker.log2BitmapWord;
+        int rightmostBitmapWordIndex = rightmostBitmapWordIndex();
+        int fingerBitmapWordIndex = heapMarker.bitmapWordIndex(finger);
+        int fingerRegion = HeapRegionConstants.regionStart(finger).minus(heapMarker.coveredAreaStart).unsignedShiftedRight(HeapRegionConstants.log2RegionSizeInBytes).toInt();
+        regionsRanges.reset(fingerRegion);
+
+        while (regionsRanges.hasNext()) {
+            final RegionRange regionRange = regionsRanges.next();
+            final int rangeLeftmostBitmapWordIndex = regionRange.firstRegion() << log2RegionToBitmapWord;
+            final int rangeRightmostBitmapWordIndex = rangeLeftmostBitmapWordIndex + (regionRange.numRegions() << log2RegionToBitmapWord);
+            if (rangeLeftmostBitmapWordIndex > fingerBitmapWordIndex) {
+                fingerBitmapWordIndex = rangeLeftmostBitmapWordIndex;
+            }
+            if (rangeRightmostBitmapWordIndex > rightmostBitmapWordIndex) {
+                visitGreyObjectsToEndOfScan(fingerBitmapWordIndex, rightmostBitmapWordIndex);
+                break;
+            }
+            visitGreyObjects(fingerBitmapWordIndex, rangeRightmostBitmapWordIndex);
+        }
+    }
+
+    @INLINE
+    protected void verifyHasNoGreyMarks(Address end) {
+        if (!TricolorHeapMarker.VerifyGreyLessAreas) {
+            return;
+        }
+        if (regionsRanges != null) {
+            regionsRanges.reset();
+            heapMarker.verifyHasNoGreyMarks(regionsRanges, end);
+        } else {
+            heapMarker.verifyHasNoGreyMarks(heapMarker.coveredAreaStart, end);
+        }
+    }
+
+    protected void beginRecovery() {
+        final ForwardScanState forwardScanState = heapMarker.forwardScanState;
+        heapMarker.startTimer(heapMarker.recoveryScanTimer);
+        heapMarker.currentScanState = this;
+        // set the upper bound for rescanning to the finger of the forward scan
+        endOfScan = forwardScanState.finger;
+        // cache rightmost locally.
+        rightmost = forwardScanState.rightmost;
+        markingStackFlusher.setScanState(this);
+        if (MaxineVM.isDebug() && TraceMarking) {
+            Log.println("Begin Overflow Scan");
+            heapMarker.traceMark(endOfScan, " => endOfScan\n");
+            heapMarker.traceMark(rightmost, " => rightmost\n");
+        }
+    }
+
+    protected void endRecovery() {
+        final ForwardScanState forwardScanState = heapMarker.forwardScanState;
+        verifyHasNoGreyMarks(forwardScanState.finger);
+        forwardScanState.rightmost = rightmost;
+        markingStackFlusher.setScanState(forwardScanState);
+        heapMarker.currentScanState = forwardScanState;
+        heapMarker.stopTimer(heapMarker.recoveryScanTimer);
+        if (MaxineVM.isDebug() && TraceMarking) {
+            Log.print("End Overflow Scan (# mark stack overflow = ");
+            Log.print(numMarkinkgStackOverflow); Log.println(")");
+            heapMarker.traceMark(finger, " => overflow finger\n");
+            heapMarker.traceMark(rightmost, " => rightmost\n");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "overflow scan";
+    }
+}
