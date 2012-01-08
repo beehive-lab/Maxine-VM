8fad51f5f724b169eb6ce0dc7bd2c724371cf812
Michael Van De Vanter <michael.van.de.vanter@oracle.com>
2012-01-07 18:45:10 -0800

    Inspector: checkstyle, copyright fix

--- com.oracle.max.vm/test/test/inspector/DeadObjectTest.java
+++ com.oracle.max.vm/test/test/inspector/DeadObjectTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,6 +31,7 @@ public class DeadObjectTest {
     /**
      * @param args
      */
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
         SimpleObject aSimpleObject;
 
@@ -42,6 +43,7 @@ public class DeadObjectTest {
 
         System.gc();
         System.out.println("Object=" + aSimpleObject);
+
         System.out.println("Demo1 end");
     }
 
