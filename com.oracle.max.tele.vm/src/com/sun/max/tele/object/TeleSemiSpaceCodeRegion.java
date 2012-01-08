97f05504c678be26b912f21066acb34470d95d95
Michael Van De Vanter <michael.van.de.vanter@oracle.com>
2012-01-07 18:10:30 -0800

    Inspector: fix a nasty circularity in the initialization of code region descriptions; the more we try to model correctly the more these appear

--- com.oracle.max.tele.vm/src/com/sun/max/tele/object/TeleSemiSpaceCodeRegion.java
+++ com.oracle.max.tele.vm/src/com/sun/max/tele/object/TeleSemiSpaceCodeRegion.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -74,6 +74,11 @@ public final class TeleSemiSpaceCodeRegion extends TeleCodeRegion {
     @Override
     public boolean isManaged() {
         return true;
+    }
+
+    @Override
+    public boolean isBootCodeRegion() {
+        return false;
     }
 
 }
