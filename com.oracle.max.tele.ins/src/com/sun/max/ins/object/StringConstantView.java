ce7b1f75e8067a6a3ad7d4f4c6bf88c0a6cbd569
Michael Van De Vanter <michael.van.de.vanter@oracle.com>
2012-01-07 18:12:38 -0800

    Inspector: rework specialized (tabbed) object views to respond consistently when displaying memory for objects that have died.

--- com.oracle.max.tele.ins/src/com/sun/max/ins/object/StringConstantView.java
+++ com.oracle.max.tele.ins/src/com/sun/max/ins/object/StringConstantView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -68,6 +68,9 @@ public class StringConstantView extends ObjectView<StringConstantView> {
         stringPane = StringPane.createStringPane(this, new StringSource() {
             public String fetchString() {
                 return teleStringConstant.getString();
+            }
+            public boolean isLive() {
+                return teleObject().isLive();
             }
         });
         tabbedPane.add("string value", stringPane);
