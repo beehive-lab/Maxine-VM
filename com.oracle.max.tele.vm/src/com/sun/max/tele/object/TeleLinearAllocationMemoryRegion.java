f6791d2b52e89d9373e84a2abda6e8df261f3e3f
Michael Van De Vanter <michael.van.de.vanter@oracle.com>
2012-01-23 15:31:30 -0800

    Insector: comment

--- com.oracle.max.tele.vm/src/com/sun/max/tele/object/TeleLinearAllocationMemoryRegion.java
+++ com.oracle.max.tele.vm/src/com/sun/max/tele/object/TeleLinearAllocationMemoryRegion.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -99,6 +99,7 @@ public class TeleLinearAllocationMemoryRegion extends TeleRuntimeMemoryRegion {
         try {
             final Reference markReference = fields().LinearAllocationMemoryRegion_mark.readReference(reference());
             markCache = markReference.readWord(AtomicWord.valueOffset()).asPointer();
+            // This essentially marks the usage cache as dirty.
             usageCache = MaxMemoryRegion.Util.NULL_MEMORY_USAGE;
         } catch (DataIOError dataIOError) {
             // No update; data read failed for some reason other than VM availability
