fc361fd1e4f5085aae65434cf036d05662fb9af9
Michael Van De Vanter <michael.van.de.vanter@oracle.com>
2012-01-31 16:27:01 -0800

    Inspector:  fix a display bug with the VMLog and Allocations Views when turning the filter option on and off.

--- com.oracle.max.tele.ins/src/com/sun/max/ins/gui/InspectorFrame.java
+++ com.oracle.max.tele.ins/src/com/sun/max/ins/gui/InspectorFrame.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -146,6 +146,15 @@ public interface InspectorFrame extends RootPaneContainer, Prober {
      * @see JInternalFrame#pack()
      */
     void pack();
+
+    /**
+     * Causes the container to lay out its hierarchy again, after it
+     * has been displayed; it should be
+     * called whenever a member is added or deleted.
+     *
+     * @see {@link Container#validate()}
+     */
+    void validate();
 
     /**
      * Marks frame as needing to lay out contents again.
