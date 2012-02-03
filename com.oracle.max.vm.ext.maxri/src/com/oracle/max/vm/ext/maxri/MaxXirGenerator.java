8be36232f14f83a5bab129d2a74fbba04d583c67
Laurent Daynes <Laurent.Daynes@oracle.com>
2012-02-03 12:02:22 +0100

    Fixed import.

--- com.oracle.max.vm.ext.maxri/src/com/oracle/max/vm/ext/maxri/MaxXirGenerator.java
+++ com.oracle.max.vm.ext.maxri/src/com/oracle/max/vm/ext/maxri/MaxXirGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -57,7 +57,6 @@ import com.sun.max.vm.compiler.*;
 import com.sun.max.vm.compiler.target.*;
 import com.sun.max.vm.debug.*;
 import com.sun.max.vm.heap.*;
-import com.sun.max.vm.heap.gcx.*;
 import com.sun.max.vm.layout.*;
 import com.sun.max.vm.object.*;
 import com.sun.max.vm.runtime.*;
