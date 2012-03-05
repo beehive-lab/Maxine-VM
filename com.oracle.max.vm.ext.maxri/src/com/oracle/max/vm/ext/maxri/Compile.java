847c1126fbb4c48a40a3cda8c1990be7a1980f6d
Doug Simon <doug.simon@oracle.com>
2012-03-05 12:54:10 +0100

    Fixed issue with resolving VM classes from boot classes in hosted mode.

--- com.oracle.max.vm.ext.maxri/src/com/oracle/max/vm/ext/maxri/Compile.java
+++ com.oracle.max.vm.ext.maxri/src/com/oracle/max/vm/ext/maxri/Compile.java
@@ -160,6 +160,7 @@ public class Compile {
         }
 
         final Classpath classpath = Classpath.fromSystem();
+//        final List<MethodActor> methods = new MyMethodFinder().find(arguments, classpath, HostedVMClassLoader.HOSTED_VM_CLASS_LOADER, null);
         final List<MethodActor> methods = new MyMethodFinder().find(arguments, classpath, Compile.class.getClassLoader(), null);
         final ProgressPrinter progress = new ProgressPrinter(out, methods.size(), verboseOption.getValue(), false);
 
