bbd3fdf72ebab9dc516197407e2c375c0bd4f450
Michael Van De Vanter <michael.van.de.vanter@oracle.com>
2012-06-04 15:52:07 -0700

    Inspector:  add new RemoteObjectStatus QUASI, to be used for references and remote objects that deal with object-formatted memory that does not hold a live object.

--- com.oracle.max.tele.vm/src/com/sun/max/tele/object/RemoteObjectStatus.java
+++ com.oracle.max.tele.vm/src/com/sun/max/tele/object/RemoteObjectStatus.java
@@ -24,41 +24,46 @@ package com.sun.max.tele.object;
 
 import java.util.*;
 
+import com.sun.max.tele.reference.*;
+
 /**
- * The status of an object represented in VM memory, from the perspective of a remove viewer that
+ * The status of an object represented in VM memory, from the perspective of a remove vantage that
  * may not have complete information about an object or even, in the case of GC, may have a slightly
  * different view of the object's status at certain times than does the VM itself.
  * <ul>
  * <li> {@link #LIVE}: Determined to be reachable as of the most recent collection, or <em>presumed</em>
- * to be reachable during certain GC phases when its reachability has not yet been det.</li>
- * <li> {@link #UNKNOWN}: During liveness analysis:  formerly live, not yet determined reachable</li>
- * <li> {@link #DEAD}: Unreachable object; no assumptions about memory may be made</li>
+ * to be reachable during certain GC phases when its reachability has not yet been determined.</li>
+ * <li> {@link #QUASI}: A region of VM memory that is formatted as an object (or nearly so) that
+ * would be interesting and useful to view as an object, but which is not a live object from the
+ * perspective of the VM; examples might include forwarded objects (during specific phases of
+ * a relocating GC) or free memory chunks formatted as objects for GC implementation convenience;</li>
+ * <li> {@link #DEAD}: Unreachable memory, usually at a site that formerly held a live or
+ * quasi-object but which no longer does; no assumptions about memory may be made</li>
  * </ul>
  * <p>
- * Possible transitions:
- * <ul>
- * <li> {@link #LIVE} --> {@link #UNKNOWN}: at the beginning of liveness analysis,
- * when the status of all objects becomes uncertain until further notice;</li>
- * <li> {@link #UNKNOWN} --> {@link #LIVE}: during liveness analysis, when an object
- * is determined to be reachable;</li>
- * <li> {@link #UNKNOWN} --> {@link #DEAD}: at the conclusion of liveness analysis, when
- * it becomes certain that an object is unreachable;</li>
- * </ul>
+ * {@linkplain RemoteReference Remote references} and thus {@linkplain TeleObject remote objects} can
+ * change status in response to both changes by memory managers in the VM and by discovery of additional
+ * information about the status of VM memory management.
  */
 public enum RemoteObjectStatus {
 
     /**
-     * The region of memory is in a live allocation area and represents an object
-     * that was determined to be reachable as of the most recent collection.
+     * The region of memory is either in a live allocation area (representing an object
+     * that was determined to be reachable as of the most recent collection) or is in a region
+     * where GC is underway and for which reachability has not yet been determined.
      */
     LIVE("Live", "Determined to be reachable as of the most recent collection"),
 
-    /**
-     * Only during liveness analysis: the region of memory was live as of the previous collection
-     * and has not yet been determined live during the current analysis.
-     */
     @Deprecated
     UNKNOWN("Unknown", "During liveness analysis:  formerly live, not yet determined reachable"),
+
+    /**
+     * The region of memory is formatted as (or mostly as) a VM object, and it can be useful to view
+     * the memory's contents as if it were an object, but it does not represent a LIVE object.  Examples
+     * include old copies of forwarded objects (while GC is still underway), explicit representation of
+     * free memory chunks, and others.
+     */
+    QUASI("Quasi", "Formatted as an object, but not a LIVE object"),
 
     /**
      * The region of memory formerly represented an object that has been collected.
@@ -88,6 +93,15 @@ public enum RemoteObjectStatus {
      */
     public boolean isLive() {
         return this == LIVE;
+    }
+
+    /**
+     * Does the memory represent interesting information, represented in VM object format, that is not a live object?
+     *
+     * @return {@code this != } {@link #QUASI}.
+     */
+    public boolean isQuasi() {
+        return this == QUASI;
     }
 
     /**
