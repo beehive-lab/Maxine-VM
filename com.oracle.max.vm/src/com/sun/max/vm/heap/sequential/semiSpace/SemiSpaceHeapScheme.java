1ccb9d79f74d5a4130d576c543e1dd94124ade52
Laurent Daynes <laurent.daynes@oracle.com>
2012-08-14 13:15:44 +0200

    gc: fixed MSHeapScheme regression gc: moved detail GC log to DebugHeap class to increase sharing of GC tracing across heap scheme. gc: Removed SemiSpaceDebugHeap and moved and generalized tracing/verifying code to DebugHeap to increase reuse across heap schemes

--- com.oracle.max.vm/src/com/sun/max/vm/heap/sequential/semiSpace/SemiSpaceHeapScheme.java
+++ com.oracle.max.vm/src/com/sun/max/vm/heap/sequential/semiSpace/SemiSpaceHeapScheme.java
@@ -978,8 +978,9 @@ public class SemiSpaceHeapScheme extends HeapSchemeWithTLAB implements CellVisit
         if (MaxineVM.isDebug()) {
             if (Heap.logGCPhases()) {
                 phaseLogger.logVerifyingHeapObjects(Interval.BEGIN);
+                phaseLogger.logVerifyingRegion(toSpace, toSpace.start().asPointer(), allocationMark());
             }
-            SemiSpaceDebugHeap.verifyRegion(toSpace, toSpace.start().asPointer(), allocationMark(), toSpace, refVerifier);
+            DebugHeap.verifyRegion(toSpace, toSpace.start().asPointer(), allocationMark(), toSpace, refVerifier, detailLogger);
             if (Heap.logGCPhases()) {
                 phaseLogger.logVerifyingHeapObjects(Interval.END);
                 phaseLogger.logVerifyingCodeObjects(Interval.BEGIN);
@@ -1000,7 +1001,10 @@ public class SemiSpaceHeapScheme extends HeapSchemeWithTLAB implements CellVisit
 
     private void verifyCodeRegion(CodeRegion cr) {
         if (!cr.size().isZero()) {
-            SemiSpaceDebugHeap.verifyRegion(cr, cr.start().asPointer(), cr.getAllocationMark(), toSpace, refVerifier);
+            if (Heap.logGCPhases()) {
+                phaseLogger.logVerifyingRegion(cr, cr.start().asPointer(), cr.getAllocationMark());
+            }
+            DebugHeap.verifyRegion(cr, cr.start().asPointer(), cr.getAllocationMark(), toSpace, refVerifier, detailLogger);
         }
     }
 
@@ -1405,147 +1409,10 @@ public class SemiSpaceHeapScheme extends HeapSchemeWithTLAB implements CellVisit
         return timeLogger;
     }
 
-    @HOSTED_ONLY
-    @VMLoggerInterface
-    private interface DetailLoggerInterface {
-        void visitCell(
-            @VMLogParam(name = "cell") Pointer cell);
 
-        void forward(
-            @VMLogParam(name = "classActor") ClassActor classActor,
-            @VMLogParam(name = "fromCell") Pointer fromCell,
-            @VMLogParam(name = "toCell") Pointer toCell,
-            @VMLogParam(name = "size") int size);
-
-        void skipped(
-            @VMLogParam(name = "padBytes") int padBytes);
-
-        void verifyObject(
-            @VMLogParam(name = "hubClassActor") ClassActor hubClassActor,
-            @VMLogParam(name = "cell") Pointer cell,
-            @VMLogParam(name = "size") int size);
-    }
-
-    public static final class DetailLogger extends DetailLoggerAuto {
-
-        DetailLogger() {
-            super("GCDetail", "detailed operation.");
-        }
-
-        @Override
-        public void checkOptions() {
-            super.checkOptions();
-            checkDominantLoggerOptions(Heap.gcAllLogger);
-        }
-
-        @Override
-        protected void traceVisitCell(Pointer cell) {
-            Log.print("Visiting cell ");
-            Log.println(cell);
-        }
-
-        @Override
-        protected void traceForward(ClassActor classActor, Pointer fromCell, Pointer toCell, int size) {
-            Log.print("Forwarding ");
-            Log.print(classActor.name.string);
-            Log.print(" from ");
-            Log.print(fromCell);
-            Log.print(" to ");
-            Log.print(toCell);
-            Log.print(" [");
-            Log.print(size);
-            Log.println(" bytes]");
-        }
-
-        @Override
-        protected void traceSkipped(int padBytes) {
-            Log.print("Skipped ");
-            Log.print(padBytes);
-            Log.println(" cell padding bytes");
-        }
-
-        @Override
-        protected void traceVerifyObject(ClassActor hubClassActor, Pointer cell, int size) {
-            Log.print("Verifying ");
-            Log.print(hubClassActor.name.string);
-            Log.print(" at ");
-            Log.print(cell);
-            Log.print(" [");
-            Log.print(size);
-            Log.println(" bytes]");
-        }
-    }
-
-    static final DetailLogger detailLogger = new DetailLogger();
+    static final DebugHeap.DetailLogger detailLogger = new DebugHeap.DetailLogger();
 
 // START GENERATED CODE
-    private static abstract class DetailLoggerAuto extends com.sun.max.vm.log.VMLogger {
-        public enum Operation {
-            Forward, Skipped, VerifyObject,
-            VisitCell;
-
-            @SuppressWarnings("hiding")
-            public static final Operation[] VALUES = values();
-        }
-
-        private static final int[] REFMAPS = null;
-
-        protected DetailLoggerAuto(String name, String optionDescription) {
-            super(name, Operation.VALUES.length, optionDescription, REFMAPS);
-        }
-
-        @Override
-        public String operationName(int opCode) {
-            return Operation.VALUES[opCode].name();
-        }
-
-        @INLINE
-        public final void logForward(ClassActor classActor, Pointer fromCell, Pointer toCell, int size) {
-            log(Operation.Forward.ordinal(), classActorArg(classActor), fromCell, toCell, intArg(size));
-        }
-        protected abstract void traceForward(ClassActor classActor, Pointer fromCell, Pointer toCell, int size);
-
-        @INLINE
-        public final void logSkipped(int padBytes) {
-            log(Operation.Skipped.ordinal(), intArg(padBytes));
-        }
-        protected abstract void traceSkipped(int padBytes);
-
-        @INLINE
-        public final void logVerifyObject(ClassActor hubClassActor, Pointer cell, int size) {
-            log(Operation.VerifyObject.ordinal(), classActorArg(hubClassActor), cell, intArg(size));
-        }
-        protected abstract void traceVerifyObject(ClassActor hubClassActor, Pointer cell, int size);
-
-        @INLINE
-        public final void logVisitCell(Pointer cell) {
-            log(Operation.VisitCell.ordinal(), cell);
-        }
-        protected abstract void traceVisitCell(Pointer cell);
-
-        @Override
-        protected void trace(Record r) {
-            switch (r.getOperation()) {
-                case 0: { //Forward
-                    traceForward(toClassActor(r, 1), toPointer(r, 2), toPointer(r, 3), toInt(r, 4));
-                    break;
-                }
-                case 1: { //Skipped
-                    traceSkipped(toInt(r, 1));
-                    break;
-                }
-                case 2: { //VerifyObject
-                    traceVerifyObject(toClassActor(r, 1), toPointer(r, 2), toInt(r, 3));
-                    break;
-                }
-                case 3: { //VisitCell
-                    traceVisitCell(toPointer(r, 1));
-                    break;
-                }
-            }
-        }
-    }
-
     private static abstract class PhaseLoggerAuto extends com.sun.max.vm.heap.HeapScheme.PhaseLogger {
         public enum Operation {
             MovingReachable, ProcessingSpecialReferences, ScanningBootHeap,
