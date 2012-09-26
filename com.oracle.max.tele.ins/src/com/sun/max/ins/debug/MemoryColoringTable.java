23d5fd4f14798c0119ae0ba7db1891f47ecb281f
Michael Van De Vanter <michael.van.de.vanter@oracle.com>
2012-09-25 20:58:02 -0700

    Inspector:  refactor some memory region classes to solve a problem with Memory Views on memory regikons whose names in the VM change dynamically.

--- com.oracle.max.tele.ins/src/com/sun/max/ins/debug/MemoryColoringTable.java
+++ com.oracle.max.tele.ins/src/com/sun/max/ins/debug/MemoryColoringTable.java
@@ -285,7 +285,7 @@ public final class MemoryColoringTable extends InspectorTable {
 
         public MarkBitmapTableModel(Inspection inspection, MaxMarkBitmap markBitmap) {
             super(inspection, markBitmap.coveredMemoryRegion().start());
-            setMemoryRegion(new MemoryWordRegion(inspection.vm(), markBitmap.coveredMemoryRegion()));
+            setMemoryRegion(new MemoryWordRegion(inspection(), markBitmap.coveredMemoryRegion()));
             //positionBias = memoryWordRegion.start().minus(origin).dividedBy(getWordSize()).toInt();
         }
 
@@ -338,7 +338,7 @@ public final class MemoryColoringTable extends InspectorTable {
             final Address address = memoryWordRegion.getAddressAt(row);
             MaxMemoryRegion rowMemoryRegion = addressToMemoryRegion.get(address.toLong());
             if (rowMemoryRegion == null) {
-                rowMemoryRegion = new MemoryWordRegion(vm(), address, 1);
+                rowMemoryRegion = new MemoryWordRegion(inspection(), address, 1);
                 addressToMemoryRegion.put(address.toLong(), rowMemoryRegion);
             }
             return rowMemoryRegion;
