6909c62c67dcf9ca4719c83d739d84cff0901aa0
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2014-06-10 16:20:04 +0300

    Remove bogus test files

--- Byte.java
+++
@@ -1,8 +0,0 @@
-
-public class Byte
-{
-public static void main(String args[]) {
-System.err.println(Long.valueOf("FFFFFFFF",16));
-System.err.println(String.format("0x%8s", Integer.toHexString(0xff)).replace(' ', '0'));
-}
-};
