b0cf26f661f7d70b582cdd0ff38f6f3313e95140
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2014-06-17 22:55:32 +0300

    Refactor ARMV7

--- com.oracle.max.asm/src/com/oracle/max/asm/target/armv7/ARMV7.java
+++ com.oracle.max.asm/src/com/oracle/max/asm/target/armv7/ARMV7.java
@@ -1,30 +1,24 @@
 /*
- * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved. DO NOT ALTER OR REMOVE COPYRIGHT NOTICES
+ * OR THIS FILE HEADER.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * This code is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License version 2 only, as published by the Free Software Foundation.
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License version 2 for
+ * more details (a copy is included in the LICENSE file that accompanied this code).
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License version 2 along with this work; if not, write to
+ * the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA or visit www.oracle.com if you need
+ * additional information or have any questions.
  */
 package com.oracle.max.asm.target.armv7;
 
 import static com.oracle.max.cri.intrinsics.MemoryBarriers.*;
 import static com.sun.cri.ci.CiRegister.RegisterFlag.*;
-import static com.sun.cri.ci.CiKind.*;
 
 import com.sun.cri.ci.*;
 import com.sun.cri.ci.CiRegister.RegisterFlag;
@@ -33,8 +27,7 @@ import com.sun.cri.ci.CiRegister.RegisterFlag;
  * Represents the ARMv7 architecture.
  */
 public class ARMV7 extends CiArchitecture {
-    //public static String name = new String("ARMV7");
-    // General purpose CPU registers
+
     public static final CiRegister r0 = new CiRegister(0, 0, 4, "r0", CPU, RegisterFlag.Byte);
     public static final CiRegister r1 = new CiRegister(1, 1, 4, "r1", CPU, RegisterFlag.Byte);
     public static final CiRegister r2 = new CiRegister(2, 2, 4, "r2", CPU, RegisterFlag.Byte);
@@ -43,22 +36,17 @@ public class ARMV7 extends CiArchitecture {
     public static final CiRegister r5 = new CiRegister(5, 5, 4, "r5", CPU, RegisterFlag.Byte);
     public static final CiRegister r6 = new CiRegister(6, 6, 4, "r6", CPU, RegisterFlag.Byte);
     public static final CiRegister r7 = new CiRegister(7, 7, 4, "r7", CPU, RegisterFlag.Byte);
-
-    public static final CiRegister r8  = new CiRegister(8,  8,  4, "r8", CPU, RegisterFlag.Byte);
-    public static final CiRegister r9  = new CiRegister(9,  9,  4, "r9", CPU, RegisterFlag.Byte);
+    public static final CiRegister r8 = new CiRegister(8, 8, 4, "r8", CPU, RegisterFlag.Byte);
+    public static final CiRegister r9 = new CiRegister(9, 9, 4, "r9", CPU, RegisterFlag.Byte);
     public static final CiRegister r10 = new CiRegister(10, 10, 4, "r10", CPU, RegisterFlag.Byte);
     public static final CiRegister r11 = new CiRegister(11, 11, 4, "r11", CPU, RegisterFlag.Byte);
     public static final CiRegister r12 = new CiRegister(12, 12, 4, "r12", CPU, RegisterFlag.Byte);
-    // R13 -> SP
     public static final CiRegister r13 = new CiRegister(13, 13, 4, "r13", CPU, RegisterFlag.Byte);
-    // R14 -> LR
     public static final CiRegister r14 = new CiRegister(14, 14, 4, "r14", CPU, RegisterFlag.Byte);
-    // R15 -> PC
     public static final CiRegister r15 = new CiRegister(15, 15, 4, "r15", CPU, RegisterFlag.Byte);
 
-
-    //TODO: CiArchitecture doesn't allow mixed-size registers (pretending we have 4 byte FP regs)
-    //TODO: Might be something to do with the spillSlotSize the size of the stack slot used to spill
+    // TODO: CiArchitecture doesn't allow mixed-size registers (pretending we have 4 byte FP regs)
+    // TODO: Might be something to do with the spillSlotSize the size of the stack slot used to spill
     // TODO the value of the register
     // APN I think the spill slot size can be specified when creating a register so if it is possible to
     // do this per class ie FPU? then it is possible to state that the DO..D15 registers are 64bit and the
@@ -115,51 +103,25 @@ public class ARMV7 extends CiArchitecture {
     public static final CiRegister s29 = new CiRegister(61, 29, 4, "s29", RegisterFlag.FPU);
     public static final CiRegister s30 = new CiRegister(62, 30, 4, "s30", RegisterFlag.FPU);
     public static final CiRegister s31 = new CiRegister(63, 31, 4, "s31", RegisterFlag.FPU);
-    public static final CiRegister[] cpuRegisters = {
-        r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15
-    };
 
-    public static final CiRegister rip = new CiRegister(64, -1, 0, "rip");
+    public static final CiRegister[] cpuRegisters = { r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15};
 
+    public static final CiRegister[] floatRegisters = { d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15,
+                    s16, s17, s18, s19, s20, s21, s22, s23, s24, s25, s26, s27, s28, s29, s30, s31};
 
-    // TODO: not including the other 32 advanced SIMD floating point registers out of laziness for now
-    // also because we are not targetting advanced SIMD yet.
-    public static final CiRegister[] floatRegisters = {
-        d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15,
-        s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16, s17, s18, s19, s20,
-        s21, s22, s23, s24, s25, s26, s27, s28, s29, s30, s31
-    };
-
-
-    /**
-     * Register used to construct an instruction-relative address.
-     */
-
-
-    public static final CiRegister[] allRegisters = {
-        r0, r1, r2, r3, r4, r5, r6, r7,
-        r8, r9, r10, r11, r12, r13, r14, r15,
-        d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15,
-        s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15,
-        s16, s17, s18, s19, s20,
-        s21, s22, s23, s24, s25, s26, s27, s28, s29, s30, s31
-    };
-
+    public static final CiRegister[] allRegisters = { r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, s0,
+                    s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16, s17, s18, s19, s20, s21, s22, s23, s24, s25, s26, s27, s28, s29, s30, s31};
 
     // TODO: checkout load/store ordering on A15
     // TODO: checkout nativeDisplacementOffset on A15
     public ARMV7() {
-        super("ARMV7",
-                4,
-                ByteOrder.BigEndian,
-                allRegisters,
-                LOAD_STORE | STORE_STORE,
-                1,
-                s31.number + 1,
-                //r15.encoding + 1,
-                4);
+        super("ARMV7", 4, ByteOrder.BigEndian, allRegisters, LOAD_STORE | STORE_STORE, 1, s31.number + 1, 4);
     }
-    public static final CiRegisterValue RSP = r13.asValue(Int); // APN might need to be an int?
+
+    public static final CiRegister RSP = r13;
+    public static final CiRegister LR = r14;
+    public static final CiRegister PC = r15;
+    public static final CiRegister rip = new CiRegister(64, -1, 0, "rip");
 
     @Override
     public boolean isARM() {
