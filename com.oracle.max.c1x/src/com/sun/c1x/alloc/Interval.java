3ece26d16df70052652e3b87bab082a01f6d8fd1
Dr. Andy Nisbet <andy.nisbet@manchester.ac.uk>
2014-09-18 15:28:53 +0000

    Interval.java reenabled assertion

--- com.oracle.max.c1x/src/com/sun/c1x/alloc/Interval.java
+++ com.oracle.max.c1x/src/com/sun/c1x/alloc/Interval.java
@@ -511,12 +511,6 @@ public final class Interval {
             assert this.location == null || this.location.isRegister() : "cannot re-assign location for " + this;
             assert location.isStackSlot();
             assert location.kind != CiKind.Illegal;
-            /*if(location.kind != this.kind) {
-                 this.location = location;
-                 this.kind = location.kind;
-                 System.out.println("Interval going to fail RegisterAllocation so it is bypassed and the assert location.kind == this.kind is TURNED OFF");
-		// DIRTY HACK FIXME
-	    } else*/
             assert location.kind == this.kind;
         }
         this.location = location;
