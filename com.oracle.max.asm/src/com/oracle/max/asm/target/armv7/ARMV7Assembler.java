470a280f7d96cb05c568df19a588198777418582
Dr. Andy Nisbet <andy.nisbet@manchester.ac.uk>
2014-10-23 10:37:18 +0100

    Fix to allow ARM boot image to compile ... seems to be some issue relating to maximum number of instructions used to implement a call and the before/after that is then passed to the Safepoint ... ask Christos to explain. Had to remove nop(4) from the indirect call else an assertion was thrown during T1XTemplate compilation.

--- com.oracle.max.asm/src/com/oracle/max/asm/target/armv7/ARMV7Assembler.java
+++ com.oracle.max.asm/src/com/oracle/max/asm/target/armv7/ARMV7Assembler.java
@@ -1082,11 +1082,11 @@ public class ARMV7Assembler extends AbstractAssembler {
     }
 
     public final void call(CiRegister target) {
-        nop(4);
+        //nop(4);
         // THIs is an indirect call, assuming the contents of the registers are a memory location we need to load
         //add(ConditionFlag.Always, false, ARMV7.r15, ARMV7.r12, 0, 0);
-        ldrImmediate(ConditionFlag.Always,1,0,0,ARMV7.r12,target,0);
-	int instruction = blxHelper(ConditionFlag.Always,target);
+       ldrImmediate(ConditionFlag.Always,1,0,0,ARMV7.r12,target,0);
+	int instruction = blxHelper(ConditionFlag.Always,ARMV7.r12);
 	emitInt(instruction);
     }
 
