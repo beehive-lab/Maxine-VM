2bc702c3624fe2e1a962a9acd6dc5457301f0f01
Andrey Rodchenko <arodchen@users.noreply.github.com>
2014-12-07 18:16:28 +0000

    Minor fixes in comments.

--- com.oracle.max.vm/src/com/sun/max/vm/jdk/JDK_sun_management_ThreadImpl.java
+++ com.oracle.max.vm/src/com/sun/max/vm/jdk/JDK_sun_management_ThreadImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,7 @@ package com.sun.max.vm.jdk;
 import com.sun.max.annotate.*;
 
 /**
- * Method substitutions for sun.management.ThreadImpl.
+ * Method substitutions for sun.management.ThreadImpl.
  */
 @METHOD_SUBSTITUTIONS(className = "sun.management.ThreadImpl")
 final class JDK_sun_management_ThreadImpl {
