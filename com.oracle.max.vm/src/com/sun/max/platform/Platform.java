d3e04f671eb61d65ecaef0d4ae3087a1225ce1e2
Jiaqi Liu <jiaqi.liu.cs@gmail.com>
2015-04-19 18:18:51 +0100

    [aarch64] AArch64/Linux stackAlignment = 16

--- com.oracle.max.vm/src/com/sun/max/platform/Platform.java
+++ com.oracle.max.vm/src/com/sun/max/platform/Platform.java
@@ -168,6 +168,13 @@ public final class Platform {
         } else if (isa == ISA.AARCH64) {
             arch = new AARCH64();
             if (os == OS.LINUX) {
+                // Linux apparently also requires it for functions that pass floating point functions on the stack.
+                // One such function in the Maxine code base is log_print_float() in log.c which passes a float
+                // value to fprintf on the stack. However, gcc doesn't fix the alignment itself so we simply
+                // adopt the global convention on Linux of 16-byte alignment for stacks. If this is a performance issue,
+                // this can later be refined to only be for JNI stubs that pass a float or double to native code.
+
+                // Solaris has the same issues.
                 stackAlignment = 16;
             } else {
                 throw FatalError.unexpected("Unimplemented stack alignment: " + os);
