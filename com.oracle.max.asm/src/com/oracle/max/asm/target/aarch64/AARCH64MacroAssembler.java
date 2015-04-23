3c622d093f08e1c5c6db324f54cc9d367e2176c9
Jiaqi Liu <jiaqi.liu.cs@gmail.com>
2015-04-23 18:23:30 +0100

    Skeletons for ARM64 macro assembler and adapter generator. No real code is implemented. They are only dummy placeholders.

---
+++ com.oracle.max.asm/src/com/oracle/max/asm/target/aarch64/AARCH64MacroAssembler.java
@@ -0,0 +1,11 @@
+package com.oracle.max.asm.target.aarch64;
+
+import com.sun.cri.ci.*;
+import com.sun.cri.ri.*;
+
+
+public class AARCH64MacroAssembler extends AARCH64Assembler {
+    public AARCH64MacroAssembler(CiTarget target, RiRegisterConfig registerConfig) {
+        super(target, registerConfig);
+    }
+}
