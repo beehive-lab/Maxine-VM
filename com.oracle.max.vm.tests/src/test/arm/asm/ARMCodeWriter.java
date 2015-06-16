05183fe971031cb7594e47b5ad579c7e7298e84a
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2015-06-16 13:00:20 +0300

    Code cleanup

--- com.oracle.max.vm.tests/src/test/arm/asm/ARMCodeWriter.java
+++ com.oracle.max.vm.tests/src/test/arm/asm/ARMCodeWriter.java
@@ -48,50 +48,19 @@ public class ARMCodeWriter {
         val += "(*pf)(" + listOfValues + ");\n";
         val += "asm volatile(\"forever: b forever\");\n";
         return val;
-
-    }/*
-      * void (*pf)(int) = (void (*))(code); print_uart0("changed test.c!\n"); (*pf)(1); // Need to change this to
-      * something related to the test itself asm volatile("forever: b forever");
-      */
+    }
 
     public void createStaticCodeStubsFile(String functionPrototype, byte[] stubs, int entryPoint) {
         try {
-
             PrintWriter writer = new PrintWriter("codebuffer.c", "UTF-8");
             writer.println("unsigned char codeArray[" + ((totalInstructions + 1) * 4 + stubs.length) + "] __attribute__((aligned(0x1000))) = { \n");
-            // writer.println("void c_entry() {");
             log("unsigned char code[" + ((totalInstructions + 1) * 4 + stubs.length) + "] __attribute__((aligned(0x1000))) ;\n");
             log("void c_entry() {");
-            // long xxx = 0xe30090f0; // r9 240
-            // int val;
             for (int i = 0; i < stubs.length; i += 4) {
-                // val = i;
-                /*
-                 * writer.println("codeArray[" + i + " + 3] = " + stubs[i] + ";"); writer.println("codeArray[" + i +
-                 * " + 2] = " + stubs[i+1] + ";"); writer.println("codeArray[" + i + " + 1] = " + stubs[i+2] + ";");
-                 * writer.println("codeArray[" + i + " + 0] = " + stubs[i+3] + ";");
-                 */
-                //writer.println("0x" + Integer.toHexString(stubs[i + 3]) + ", " + "0x" + Integer.toHexString(stubs[i + 2]) + ", " + "0x" + Integer.toHexString(stubs[i + 1]) + ", " + "0x" +
-                                //Integer.toHexString(stubs[i]) + ",\n");
-                writer.println("0x" + Integer.toHexString(stubs[i ]) + ", " + "0x" + Integer.toHexString(stubs[i + 1]) + ", " + "0x" + Integer.toHexString(stubs[i + 2]) + ", " + "0x" +
-                        Integer.toHexString(stubs[i+3]) + ",\n");
+                writer.println("0x" + Integer.toHexString(stubs[i]) + ", " + "0x" + Integer.toHexString(stubs[i + 1]) + ", " + "0x" + Integer.toHexString(stubs[i + 2]) + ", " + "0x" +
+                                Integer.toHexString(stubs[i + 3]) + ",\n");
             }
             writer.println("0xea, 0xff, 0xff, 0xfe  };\n");
-            /*
-             * for (int i = stubs.length/4; i < (totalInstructions+ stubs.length/4); i++) { xxx =
-             * instructions[i-stubs.length/4]; val = (i-stubs.length/4) * 4+stubs.length; writer.println("codeArray[" +
-             * val + "] = " + (xxx & 0xff) + ";"); log("codeArray[" + val + "] = 0x" + Long.toString(xxx & 0xff, 16) +
-             * ";"); val = val + 1; writer.println("code[" + val + "] = " + ((xxx >> 8) & 0xff) + ";"); log("codeArray["
-             * + val + "] = 0x" + Long.toString((xxx >> 8) & 0xff, 16) + ";"); val = val + 1; writer.println("code[" +
-             * val + "] = " + ((xxx >> 16) & 0xff) + ";"); log("codeArray[" + val + "] = 0x" + Long.toString((xxx >> 16)
-             * & 0xff, 16) + ";"); val = val + 1; writer.println("code[" + val + "] = " + ((xxx >> 24) & 0xff) + ";");
-             * log("codeArray[" + val + "] = 0x" + Long.toString((xxx >> 24) & 0xff, 16) + ";"); }
-             */
-            /*
-             * writer.println("codeArray[" + stubs.length + "] = " + 0xfe + ";"); writer.println("codeArray[" +
-             * stubs.length + "+1] = " + 0xff + ";"); writer.println("codeArray[" + stubs.length + "+2] = " + 0xff +
-             * ";"); writer.println("codeArray[" + stubs.length + "+3] = " + 0xea + ";");
-             */
             writer.println("unsigned char *code = codeArray + " + entryPoint + ";");
             writer.println("void c_entry() {");
             writer.print(functionPrototype);
@@ -104,34 +73,20 @@ public class ARMCodeWriter {
 
     public void createCodeStubsFile(byte[] stubs, int entryPoint) {
         try {
-
             PrintWriter writer = new PrintWriter("codebuffer.c", "UTF-8");
             writer.println("unsigned char codeArray[" + ((totalInstructions + 1) * 4 + stubs.length) + "] __attribute__((aligned(0x1000))) ;\n");
             writer.println("void c_entry() {");
             log("unsigned char code[" + ((totalInstructions + 1) * 4 + stubs.length) + "] __attribute__((aligned(0x1000))) ;\n");
             log("void c_entry() {");
-            // long xxx = 0xe30090f0; // r9 240
-            // int val;
             for (int i = 0; i < stubs.length; i += 4) {
-                // val = i;
                 writer.println("codeArray[" + i + " ] = " + stubs[i] + ";");
                 writer.println("codeArray[" + i + " + 1] = " + stubs[i + 1] + ";");
                 writer.println("codeArray[" + i + " + 2] = " + stubs[i + 2] + ";");
                 writer.println("codeArray[" + i + " + 3] = " + stubs[i + 3] + ";");
             }
-            /*
-             * for (int i = stubs.length/4; i < (totalInstructions+ stubs.length/4); i++) { xxx =
-             * instructions[i-stubs.length/4]; val = (i-stubs.length/4) * 4+stubs.length; writer.println("codeArray[" +
-             * val + "] = " + (xxx & 0xff) + ";"); log("codeArray[" + val + "] = 0x" + Long.toString(xxx & 0xff, 16) +
-             * ";"); val = val + 1; writer.println("code[" + val + "] = " + ((xxx >> 8) & 0xff) + ";"); log("codeArray["
-             * + val + "] = 0x" + Long.toString((xxx >> 8) & 0xff, 16) + ";"); val = val + 1; writer.println("code[" +
-             * val + "] = " + ((xxx >> 16) & 0xff) + ";"); log("codeArray[" + val + "] = 0x" + Long.toString((xxx >> 16)
-             * & 0xff, 16) + ";"); val = val + 1; writer.println("code[" + val + "] = " + ((xxx >> 24) & 0xff) + ";");
-             * log("codeArray[" + val + "] = 0x" + Long.toString((xxx >> 24) & 0xff, 16) + ";"); }
-             */
-            writer.println("codeArray[" + (stubs.length+3) +  "] = " + 0xfe + ";");
-            writer.println("codeArray[" + (stubs.length + 2) +"] = " + 0xff + ";");
-            writer.println("codeArray[" + (stubs.length+1) + "] = " + 0xff + ";");
+            writer.println("codeArray[" + (stubs.length + 3) + "] = " + 0xfe + ";");
+            writer.println("codeArray[" + (stubs.length + 2) + "] = " + 0xff + ";");
+            writer.println("codeArray[" + (stubs.length + 1) + "] = " + 0xff + ";");
             writer.println("codeArray[" + (stubs.length) + "] = " + 0xea + ";");
             writer.println("unsigned char *code = codeArray + " + entryPoint + ";");
             writer.close();
@@ -148,7 +103,6 @@ public class ARMCodeWriter {
             writer.println("void c_entry() {");
             log("unsigned char code[" + ((totalInstructions + 1) * 4) + "] __attribute__((aligned(0x1000))) ;\n");
             log("void c_entry() {");
-
             long xxx = 0xe30090f0; // r9 240
             int val;
             for (int i = 0; i < totalInstructions; i++) {
@@ -163,12 +117,8 @@ public class ARMCodeWriter {
                 writer.println("code[" + val + "] = " + ((xxx >> 16) & 0xff) + ";");
                 log("code[" + val + "] = 0x" + Long.toString((xxx >> 16) & 0xff, 16) + ";");
                 val = val + 1;
-
                 writer.println("code[" + val + "] = " + (xxx >>24 & 0xff) + ";");
                 log("code[" + val + "] = 0x" + Long.toString(xxx>>24 & 0xff, 16) + ";");
-
-
-
             }
             writer.println("code[" + totalInstructions * 4 + "] = " + 0xfe + ";");
             log("code[" + totalInstructions * 4 + "] = " + 0xfe + ";");
@@ -185,8 +135,3 @@ public class ARMCodeWriter {
         }
     }
 }
-/*
- * unsigned char code[12] __attribute__((aligned(0x1000))) ; void c_entry() { code[0] = 0xff; code[1] = 0x90; code[2] =
- * 0xa0; code[3] = 0xe3; code[4] = 0xff; code[5] = 0xaf;// r10? code[6] =0x4f; code[7] = 0xe3; // do load or r9 twice
- * code[8] = 0xfe; code[9] = 0xff; code[10] = 0xff; code[11] = 0xea;
- */
