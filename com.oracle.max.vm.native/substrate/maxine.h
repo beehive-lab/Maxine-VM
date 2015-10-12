731e8950b3b8df8ce7b6db956c1f7a4a2034341a
Dr. Andy Nisbet <andy.nisbet@manchester.ac.uk>
2015-10-12 17:50:59 +0100

    Fix to allow compilation on X86

--- com.oracle.max.vm.native/substrate/maxine.h
+++ com.oracle.max.vm.native/substrate/maxine.h
@@ -28,8 +28,8 @@
 
 //extern jlong native_nanoTime(void);
 //extern jlong native_currentTimeMillis(void);
-extern long long native_nanoTime(void);
-extern long long native_currentTimeMillis(void);
+extern jlong native_nanoTime(void);
+extern jlong native_currentTimeMillis(void);
 extern void *native_executablePath(void);
 extern void  native_exit(int code);
 extern void *native_environment(void);
