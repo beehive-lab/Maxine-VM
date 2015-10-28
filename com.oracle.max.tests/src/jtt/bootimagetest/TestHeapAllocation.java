2fbe36b66a63e335d4c8bf11e1e195979ca3a807
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2015-10-28 18:43:04 +0000

    Fix TestHeapAllocation

--- com.oracle.max.tests/src/jtt/bootimagetest/TestHeapAllocation.java
+++ com.oracle.max.tests/src/jtt/bootimagetest/TestHeapAllocation.java
@@ -1,27 +1,19 @@
 package jtt.bootimagetest;
 
-import java.io.*;
-import java.util.*;
-
-/**
- * Created by andyn on 29/10/16
- * Simple example to test heap allocaton
+/*
+ * @Harness: java
+ * @Runs: 1 = true;
  */
-
 public class TestHeapAllocation {
 
 
     public TestHeapAllocation() {
     }
 
-    public static boolean test() {
-	/*
-	 The prints are there for debuggin purposes, they result in indirect calls via $r8 on ARM
-	*/
+    public static boolean test(int a) {
 	System.out.println("TestHeapAllocation.test ENTER");
         TestHeapAllocation  tmp = new TestHeapAllocation();
 	System.out.println("TestHeapAllocation.test DONEALLOC");
 	return tmp != null;
-
    }
 }
