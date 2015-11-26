e6042411178e14e3cd94fbb7d8f6d4cdecf477e9
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2015-11-26 19:27:20 +0200

    Code cleanup

--- maxine-tester/junit-tests/test.c
+++ maxine-tester/junit-tests/test.c
@@ -1,37 +1,17 @@
-volatile unsigned int * const UART0DR = (unsigned int *)0x101f1000;
- 
-void print_uart0(const char *s) {
- while(*s != '\0') { /* Loop until end of string */
- *UART0DR = (unsigned int)(*s); /* Transmit char */
- s++; /* Next char */
- }
-}
+volatile unsigned int * const UART0DR = (unsigned int *) 0x101f1000;
 
-/*int testme(double a, double b) {
-// to compile with me in use gcc to link and -nostartupfiles
-	return (a<b);
-}*/
+void print_uart0(const char *s) {
+	while (*s != '\0') {
+		*UART0DR = (unsigned int) (*s);
+		s++;
+	}
+}
 
 #include "./codebuffer.c"
 
-/*unsigned char  code[12] __attribute__((aligned(0x1000))) ;
-void c_entry() {
-code[0] = 0xff;
-code[1] = 0x90;
-code[2] = 0xa0;
-code[11] = 0xea;
-*/
-
-/*
-	Apologies, need to pass a parameter to this function, and can no longer do this in the test harness ... will try and fix this prior to pushing 
- expect this to break some of the other tests 
-*/
-//void (*pf)(int) = (0); pf  = (void (*))(code);
-#ifdef OLDCOMPILE
 void (*pf)(int) = (void (*))(code);
- print_uart0("changed test.c!\n");
-(*pf)(1); // Need to change this to something related to the test itself
+print_uart0("changed test.c!\n");
+(*pf)(1);
 asm volatile("forever: b forever");
-#endif
 }
 
