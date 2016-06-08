862d4710266a0824a03bfac77a0b8b5f2f9617e2
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2016-06-08 06:40:38 +0200

    Add FP reg in ARMv7

--- com.oracle.max.asm/src/com/oracle/max/asm/target/armv7/ARMV7.java
+++ com.oracle.max.asm/src/com/oracle/max/asm/target/armv7/ARMV7.java
@@ -104,6 +104,7 @@ public class ARMV7 extends CiArchitecture {
     public static final CiRegisterValue RSP = r13.asValue(CiKind.Int);
     public static final CiRegister LR = r14;
     public static final CiRegister PC = r15;
+    public static final CiRegister FP = r11;
     public static final CiRegister rsp = r13;
     public static final CiRegister rip = new CiRegister(48, -1, 0, "rip");
 
