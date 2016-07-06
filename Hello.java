52154a56cc0670c88a4aea03bbba85c53d05d2e0
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2016-07-07 01:35:03 +0300

    Fix in Inflated locks

--- Hello.java
+++ Hello.java
@@ -4,30 +4,30 @@ public class Hello {
 
 public static void main(String []args) {
 
-
-	System.err.println("HELLO WORLD");
-	int x[] = new int [1024];
-	int j;
-	for(j = 0; j < 1024;j++) {
-		if(j == 0 || j == 1) System.err.println("DOING LOOP " + j);
-		x[j] = j;
-		String xx = Integer.toString(x[j]);
-		System.err.println("---> " + xx);
+	int a = 0;
+	int b = 0;
+	int c = 0 ;
+	while (true) {
+		System.out.println("IN " + a);
+		a++;
+		
+			outer: if (c % 2 == 0) {
+				if (b >= 0) {
+					b = -1;
+					System.out.println("MAKE -1");
+					break outer;
+				} else {
+					b = 1;
+					System.out.println("MAKE 1");
+				}
+				c++;
+			}
+		
+		System.out.println("OUT " + a);
+		
+		
 	}
-	System.err.println("DONE LOOP");
-//	System.err.println(args[0]);
-	for(int i = 0; i < args.length;i++)	{
-
-		System.err.println(args[i]);
-
-	}
-	System.err.println("DONE ARGS");
-System.err.println(new Date());
-long xx = System.currentTimeMillis();
-
-Date currentDate = new Date(xx);
-System.err.println(currentDate);
 
 }
 
-};
+}
