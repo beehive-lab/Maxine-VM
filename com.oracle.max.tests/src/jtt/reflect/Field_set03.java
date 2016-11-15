7c6d1defbb5d77af4409ae4a3678b627c1366364
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2016-11-15 22:24:10 +0200

    Code cleanup

--- com.oracle.max.tests/src/jtt/reflect/Field_set03.java
+++ com.oracle.max.tests/src/jtt/reflect/Field_set03.java
@@ -58,11 +58,8 @@ public class Field_set03 {
             Field_set03.class.getField("floatField").setFloat(object, 16);
             return object.floatField == 16;
         } else if (arg == 6) {
-		//System.out.println("BEFORE" + (int)(object.doubleField));
-            	Field_set03.class.getField("doubleField").setDouble(object, 17);
-		//System.out.println("AFTER " + (int)(object.doubleField));
-            	Field_set03.class.getField("doubleField").setDouble(object, 17);
-		//System.out.println("AFTER " + (int)(object.doubleField));
+            Field_set03.class.getField("doubleField").setDouble(object, 17);
+            Field_set03.class.getField("doubleField").setDouble(object, 17);
             return object.doubleField == 17;
         } else if (arg == 7) {
             Field_set03.class.getField("booleanField").setBoolean(object, true);
