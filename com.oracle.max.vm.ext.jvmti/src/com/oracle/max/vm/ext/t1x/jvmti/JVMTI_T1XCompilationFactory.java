be0dad2c41b1abc5dd9d145dbf6eaccc0a1a586f
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2016-11-16 15:51:18 +0200

    Code cleanup

--- com.oracle.max.vm.ext.jvmti/src/com/oracle/max/vm/ext/t1x/jvmti/JVMTI_T1XCompilationFactory.java
+++ com.oracle.max.vm.ext.jvmti/src/com/oracle/max/vm/ext/t1x/jvmti/JVMTI_T1XCompilationFactory.java
@@ -26,15 +26,13 @@ import com.oracle.max.vm.ext.t1x.*;
 import com.oracle.max.vm.ext.t1x.jvmti.amd64.*;
 import com.oracle.max.vm.ext.t1x.jvmti.armv7.*;
 
-
-
 public class JVMTI_T1XCompilationFactory extends T1XCompilationFactory {
     @Override
     public T1XCompilation newT1XCompilation(T1X t1x) {
         if (T1X.isAMD64()) {
             return new JVMTI_AMD64T1XCompilation(t1x);
         } else if (T1X.isARM()) {
-             return new JVMTI_ARMV7T1XCompilation(t1x);
+            return new JVMTI_ARMV7T1XCompilation(t1x);
         } else {
             throw T1X.unimplISA();
         }
