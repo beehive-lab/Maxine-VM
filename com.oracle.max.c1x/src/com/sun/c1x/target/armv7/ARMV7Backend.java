8e8a4547bb845064befb3769e1df92dcf82a8513
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2016-11-28 14:46:35 +0100

    Code cleanup

--- com.oracle.max.c1x/src/com/sun/c1x/target/armv7/ARMV7Backend.java
+++ com.oracle.max.c1x/src/com/sun/c1x/target/armv7/ARMV7Backend.java
@@ -1,24 +1,19 @@
 /*
- * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved. DO NOT ALTER OR REMOVE COPYRIGHT NOTICES
+ * OR THIS FILE HEADER.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * This code is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License version 2 only, as published by the Free Software Foundation.
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License version 2 for
+ * more details (a copy is included in the LICENSE file that accompanied this code).
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License version 2 along with this work; if not, write to
+ * the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA or visit www.oracle.com if you need
+ * additional information or have any questions.
  */
 package com.sun.c1x.target.armv7;
 
@@ -37,15 +32,17 @@ import com.sun.cri.ri.*;
 import com.sun.cri.xir.*;
 
 /**
- * The {@code X86Backend} class represents the backend for the ARMV7 architecture.
+ * The {@code ARMV&Backend} class represents the backend for the ARMV7 architecture.
  */
 public class ARMV7Backend extends Backend {
 
     public ARMV7Backend(C1XCompiler compiler) {
         super(compiler);
     }
+
     /**
-     * Creates a new LIRGenerator for x86.
+     * Creates a new LIRGenerator for ARMv7.
+     *
      * @param compilation the compilation for which to create the LIR generator
      * @return an appropriate LIR generator instance
      */
@@ -55,7 +52,8 @@ public class ARMV7Backend extends Backend {
     }
 
     /**
-     * Creates a new LIRAssembler for x86.
+     * Creates a new LIRAssembler for ARMv7.
+     *
      * @param compilation the compilation for which to create the LIR assembler
      * @return an appropriate LIR assembler instance
      */
@@ -68,6 +66,7 @@ public class ARMV7Backend extends Backend {
     public FrameMap newFrameMap(C1XCompilation compilation, RiResolvedMethod method, int numberOfLocks) {
         return new FrameMap(compilation, method, numberOfLocks);
     }
+
     @Override
     public AbstractAssembler newAssembler(RiRegisterConfig registerConfig) {
         return new ARMV7MacroAssembler(compiler.target, registerConfig);
@@ -106,7 +105,6 @@ public class ARMV7Backend extends Backend {
         }
         return result;
     }
-
 
     @Override
     public CompilerStub emit(XirTemplate t) {
