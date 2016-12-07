4c2036e9f4ab16b975fa5406b7f5e186584772e0
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2016-12-07 01:47:14 +0100

    Code cleanup

--- com.oracle.max.vm/src/com/sun/max/vm/runtime/arm/ARMTrapFrameAccess.java
+++ com.oracle.max.vm/src/com/sun/max/vm/runtime/arm/ARMTrapFrameAccess.java
@@ -80,7 +80,7 @@ public final class ARMTrapFrameAccess extends TrapFrameAccess {
     static {
         CiRegister[] csaRegs = { r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, /* r13, */ r14, s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16, s17, s18, s19, s20,
                         s21, s22, s23, s24, s25, s26, s27, s28, s29, s30, s31};
-        int size = 4 * 14 + 8 * 32; // Size is wrong for FPU
+        int size =( 4 * 14) + (8 * 32);
         TRAP_NUMBER_OFFSET = size;
         size += 4;
         FLAGS_OFFSET = size;
