1f92b3c9d1a7813ad2e16fb5f324978e250c1daf
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2017-03-30 21:51:12 +0300

    [license] com.oracle.max.tests

--- com.oracle.max.tests/src/jtt/bytecode/BC_dload_9.java
+++ com.oracle.max.tests/src/jtt/bytecode/BC_dload_9.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2007, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, APT Group, School of Computer Science,
+ * The University of Manchester. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -19,9 +20,6 @@
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
- */
-/*
- * Copyright (c) 2007 Sun Microsystems, Inc. All rights reserved. Use is subject to license terms.
  */
 package jtt.bytecode;
 
