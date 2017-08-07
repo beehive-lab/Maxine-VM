b77a3d232b30ca855c884badd0566969707dc92d
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2017-08-07 18:24:55 +0300

    [test] Add new test that retrieves and prints date from the system

--- com.oracle.max.tests/src/test/output/PrintDate01.java
+++ com.oracle.max.tests/src/test/output/PrintDate01.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2009, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, APT Group, School of Computer Science,
+ * The University of Manchester. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -15,18 +16,16 @@
  * You should have received a copy of the GNU General Public License version
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
  */
 package test.output;
 
+import java.text.*;
 import java.util.*;
 
-public class PrintDate {
+public class PrintDate01 {
     public static void main(String[] args) {
-        final Date date = new Date(1000000000000L);
-        System.out.println(date);
+        Date x = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println(dateFormat.format(x));
     }
 }
