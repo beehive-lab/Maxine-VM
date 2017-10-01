5a4aafae8e54a5461a1595655dccff4349958fff
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2017-10-01 18:31:26 +0100

    [aarch64] Add correct implementation of startup.s in aarch64

--- maxine-tester/junit-tests/startup_aarch64.s
+++ maxine-tester/junit-tests/startup_aarch64.s
@@ -1,28 +1,13 @@
 .global _Reset
 _Reset:
- LDR sp, =stack_top
- BL Setup_Undef_Stack
+ ADR x0,stack_top
+ MOV sp,x0
  BL VFP_enable
- BL c_entry	
- B asm_entry
+ BL c_entry
  B .
 
-.EQU Mode_UNDEF, 0x1B
-.EQU UNDEF_Stack, 0x4000;
-Setup_Undef_Stack:
-	MRS r0,CPSR
-	MOV r1,r0
-	BIC r1,r1,#0x1f
-	ORR r1,r1,#Mode_UNDEF
-	MSR CPSR_c, r1
-	LDR SP,=UNDEF_Stack
-	MSR CPSR_c,r0
-	BX LR
-
-
-.EQU VFPVAL, 0x40000000
+.EQU VFPVAL, 0x30
 VFP_enable:
-	MOV r1,#VFPVAL
-	FMXR FPEXC, r1
-        BX LR	
-
+        MOVZ x1,#VFPVAL,LSL #16
+        MSR CPACR_EL1, x1
+        BR x30
