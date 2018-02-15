69582a5ad1353533ff544a314214953ebc404d7f
Tim Hartley <t.j.hartley@gmail.com>
2018-02-15 15:12:37 +0000

    [fix][armv8] Increase return address to 16 bytes

--- com.oracle.max.asm/src/com/oracle/max/asm/target/aarch64/Aarch64.java
+++ com.oracle.max.asm/src/com/oracle/max/asm/target/aarch64/Aarch64.java
@@ -164,7 +164,12 @@ public class Aarch64 extends CiArchitecture {
               0, /*LOAD_STORE | STORE_STORE*/   //implicitMemoryBarriers (no implicit barriers)
               -1,                               //nativeCallDisplacementOffset (ingore)
               32,                               //registerReferenceMapBitCount
-              8);                               //returnAddressSize (8 bytes)
+/* Although aarch64 addresses have a 64 bit precision, a callers return address effectively occupies a 16 byte
+ * stack slot in an activation frame according to the AAPCS sp alignment. The returnAddressSize
+ * field is used for stack navigation/alignment purposes.
+ */
+
+              16);                               //returnAddressSize (16 bytes)
     }
 
     /**
