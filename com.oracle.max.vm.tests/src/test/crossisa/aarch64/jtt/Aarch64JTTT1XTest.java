23981884dfadebdf3676603a7dc11b3cd0e180c4
Andreas Andronikakis <aandronikakis@hotmail.com>
2018-02-27 22:22:57 +0000

    [test][T1X] Enable test_T1X_jtt_BC_iadd2()

--- com.oracle.max.vm.tests/src/test/crossisa/aarch64/jtt/Aarch64JTTT1XTest.java
+++ com.oracle.max.vm.tests/src/test/crossisa/aarch64/jtt/Aarch64JTTT1XTest.java
@@ -19,9 +19,12 @@
  */
 package test.crossisa.aarch64.jtt;
 
+import static com.oracle.max.asm.target.aarch64.Aarch64.*;
 import static com.sun.max.vm.MaxineVM.*;
+import static org.objectweb.asm.util.MaxineByteCode.getByteArray;
 
 import java.io.*;
+import java.lang.reflect.Modifier;
 import java.util.*;
 
 import com.oracle.max.asm.target.aarch64.*;
@@ -235,4 +238,27 @@ public class Aarch64JTTT1XTest extends MaxTestCase {
         }
     }
 
+    public void test_T1X_jtt_BC_iadd2() throws Exception {
+        byte[] argsOne = {1, 0, 33, 1, -128, 127};
+        byte[] argsTwo = {2, -1, 67, -1, 1, 1};
+        initTests();
+        t1x.createOfflineTemplate(c1x, T1XTemplateSource.class, t1x.templates, "ireturnUnlock");
+        t1x.createOfflineTemplate(c1x, T1XTemplateSource.class, t1x.templates, "ireturn");
+        t1x.createOfflineTemplate(c1x, T1XTemplateSource.class, t1x.templates, "iadd");
+        byte[] code = getByteArray("test", "jtt.bytecode.BC_iadd2");
+        for (int i = 0; i < argsOne.length; i++) {
+            int answer = jtt.bytecode.BC_iadd2.test(argsOne[i], argsTwo[i]);
+            expectedValues[0] = answer;
+            initialiseFrameForCompilation(code, "(BB)I", Modifier.PUBLIC | Modifier.STATIC);
+            Aarch64MacroAssembler masm = theCompiler.getMacroAssembler();
+            masm.mov64BitConstant(r0, argsOne[i]);
+            masm.mov64BitConstant(r1, argsTwo[i]);
+            masm.push(r0, r1);
+            theCompiler.offlineT1XCompile(anMethod, codeAttr, code, code.length - 1);
+            masm.pop(r0);
+            long[] registerValues = generateAndTest(expectedValues, testvalues, bitmasks);
+            assert registerValues[0] == answer : "Failed incorrect value " + registerValues[0] + " " + answer;
+            theCompiler.cleanup();
+        }
+    }
 }
