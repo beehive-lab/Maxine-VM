13220aa8a03b8a094361a0433a6e3d161c8dde8d
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2018-03-14 21:13:55 +0200

    [fix][test][aarch64] Fix and enable C1X_jtt_BC_lsub

--- com.oracle.max.vm.tests/src/test/crossisa/aarch64/jtt/Aarch64JTTC1XTest.java
+++ com.oracle.max.vm.tests/src/test/crossisa/aarch64/jtt/Aarch64JTTC1XTest.java
@@ -2721,7 +2721,7 @@ public class Aarch64JTTC1XTest {
         }
     }
 
-    // @Test
+    @Test
     public void C1X_jtt_BC_lsub() throws Exception {
         vm().compilationBroker.setOffline(initialised);
         String klassName = getKlassName("jtt.bytecode.BC_lsub");
@@ -2744,8 +2744,8 @@ public class Aarch64JTTC1XTest {
             long   expectedValue     = BC_lsub.test(pair.lfirst, pair.lsecond);
             String functionPrototype = Aarch64CodeWriter.preAmble("long long", "long long, long long", Long.toString(pair.lfirst) + "LL," + Long.toString(pair.lsecond) + "LL");
             long[] registerValues    = generateAndTestStubs(functionPrototype, entryPoint, codeBytes, expectedValues, testvalues, bitmasks);
-            long   returnValue       = connectRegs(registerValues[0], registerValues[1]);
-            assert returnValue == expectedValue : "Failed incorrect value r0 " + registerValues[0] + " r1 " + registerValues[1] + " " + expectedValue + " " + returnValue;
+            long   returnValue       = registerValues[0];
+            assert returnValue == expectedValue : "Failed incorrect value " + expectedValue + " " + returnValue;
             theCompiler.cleanup();
         }
     }
