b02529e4f96e99441c6c069e8c01827a2209cc33
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2018-03-14 20:31:16 +0200

    [fix][test][aarch64] Fix and enable C1X_jtt_BC_land

--- com.oracle.max.vm.tests/src/test/crossisa/aarch64/jtt/Aarch64JTTC1XTest.java
+++ com.oracle.max.vm.tests/src/test/crossisa/aarch64/jtt/Aarch64JTTC1XTest.java
@@ -2044,7 +2044,7 @@ public class Aarch64JTTC1XTest {
         }
     }
 
-    // @Test
+    @Test
     public void C1X_jtt_BC_land() throws Exception {
         vm().compilationBroker.setOffline(initialised);
         String klassName = getKlassName("jtt.bytecode.BC_land");
@@ -2065,8 +2065,8 @@ public class Aarch64JTTC1XTest {
             long   expectedValue     = BC_land.test(pair.lfirst, pair.lsecond);
             String functionPrototype = Aarch64CodeWriter.preAmble("long long", "long long, long long", Long.toString(pair.lfirst) + "LL," + Long.toString(pair.lsecond) + "LL");
             long[] registerValues    = generateAndTestStubs(functionPrototype, entryPoint, codeBytes, expectedValues, testvalues, bitmasks);
-            long   returnValue       = connectRegs(registerValues[0], registerValues[1]);
-            assert returnValue == expectedValue : "Failed incorrect value r0 " + registerValues[0] + " r1 " + registerValues[1] + " " + expectedValue + " " + returnValue;
+            long   returnValue       = registerValues[0];
+            assert returnValue == expectedValue : "Failed incorrect value " + expectedValue + " " + returnValue;
             theCompiler.cleanup();
         }
     }
