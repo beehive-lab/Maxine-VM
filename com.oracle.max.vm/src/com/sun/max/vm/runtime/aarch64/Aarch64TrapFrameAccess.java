113322def7348d307e31f1906e5b289447f06385
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2018-04-23 10:06:39 +0300

    [aarch64] Add assertions in unimplemented methods

--- com.oracle.max.vm/src/com/sun/max/vm/runtime/aarch64/Aarch64TrapFrameAccess.java
+++ com.oracle.max.vm/src/com/sun/max/vm/runtime/aarch64/Aarch64TrapFrameAccess.java
@@ -34,55 +34,61 @@ public class Aarch64TrapFrameAccess extends TrapFrameAccess {
     @Override
     public Pointer getPCPointer(Pointer trapFrame) {
         // TODO Auto-generated method stub
+        assert false : "Unimplemented";
         return null;
     }
 
     @Override
     public Pointer getSP(Pointer trapFrame) {
         // TODO Auto-generated method stub
+        assert false : "Unimplemented";
         return null;
     }
 
     @Override
     public Pointer getFP(Pointer trapFrame) {
         // TODO Auto-generated method stub
+        assert false : "Unimplemented";
         return null;
     }
 
     @Override
     public Pointer getSafepointLatch(Pointer trapFrame) {
         // TODO Auto-generated method stub
+        assert false : "Unimplemented";
         return null;
     }
 
     @Override
     public void setSafepointLatch(Pointer trapFrame, Pointer value) {
         // TODO Auto-generated method stub
-
+        assert false : "Unimplemented";
     }
 
     @Override
     public int getTrapNumber(Pointer trapFrame) {
         // TODO Auto-generated method stub
+        assert false : "Unimplemented";
         return 0;
     }
 
     @Override
     public Pointer getCalleeSaveArea(Pointer trapFrame) {
         // TODO Auto-generated method stub
+        assert false : "Unimplemented";
         return null;
     }
 
     @Override
     public void setTrapNumber(Pointer trapFrame, int trapNumber) {
         // TODO Auto-generated method stub
-
+        assert false : "Unimplemented";
     }
 
     @Override
     public void logTrapFrame(Pointer trapFrame) {
         // TODO Auto-generated method stub
-
+        assert false : "Unimplemented";
     }
 
 }
