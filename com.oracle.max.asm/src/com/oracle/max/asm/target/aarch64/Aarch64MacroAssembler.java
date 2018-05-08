ce69853c5f449783588a6b15046b2e0ba6f2cc22
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2018-05-09 00:37:02 +0300

    [aarch64] Optimize/Simplify cmp

--- com.oracle.max.asm/src/com/oracle/max/asm/target/aarch64/Aarch64MacroAssembler.java
+++ com.oracle.max.asm/src/com/oracle/max/asm/target/aarch64/Aarch64MacroAssembler.java
@@ -1338,12 +1338,7 @@ public class Aarch64MacroAssembler extends Aarch64Assembler {
                 super.subs(size, Aarch64.zr, x, (int) y);
             }
         } else {
-            if (y <= Integer.MAX_VALUE && y >= Integer.MIN_VALUE) {
-                mov32BitConstant(scratchRegister, y);
-            } else {
-                assert size == 64;
-                mov64BitConstant(scratchRegister, y);
-            }
+            mov(scratchRegister, y);
             cmp(size, x, scratchRegister);
         }
     }
