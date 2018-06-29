c946124a1dcc969f6bafebbf28bc0b5dd8407cfc
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2018-06-29 06:18:05 +0300

    [fix][aarch64] Fix Stub.returnAddressPointer for Aarch64

--- com.oracle.max.vm/src/com/sun/max/vm/compiler/target/Stub.java
+++ com.oracle.max.vm/src/com/sun/max/vm/compiler/target/Stub.java
@@ -226,6 +226,8 @@ public final class Stub extends TargetMethod {
             return AMD64TargetMethodUtil.returnAddressPointer(frame);
         } else if (platform().isa == ISA.ARM) {
             return ARMTargetMethodUtil.returnAddressPointer(frame);
+        } else if (platform().isa == ISA.Aarch64) {
+            return Aarch64TargetMethodUtil.returnAddressPointer(frame);
         } else {
             throw FatalError.unimplemented("com.sun.max.vm.compiler.target.Stub.returnAddressPointer");
         }
