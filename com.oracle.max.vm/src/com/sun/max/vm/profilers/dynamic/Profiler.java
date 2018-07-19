5e06f2549c67c142b9f3d9866b38754b86611e66
Orion Papadakis <orion.papadakis@postgrad.manchester.ac.uk>
2018-07-19 15:13:32 +0100

    [WIP] add initial and simple functionality for the Profiler

--- com.oracle.max.vm/src/com/sun/max/vm/profilers/dynamic/Profiler.java
+++ com.oracle.max.vm/src/com/sun/max/vm/profilers/dynamic/Profiler.java
@@ -1,6 +1,49 @@
 package com.sun.max.vm.profilers.dynamic;
 
+import com.sun.max.unsafe.Size;
+import com.sun.max.vm.Log;
+import com.sun.max.vm.MaxineVM;
+import com.sun.max.vm.thread.VmThread;
+
+import java.util.Hashtable;
+
 public class Profiler {
+
+    /**
+     * Histogram: the data structure that stores the profiling outcome
+     */
+
+    public Hashtable histogram;
+
+
+    //constructor
+    public Profiler() {
+        this.histogram = new Hashtable();
+    }
+
+
+    /**
+     * Methods to manage the histogram
+     */
+
+    /**
+     * This method is called when a profiled object is allocated.
+     * Increments the number of the equal-size allocated objects.
+     * */
+    public void record(Size size){
+        //histogram.get(size);
+        Log.println("Size="+size.toLong()+" Bytes, ThreadId="+VmThread.current().id());
+
+    }
+
+    public void profile(Size size){
+        // if the thread local profiling flag is enabled
+        if(!VmThread.current().PROFILE){
+            if(MaxineVM.isRunning()){
+                record(size);
+            }
+        }
+    }
 
     
 }
