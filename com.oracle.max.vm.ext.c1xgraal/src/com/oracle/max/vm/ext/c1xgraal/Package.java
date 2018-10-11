a2f87baa1af402ba9417758bbcaab8fc0808a728
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2018-10-11 04:37:29 +0300

    [refactor] mv com.sun.max.config.c1xgraal com.oracle.max.vm.ext.c1xgraal

--- com.oracle.max.vm.ext.c1xgraal/src/com/oracle/max/vm/ext/c1xgraal/Package.java
+++ com.oracle.max.vm.ext.c1xgraal/src/com/oracle/max/vm/ext/c1xgraal/Package.java
@@ -20,7 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package com.sun.max.config.c1xgraal;
+package com.oracle.max.vm.ext.c1xgraal;
 
 import com.sun.max.config.*;
 import com.sun.max.vm.*;
