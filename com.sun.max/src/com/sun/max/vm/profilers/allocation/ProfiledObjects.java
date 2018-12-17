107a94256c568aa33150ce4a3354a8a6b25192a5
Orion Papadakis <orion.papadakis@postgrad.manchester.ac.uk>
2018-12-17 21:35:51 +0000

    [profiler][WIP] Initial commit for new profiling approach. The new approach replaces the histogram creation and handling with pure object information storing. Recording just the objects during run-time give us much more post-execution flexibility to extract usefull information out of them.

---
+++ com.sun.max/src/com/sun/max/vm/profilers/allocation/ProfiledObjects.java
@@ -0,0 +1,97 @@
+package com.sun.max.vm.profilers.allocation;
+
+import com.sun.max.annotate.NEVER_INLINE;
+import com.sun.max.annotate.NO_SAFEPOINT_POLLS;
+import com.sun.max.vm.Log;
+
+public class ProfiledObjects {
+
+    /**
+     * Each profiling cycle is predefined to track 10000 objects.
+     * TODO: configurable
+     */
+    public int SIZE = 100000;
+
+    /**
+     * The following 4 variables compose the stored information for each object.
+     *
+     * Index: unique for each object to make it distinguishable. [1-inf] index = 0 for empty cells
+     * Type: the object's type/class
+     * Size: the object's size. might exist objects with the same type but with different size.
+     * Address: the object's address in the heap
+     */
+    public int[] index;
+    public String[] type;
+    public int[] size;
+    public long[] address;
+
+    public int currentIndex;
+
+    public ProfiledObjects () {
+        index = new int[SIZE];
+        type = new String[SIZE];
+        size = new int[SIZE];
+        address = new long[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            index[i] = 0;
+            type[i] = "null";
+            size[i] = 0;
+            address[i] = 0;
+        }
+
+        currentIndex = 0;
+    }
+
+    @NO_SAFEPOINT_POLLS("allocation profiler call chain must be atomic")
+    @NEVER_INLINE
+    public void record(int index, String type, int size, long address) {
+        /*if( !(currentIndex < SIZE) ) {
+            int newSIZE = SIZE+1000;
+            int[] newIndex = new int[newSIZE];
+            String[] newType = new String[newSIZE];
+            int[] newSize = new int[newSIZE];
+            long[] newAddress = new long[newSIZE];
+
+            for (int i = 0; i < SIZE; i++) {
+                newIndex[i] = this.index[i];
+                newType[i] = this.type[i];
+                newSize[i] = this.size[i];
+                newAddress[i] = this.address[i];
+            }
+            for (int i = SIZE; i < newSIZE; i++) {
+                newIndex[i] = 0;
+                newType[i] = "null";
+                newSize[i] = 0;
+                newAddress[i] = 0;
+            }
+            this.index = newIndex;
+            this.type = newType;
+            this.size = newSize;
+            this.address = newAddress;
+            SIZE = newSIZE;
+
+        }*/
+        this.index[currentIndex] = index;
+        this.type[currentIndex] = type;
+        this.size[currentIndex] = size;
+        this.address[currentIndex] = address;
+        currentIndex++;
+    }
+
+    public void print() {
+        for(int i=0; i<currentIndex; i++){
+            Log.print(index[i]);
+            Log.print(" ");
+            Log.print(type[i]);
+            Log.print(" ");
+            Log.print(size[i]);
+            Log.print(" ");
+            Log.println(address[i]);
+        }
+    }
+
+    public void resetCycle() {
+        currentIndex = 0;
+    }
+}
