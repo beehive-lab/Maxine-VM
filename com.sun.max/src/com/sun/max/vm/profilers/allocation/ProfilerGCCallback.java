80a381a6e07e8dabda53a4ed701ed3c6cf27d86c
Orion Papadakis <orion.papadakis@postgrad.manchester.ac.uk>
2018-12-19 21:01:41 +0000

    [licence] Add licence header

--- com.sun.max/src/com/sun/max/vm/profilers/allocation/ProfilerGCCallback.java
+++ com.sun.max/src/com/sun/max/vm/profilers/allocation/ProfilerGCCallback.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2018, APT Group, School of Computer Science,
+ * The University of Manchester. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package com.sun.max.vm.profilers.allocation;
 
 import com.sun.max.vm.MaxineVM;
@@ -13,7 +32,7 @@ public class ProfilerGCCallback implements Heap.GCCallback {
     public void gcCallback(Heap.GCCallbackPhase gcCallbackPhase) {
         if (gcCallbackPhase == Heap.GCCallbackPhase.BEFORE) {
             //TODO: move here pre-gc actions
-        }else if (gcCallbackPhase == Heap.GCCallbackPhase.AFTER) {
+        } else if (gcCallbackPhase == Heap.GCCallbackPhase.AFTER) {
             //TODO: move here pre-gc actions
         }
     }
