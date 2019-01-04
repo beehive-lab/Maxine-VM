018ff6919f42d3e77dda30471d27c199b01af8ba
Orion Papadakis <orion.papadakis@postgrad.manchester.ac.uk>
2019-01-04 15:58:42 +0000

    [fix] Ommit jnumautils package from boot image creation phase class registration If the native library has been loaded during boot image creation, it cannot be called during run time.

--- com.sun.max/src/com/sun/max/config/jdk/Package.java
+++ com.sun.max/src/com/sun/max/config/jdk/Package.java
@@ -197,6 +197,7 @@ public class Package extends BootImagePackage {
         HostedBootClassLoader.omitClass("sun.nio.ch.Util");
         HostedBootClassLoader.omitClass("sun.nio.fs.UnixNativeDispatcher");
         HostedBootClassLoader.omitClass("sun.jkernel.Bundle");
+        HostedBootClassLoader.omitPackage("uk.ac.manchester.jnumautils", true);
         // Java 7 only class that indirectly caches references to JarFiles
         HostedBootClassLoader.omitClass(sun.misc.Launcher.class.getName() + "$BootClassPathHolder");
         HostedBootClassLoader.omitClass("java.lang.invoke.BoundMethodHandle");
