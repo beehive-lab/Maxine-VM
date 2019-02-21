67f551d53c9cd1af5ffd3bb5a65d3381f07ee2a0
Florin Blanaru <florin.blanaru96@gmail.com>
2019-02-21 11:53:58 +0000

    [riscv][c1x] save FP registers fs0-11 along s0-11 on method call

--- com.sun.max/src/com/sun/max/vm/compiler/target/RegisterConfigs.java
+++ com.sun.max/src/com/sun/max/vm/compiler/target/RegisterConfigs.java
@@ -47,6 +47,7 @@ import com.sun.max.vm.runtime.*;
 import com.sun.max.vm.runtime.aarch64.Aarch64TrapFrameAccess;
 import com.sun.max.vm.runtime.amd64.*;
 import com.sun.max.vm.runtime.arm.*;
+import com.sun.max.vm.runtime.riscv64.RISCV64TrapFrameAccess;
 
 /**
  * The set of register configurations applicable to compiled code in the VM.
@@ -459,7 +460,7 @@ public class RegisterConfigs {
 
                 CiRegisterConfig compilerStub = new CiRegisterConfig(standard, new CiCalleeSaveLayout(0, -1, 8, allRegistersExceptLatch));
                 CiRegisterConfig uncommonTrapStub = new CiRegisterConfig(standard, new CiCalleeSaveLayout(0, -1, 8, csaRegisters));
-                CiRegisterConfig trapStub = new CiRegisterConfig(standard, Aarch64TrapFrameAccess.CSL);
+                CiRegisterConfig trapStub = new CiRegisterConfig(standard, RISCV64TrapFrameAccess.CSL);
                 CiRegisterConfig trampoline = new CiRegisterConfig(standard, new CiCalleeSaveLayout(0, -1, 8,
                         RISCV64.x10, RISCV64.x11, RISCV64.x12, RISCV64.x13,
                         RISCV64.x14, RISCV64.x15, RISCV64.x16, RISCV64.x17, // parameters
@@ -471,7 +472,9 @@ public class RegisterConfigs {
 
                 CiRegisterConfig n2j = new CiRegisterConfig(standard, new CiCalleeSaveLayout(Integer.MAX_VALUE, -1, 8,
                         RISCV64.sp, RISCV64.x8, RISCV64.x9, RISCV64.s2, RISCV64.s3, RISCV64.s4, RISCV64.s5, RISCV64.s6, RISCV64.s7, RISCV64.s8,
-                        RISCV64.s9, RISCV64.s10, RISCV64.s11));
+                        RISCV64.s9, RISCV64.s10, RISCV64.s11,
+                        RISCV64.fs0, RISCV64.fs1, RISCV64.fs2, RISCV64.fs3, RISCV64.fs4, RISCV64.fs5, RISCV64.fs6,
+                        RISCV64.fs7, RISCV64.fs8, RISCV64.fs9, RISCV64.fs10, RISCV64.fs11));
                 n2j.stackArg0Offsets[JavaCallee.ordinal()] = nativeStackArg0Offset;
 
                 roleMap.put(ABI_FP, RISCV64.fp);
