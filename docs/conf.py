13a6db38514a515a772653835401d2da2cd50fcb
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2019-02-22 03:59:23 +0200

    [doc] Add commonmark support to sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,12 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
