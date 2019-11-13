3311bd14f786c925f1074ce1fd02b4a4561509aa
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2019-11-13 13:13:07 +0200

    [license] Update copyright notices

--- com.sun.c1x/src/com/sun/c1x/asm/TargetMethodAssembler.java
+++ com.sun.c1x/src/com/sun/c1x/asm/TargetMethodAssembler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, APT Group, School of Computer Science,
+ * Copyright (c) 2017, 2019, APT Group, School of Computer Science,
  * The University of Manchester. All rights reserved.
  * Copyright (c) 2011, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
--- com.sun.cri/src/com/sun/cri/ci/CiTargetMethod.java
+++ com.sun.cri/src/com/sun/cri/ci/CiTargetMethod.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright (c) 2019, APT Group, School of Computer Science,
+ * The University of Manchester. All rights reserved.
  * Copyright (c) 2014, 2015, Andrey Rodchenko. All rights reserved.
  * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
