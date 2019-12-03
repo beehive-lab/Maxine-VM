5a8208cc4582fedde5a08a90539926816b0a0876
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2019-12-03 11:46:05 +0200

    [license] Update copyright notices

--- com.sun.c1x/src/com/sun/c1x/target/amd64/AMD64XirAssembler.java
+++ com.sun.c1x/src/com/sun/c1x/target/amd64/AMD64XirAssembler.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright (c) 2019, APT Group, School of Computer Science,
+ * The University of Manchester. All rights reserved.
  * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -15,10 +17,6 @@
  * You should have received a copy of the GNU General Public License version
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
  */
 package com.sun.c1x.target.amd64;
 
